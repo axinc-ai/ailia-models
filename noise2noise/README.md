5ce6132320f88e3fe5c6c87d7af629c1728e105b
Kazuki Kyakuno <kyakuno@axell.co.jp>
2019-09-16 09:52:56 +0900

    Move noise2noise model to GCS

---
+++ noise2noise/README.md
@@ -0,0 +1,27 @@
+# Noise2Noise
+
+## Input
+
+![Input](monarch-gaussian-noisy.png)
+
+Shape : (1,3,256,256)
+Range : [0.0, 1.0]
+
+## Output
+
+![Output](output.png)
+
+Shape : (1,3,256,256)
+Range : [0, 1.0]
+
+## Reference
+
+[Learning Image Restoration without Clean Data](https://github.com/joeylitalien/noise2noise-pytorch)
+
+## Framework
+
+Pytorch 1.2.0
+
+## Model Format
+
+ONNX opset = 10
--- noise2noise/noise2noise.py
+++ noise2noise/noise2noise.py
@@ -2,6 +2,8 @@ import numpy as np
 import time
 import os
 import cv2
+import urllib.request
+import sys
 
 from PIL import Image
 
@@ -10,6 +12,15 @@ import ailia
 model_path = "noise2noise_gaussian.onnx.prototxt"
 weight_path = "noise2noise_gaussian.onnx"
 
+print("downloading ...");
+
+if not os.path.exists(model_path):
+    urllib.request.urlretrieve("https://storage.googleapis.com/ailia-models/noise2noise/"+model_path,model_path)
+if not os.path.exists(weight_path):
+    urllib.request.urlretrieve("https://storage.googleapis.com/ailia-models/noise2noise/"+weight_path,weight_path)
+
+print("loading ...");
+
 env_id=ailia.ENVIRONMENT_AUTO
 
 env_id=ailia.get_gpu_environment_id()
@@ -17,6 +28,8 @@ net = ailia.Net(model_path,weight_path,env_id=env_id)
 
 ailia_input_width = net.get_input_shape()[3]
 ailia_input_height = net.get_input_shape()[2]
+
+print("inferencing ...");
 
 file_name = './monarch-gaussian-noisy.png'
 
@@ -47,4 +60,4 @@ output_img[output_img<0] = 0
 output_img[output_img>255.] = 255.            
 output_img = output_img.astype(np.int8)
 img2 = Image.fromarray(output_img, 'RGB')
-img2.save('result.jpg')
+img2.save('output.png')
