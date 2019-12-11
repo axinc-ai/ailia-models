6cc2338ad3ef246cee5925a29c06beb8a992483c
sngyo <shingyo-chi_9731@keio.jp>
2019-12-11 12:34:36 +0900

    update README.md

--- dewarpnet/dewarpnet.py
+++ dewarpnet/dewarpnet.py
@@ -1,9 +1,8 @@
 import cv2
 import time
 import urllib.request
+import os
 import numpy as np
-
-# TODO remove
 
 import ailia
 
@@ -16,7 +15,21 @@ bm_model_path = bm_weight_path + ".prototxt"
 wc_model_path = wc_weight_path + ".prototxt"
 
 
-# TODO proper grid_sample implement
+rmt_chkpt = "https://storage.googleapis.com/ailia-models/dewarpnet/"
+
+# BM
+if not os.path.exists(bm_model_path):
+    urllib.request.urlretrieve(rmt_chkpt + bm_model_path, bm_model_path)
+if not os.path.exists(bm_weight_path):
+    urllib.request.urlretrieve(rmt_chkpt + bm_weight_path, bm_weight_path)
+
+# WC
+if not os.path.exists(wc_model_path):
+    urllib.request.urlretrieve(rmt_chkpt + wc_model_path, wc_model_path)
+if not os.path.exists(wc_weight_path):
+    urllib.request.urlretrieve(rmt_chkpt + wc_weight_path, wc_weight_path)
+
+
 def grid_sample(img, grid):
     height, width, c = img.shape
     output = np.zeros_like(img)
@@ -33,7 +46,6 @@ def grid_sample(img, grid):
 
 def unwarp(img, bm):
     w,h=img.shape[0],img.shape[1]
-    # bm = bm.transpose(0, 2, 2).transpose(2, 3)[0]
     bm = bm.transpose(1, 2, 0)
     bm0=cv2.blur(bm[:,:,0],(3,3))
     bm1=cv2.blur(bm[:,:,1],(3,3))
@@ -44,12 +56,6 @@ def unwarp(img, bm):
     res = grid_sample(img, bm)
     return res
 
-
-# TODO modify
-# if not os.path.exists(model_path):
-#    urllib.request.urlretrieve("https://storage.googleapis.com/ailia-models/hrnet_segmentation/" + model_path, model_path)
-# if not os.path.exists(weight_path):
-#    urllib.request.urlretrieve("https://storage.googleapis.com/ailia-models/hrnet_segmentation/" + weight_path, weight_path)
 
 wc_img_size = (256, 256)
 bm_img_size = (128, 128)
@@ -77,11 +83,11 @@ for i in range(1):
     bm_input = cv2.resize(pred_wc, bm_img_size).transpose(2, 0, 1)
     bm_input = np.expand_dims(bm_input, 0)
     outputs_bm = bm_net.predict(bm_input)[0]
+    uwpred = unwarp(img_org, outputs_bm)  # This is not on GPU!
     
     end = int(round(time.time() * 1000))
     print("ailia processing time {} ms".format(end-start))
 
-uwpred = unwarp(img_org, outputs_bm)
 cv2.imwrite('output.png', uwpred * 255)
     
 print('Successfully finished !')
