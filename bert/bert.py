3dbf53d6e61e12589532afe306484d751fd3b657
sngyo <shingyo-chi_9731@keio.jp>
2020-03-16 16:25:40 +0900

    UPD: masked word prediction script

--- bert/bert.py
+++ bert/bert.py
@@ -9,65 +9,62 @@ from pytorch_pretrained_bert import BertTokenizer
 import ailia
 
 
-weight_path = "bert-base-uncased.onnx"
-model_path = "bert-base-uncased.onnx.prototxt"
+NUM_PREDICT = 3
 
-rmt_ckpt = "https://storage.googleapis.com/ailia-models/bert_en/"
+WEIGHT_PATH = "bert-base-uncased.onnx"
+MODEL_PATH = "bert-base-uncased.onnx.prototxt"
 
-if not os.path.exists(model_path):
-    urllib.request.urlretrieve(rmt_ckpt + model_path, model_path)
-if not os.path.exists(weight_path):
-    urllib.request.urlretrieve(rmt_ckpt + weight_path, weight_path)
+RMT_CKPT = "https://storage.googleapis.com/ailia-models/bert_en/"
+
+if not os.path.exists(MODEL_PATH):
+    urllib.request.urlretrieve(RMT_CKPT + MODEL_PATH, MODEL_PATH)
+if not os.path.exists(WEIGHT_PATH):
+    urllib.request.urlretrieve(RMT_CKPT + WEIGHT_PATH, WEIGHT_PATH)
 
 
-def text2token(text):
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+def text2token(text, tokenizer):
     # convert a text to tokens which can be interpreted in BERT model
+    text = text.replace('_', '[MASK]')
     marked_text = "[CLS] " + text + " [SEP]"
     tokenized_text = tokenizer.tokenize(marked_text)
     indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+    masked_index = tokenized_text.index('[MASK]')
     segments_ids = [0] * len(tokenized_text)
 
     tokens_ts = torch.tensor([indexed_tokens]).numpy()
     segments_ts = torch.tensor([segments_ids]).numpy()
 
     # length fixed by 128 for now (it can be changeable?)
-    print(tokens_ts.shape)
     tokens_ts = np.pad(
         tokens_ts,
         [(0, 0), (0, 128-len(tokens_ts[0]))],
         'constant',
-        # constant_values=(0, 0)
     )
     segments_ts = np.pad(
         segments_ts,
         [(0, 0), (0, 128-len(segments_ts[0]))],
         'constant',
-        # constant_values=(0, 0)
     )
-    return tokens_ts, segments_ts
+    assert tokens_ts.shape == (1, 128)
+    assert segments_ts.shape == (1, 128)
+    return tokens_ts, segments_ts, masked_index
 
 
 def main():
-    # load data
-    # 1. DEBUG mode
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    # Prepare data
     dummy_input = np.ones((1, 128))
-    # dummy_inputs = np.array([dummy_input, dummy_input, dummy_input])
-
-    # 2. input sample sentence
-    # TODO how to implemenmt this ?
-    test_sentence = 'Who was Jim Henson ? Jim Henson was a puppeteer.'
-    tokens_ts, segments_ts = text2token(test_sentence)
+    test_sentence = 'I want to _ the car because it is cheap.'
+    tokens_ts, segments_ts, masked_index = text2token(test_sentence, tokenizer)
     input_data = np.array([tokens_ts, segments_ts])
-    print(f'[DEBUG] input shape: {input_data.shape}')
-
+    
     # net initialize
     env_id = ailia.get_gpu_environment_id()
-    print(env_id)
-    net = ailia.Net(model_path, weight_path, env_id=env_id)
+    print(f'env_id: {env_id}')
+    net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=env_id)
 
     # compute time
-    for i in range(1):
+    for i in range(5):
         start = int(round(time.time() * 1000))
         input_blobs = net.get_input_blob_list()
         for i, idx in enumerate(input_blobs):
@@ -82,10 +79,13 @@ def main():
         end = int(round(time.time() * 1000))
         print("ailia processing time {} ms".format(end-start))
 
-    print(f'[DEBUG] output length: {len(preds_ailia)}')
-    print(f'[DEBUG] output shape: {preds_ailia[0].shape}')
-    print(f'[DEBUG] output shape: {preds_ailia[1].shape}')
-
+    # Masked Word Prediction
+    predicted_indices = np.argsort(
+        preds_ailia[0][0][masked_index]
+    )[-NUM_PREDICT:][::-1]
+    predicted_tokens = tokenizer.convert_ids_to_tokens(predicted_indices)
+    print(f'predicted top {NUM_PREDICT} words: {predicted_tokens}')
+    
     print('Successfully finished!')
     
 
