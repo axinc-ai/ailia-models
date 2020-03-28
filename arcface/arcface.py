f4af100f53ec9e9f7bb17582085fcad763e69229
sngyo <shingyo-chi_9731@keio.jp>
2020-03-28 15:35:06 +0900

    UPD: refactoring arcface

--- arcface/arcface.py
+++ arcface/arcface.py
@@ -1,39 +1,56 @@
+import sys
 import time
-import os
-import urllib.request
+import argparse
 
-import cv2
 import numpy as np
 
 import ailia
 
+# import original modules
+sys.path.append('../util')
+from model_utils import check_and_download_models
+from image_utils import load_image
 
-# correct pair
-IMG_PATH_1 = 'correct_pair_1.jpg'
-IMG_PATH_2 = 'correct_pair_2.jpg'
-# IMG_PATH_2 = 'incorrect.jpg'
+
+# ======================
+# PARAMETERS
+# ======================
+IMG_PATH_1 = 'correct_pair_1.jpg'  # Base image
+IMG_PATH_2 = 'correct_pair_2.jpg'  # Correct Pair image
+IMG_PATH_3 = 'incorrect.jpg'     # Incorrect Pair image
 
 # the threshold was calculated by the `test_performance` function in `test.py`
 # of the original repository
 THRESHOLD = 0.25572845  
 
-
 WEIGHT_PATH = 'arcface.onnx'
 MODEL_PATH = 'arcface.onnx.prototxt'
-
-RMT_CKPT = "https://storage.googleapis.com/ailia-models/arcface/"
-
-if not os.path.exists(MODEL_PATH):
-    urllib.request.urlretrieve(RMT_CKPT + MODEL_PATH, MODEL_PATH)
-if not os.path.exists(WEIGHT_PATH):
-    urllib.request.urlretrieve(RMT_CKPT + WEIGHT_PATH, WEIGHT_PATH)
+REMOTE_PATH = "https://storage.googleapis.com/ailia-models/arcface/"
 
 
-def load_image(img_path):
+# ======================
+# Arguemnt Parser Config
+# ======================
+parser = argparse.ArguemntParser(
+    description='Determine if the person is the same from two facial images.'
+)
+parser.add_argument(
+    '-i', '--input', metavar='INPUT',
+    nargs='2',
+    default=[IMG_PATH_1, IMG_PATH_2],
+    help='Two iamge paths for calculating the face match'
+)
+args = parser.parse_args()
+
+
+# ======================
+# Utils
+# ======================
+def prepare_input_data(image_path):
     # (ref: https://github.com/ronghuaiyang/arcface-pytorch/issues/14)
     # use origin image and fliped image to infer,
     # and concat the feature as the final feature of the origin image.
-    image = cv2.imread(img_path, 0)
+    image = load_image(image_path, rgb=False, normalize_type='None')
     image = np.dstack((image, np.fliplr(image)))
     image = image.transpose((2, 0, 1))
     image = image[:, np.newaxis, :, :]
@@ -45,24 +62,33 @@ def load_image(img_path):
 def cosin_metric(x1, x2):
     return np.dot(x1, x2) / (np.linalg.norm(x1) * np.linalg.norm(x2))
 
-    
+
+# ======================
+# Main function
+# ======================
 def main():
+    # model files check and download
+    check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
+
+    # prepare input data
+    imgs_1 = prepare_input_data(args.input[0])
+    imgs_2 = prepare_input_data(args.input[1])
+    imgs = np.concatenate([imgs_1, imgs_2], axis=0)
+    
     # net initialize
     env_id = ailia.get_gpu_environment_id()
     print(f'env_id: {env_id}')
     net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=env_id)
 
-    imgs_1 = load_image(IMG_PATH_1)
-    imgs_2 = load_image(IMG_PATH_2)
-    imgs = np.concatenate([imgs_1, imgs_2], axis=0)
-    
-    # compute time
-    for i in range(1):
+    # compute execution time
+    print('Start inference...')
+    for i in range(5):
         start = int(round(time.time() * 1000))
         preds_ailia = net.predict(imgs)
         end = int(round(time.time() * 1000))
         print("ailia processing time {} ms".format(end - start))
 
+    # postprocessing
     fe_1 = np.concatenate([preds_ailia[0], preds_ailia[1]], axis=0)
     fe_2 = np.concatenate([preds_ailia[2], preds_ailia[3]], axis=0)
     sim = cosin_metric(fe_1, fe_2)
