3da43ee6577b3baada10a23dc3d36a64d7c3d20e
sngyo <shingyo-chi_9731@keio.jp>
2020-03-31 16:42:52 +0900

    UPD: BLAZEFACE video input mode & rename utils.py in order to avoid coflict util/utils.py

--- blazeface/blazeface.py
+++ blazeface/blazeface.py
@@ -5,9 +5,11 @@ import argparse
 import cv2
 
 import ailia
-from utils import *
+from blazeface_utils import *
+
 # import original modules
 sys.path.append('../util')
+from utils import check_file_existance
 from model_utils import check_and_download_models
 from image_utils import load_image
 from webcamera_utils import preprocess_frame
@@ -33,14 +35,15 @@ parser = argparse.ArgumentParser(
     description='BlazeFace is a fast and light-weight face detector.'
 )
 parser.add_argument(
-    '-i', '--input', metavar='IMAGEFILE_PATH',
+    '-i', '--input', metavar='IMAGE',
     default=IMAGE_PATH, 
     help='The input image path.'
 )
 parser.add_argument(
-    '-c', '--camera',
-    action='store_true',
-    help='Running the model with the webcam image as input.'
+    '-v', '--video', metavar='VIDEO',
+    default=None,
+    help='The input video path. ' +\
+         'If the VIDEO argument is set to 0, the webcam input will be used.'
 )
 parser.add_argument(
     '-s', '--savepath', metavar='SAVE_IMAGE_PATH',
@@ -98,10 +101,15 @@ def recognize_from_video():
     print(f'env_id: {env_id}')
     net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=env_id)
 
-    capture = cv2.VideoCapture(0)
-    if not capture.isOpened():
-        print("[ERROR] webcamera not found")
-        sys.exit(1)
+    if args.video == '0':
+        print('[INFO] Webcam mode is activated')
+        capture = cv2.VideoCapture(0)
+        if not capture.isOpened():
+            print("[ERROR] webcamera not found")
+            sys.exit(1)
+    else:
+        if check_file_existance(args.video):
+            capture = cv2.VideoCapture(args.video)        
     
     while(True):
         ret, frame = capture.read()
@@ -134,7 +142,7 @@ def main():
     # model files check and download
     check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
 
-    if args.camera:
+    if args.video is not None:
         # video mode
         recognize_from_video()
     else:
