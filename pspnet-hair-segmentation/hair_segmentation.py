b71463f343b8192a4efc7a64597d2be6babe7717
sngyo <shingyo-chi_9731@keio.jp>
2020-05-13 17:34:59 +0900

    FIX: model path name & UPD: refactoring pspnet-hair-segmentation

--- pspnet-hair-segmentation/hair_segmentation.py
+++ pspnet-hair-segmentation/hair_segmentation.py
@@ -1,65 +1,180 @@
-import os, urllib, time
+import sys
+import time
+import argparse
 
-import cv2
 import numpy as np
+import cv2
 
 import ailia
+# import original modules
+sys.path.append('../util')
+from webcamera_utils import preprocess_frame  # noqa: E402
+from image_utils import load_image  # noqa: E402
+from model_utils import check_and_download_models  # noqa: E402
+from utils import check_file_existance  # noqa: E402
 
 
-# FIXME [WARNING] crop (512, 512) is better than resize automatically
-img_path = 'test.jpg'
-width, height = 512, 512  # fixed when exporting model
+# ======================
+# Parameters
+# ======================
+WEIGHT_PATH = 'pspnet-hair-segmentation.onnx'
+MODEL_PATH = WEIGHT_PATH + '.prototxt'
+REMOTE_PATH =\
+    'https://storage.googleapis.com/ailia-models/pspnet-hair_segmentation/'
 
-# TODO add squeezenet version if necessary
-model_path = 'pspnet-hair-segmentation.onnx'
-weight_path = model_path + '.prototxt'
+IMAGE_PATH = 'test.jpg'
+SAVE_IMAGE_PATH = 'output.png'
+IMAGE_HEIGHT = 512
+IMAGE_WIDTH = 512
 
-# downloading
-rmt_ckpt =\
-    'https://storage.googleapis.com/ailia-models/pspnet-hair-segmentation/'
-if not os.path.exists(model_path):
-    print('model downloading...')
-    urllib.request.urlretrieve(rmt_ckpt + model_path, model_path)
-if not os.path.exists(weight_path):
-    urllib.request.urlretrieve(rmt_ckpt + weight_path, weight_path)
+
+# ======================
+# Arguemnt Parser Config
+# ======================
+parser = argparse.ArgumentParser(
+    description='Real-time hair segmentation model'
+)
+parser.add_argument(
+    '-i', '--input', metavar='IMAGE',
+    default=IMAGE_PATH,
+    help='The input image path.'
+)
+parser.add_argument(
+    '-v', '--video', metavar='VIDEO',
+    default=None,
+    help='The input video path. ' +
+         'If the VIDEO argument is set to 0, the webcam input will be used.'
+)
+parser.add_argument(
+    '-s', '--savepath', metavar='SAVE_IMAGE_PATH',
+    default=SAVE_IMAGE_PATH,
+    help='Save path for the output image.'
+)
+
+args = parser.parse_args()
+
+
+# ======================
+# Utils
+# ======================
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+
+def postprocess(src_img, preds_ailia):
+    pred = sigmoid(preds_ailia)[0][0]
+    mask = pred >= 0.5
+
+    mask_n = np.zeros((IMAGE_HEIGHT, IMAGE_WIDTH, 3))
+    mask_n[:, :, 0] = 255
+    mask_n[:, :, 0] *= mask
+
+    image_n = cv2.cvtColor(src_img, cv2.COLOR_RGB2BGR)
+
+    # discard padded area
+    h, w, _ = image_n.shape
+    delta_h = h - IMAGE_HEIGHT
+    delta_w = w - IMAGE_WIDTH
+
+    top = delta_h // 2
+    bottom = IMAGE_HEIGHT - (delta_h - top)
+    left = delta_w // 2
+    right = IMAGE_WIDTH - (delta_w - left)
+
+    mask_n = mask_n[top:bottom, left:right, :]
+    image_n = image_n * 0.5 + mask_n * 0.5
+    return image_n
+
+
+# ======================
+# Main functions
+# ======================
+def recognize_from_image():
+    # prepare input data
+    input_data = load_image(
+        args.input,
+        (IMAGE_HEIGHT, IMAGE_WIDTH),
+        normalize_type='ImageNet',
+        gen_input_ailia=True
+    )
+    src_img = load_image(
+        args.input,
+        (IMAGE_HEIGHT, IMAGE_WIDTH),
+        normalize_type='None'
+    )
+
+    # net initialize
+    env_id = ailia.get_gpu_environment_id()
+    print(f'env_id: {env_id}')
+    net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=env_id)
+
+    # compute execution time
+    for i in range(5):
+        start = int(round(time.time() * 1000))
+        preds_ailia = net.predict(input_data)
+        end = int(round(time.time() * 1000))
+        print(f'ailia processing time {end - start} ms')
+
+    # postprocessing
+    res_img = postprocess(src_img, preds_ailia)
+    cv2.imwrite(args.savepath, res_img)
+    print('Script finished successfully.')
+
+
+def recognize_from_video():
+    # net initialize
+    env_id = ailia.get_gpu_environment_id()
+    print(f'env_id: {env_id}')
+    net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=env_id)
+    
+    if args.video == '0':
+        print('[INFO] Webcam mode is activated')
+        capture = cv2.VideoCapture(0)
+        if not capture.isOpened():
+            print("[ERROR] webcamera not found")
+            sys.exit(1)
+    else:
+        if check_file_existance(args.video):
+            capture = cv2.VideoCapture(args.video)
+
+    while(True):
+        ret, frame = capture.read()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        if not ret:
+            continue
+
+        src_img, input_data = preprocess_frame(
+            frame,
+            IMAGE_HEIGHT,
+            IMAGE_WIDTH,
+            normalize_type='ImageNet'
+        )
+
+        src_img = cv2.resize(src_img, (IMAGE_WIDTH, IMAGE_HEIGHT))
+        src_img = cv2.cvtColor(src_img, cv2.COLOR_BGR2RGB)
+        
+        preds_ailia = net.predict(input_data)
+        
+        res_img = postprocess(src_img, preds_ailia)
+        cv2.imshow('frame', res_img / 255.0)
+        
+    capture.release()
+    cv2.destroyAllWindows()
+    print('Script finished successfully.')
 
 
 def main():
-    # create model
-    env_id = ailia.get_gpu_environment_id()
-    print("Environment mode: {} (-1: CPU, 1: GPU)".format(env_id))
-    net = ailia.Net(model_path, weight_path, env_id=env_id)
+    # model files check and download
+    check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
 
-    # image processing
-    org_img = cv2.imread(img_path) / 255.0
-    org_img = cv2.cvtColor(org_img, cv2.COLOR_BGR2RGB)
-
-    # image preprocessing
-    mean = [0.485, 0.456, 0.406]  # mean of ImageNet dataset
-    std = [0.229, 0.224, 0.225]  # std of ImageNet dataset
-    img = cv2.resize(org_img, (height, width))  # resize image
-
-    for i in range(3):  # normalize image
-        img[:, :, i] = (img[:, :, i] - mean[i]) / std[i]
-
-    # [x, y, channel] --> [1, channel, x, y]
-    img = np.expand_dims(np.rollaxis(img, 2, 0), axis=0) 
-    
-    # net.set_input_shape(img.shape)  # TODO remove
-
-    # compute time
-    for i in range(10):
-        start = int(round(time.time() * 1000))
-        pred = net.predict(img)
-        end = int(round(time.time() * 1000))
-        print(f"ailia processing time {end-start} ms")
-
-    # prepare mask
-    print(f'[DEBUG] {pred.shape}')
-    
-    # output saving
-    print('Successfully finished!')
+    if args.video is not None:
+        # video mode
+        recognize_from_video()
+    else:
+        # image mode
+        recognize_from_image()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
