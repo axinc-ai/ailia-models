4a937d91611e9330d16d28b43e6c2e4f5d23bd89
sngyo <shingyo-chi_9731@keio.jp>
2020-05-15 16:26:15 +0900

    UPD: facial feature refactoring benchmark

--- facial_feature/facial_feature.py
+++ facial_feature/facial_feature.py
@@ -12,14 +12,13 @@ import ailia
 
 # import original modules
 sys.path.append('../util')
-from utils import check_file_existance
-from model_utils import check_and_download_models
-from image_utils import load_image
-from webcamera_utils import preprocess_frame
+from utils import check_file_existance  # noqa: E402
+from model_utils import check_and_download_models  # noqa: E402
+from image_utils import load_image  # noqa: E402
+from webcamera_utils import preprocess_frame  # noqa: E402
 
 
 # TODO Upgrade Model
-
 # ======================
 # PARAMETERS
 # ======================
@@ -41,19 +40,25 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument(
     '-i', '--input', metavar='IMAGE',
-    default=IMAGE_PATH, 
+    default=IMAGE_PATH,
     help='The input image path.'
 )
 parser.add_argument(
     '-v', '--video', metavar='VIDEO',
     default=None,
-    help='The input video path. ' +\
+    help='The input video path. ' +
          'If the VIDEO argument is set to 0, the webcam input will be used.'
 )
 parser.add_argument(
     '-s', '--savepath', metavar='SAVE_IMAGE_PATH',
     default=SAVE_IMAGE_PATH,
     help='Save path for the output image.'
+)
+parser.add_argument(
+    '-b', '--benchmark',
+    action='store_true',
+    help='Running the inference on the same input 5 times ' +
+         'to measure execution performance. (Cannot be used in video mode)'
 )
 args = parser.parse_args()
 
@@ -88,15 +93,21 @@ def recognize_from_image():
     net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=env_id)
 
     # compute execution time
-    for i in range(5):
-        start = int(round(time.time() * 1000))
+    print('Start inference...')
+    if args.benchmark:
+        print('BENCHMARK mode')
+        for i in range(5):
+            start = int(round(time.time() * 1000))
+            preds_ailia = net.predict(img)[0]
+            end = int(round(time.time() * 1000))
+            print(f'\tailia processing time {end - start} ms')
+    else:
         preds_ailia = net.predict(img)[0]
-        end = int(round(time.time() * 1000))
-        print(f'ailia processing time {end - start} ms')
 
     # postprocess
     fig = gen_img_from_predsailia(img, preds_ailia)
     fig.savefig(args.savepath)
+    print('Script finished successfully.')
     
 
 def recognize_from_video():
@@ -113,8 +124,8 @@ def recognize_from_video():
             sys.exit(1)
     else:
         if check_file_existance(args.video):
-            capture = cv2.VideoCapture(args.video)        
-    
+            capture = cv2.VideoCapture(args.video)
+
     while(True):
         ret, frame = capture.read()
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -125,10 +136,10 @@ def recognize_from_video():
         input_image, input_data = preprocess_frame(
             frame, IMAGE_HEIGHT, IMAGE_WIDTH, data_rgb=False
         )
-        
+
         # inference
         preds_ailia = net.predict(input_data)[0]
-        
+
         # postprocessing
         fig = gen_img_from_predsailia(input_data, preds_ailia)
         fig.savefig('tmp.png')
@@ -154,4 +165,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()    
+    main()
