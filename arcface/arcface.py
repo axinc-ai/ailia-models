02885b3a96b79b14968faeb383d99b2c4ec45c90
sngyo <shingyo-chi_9731@keio.jp>
2020-05-15 13:49:11 +0900

    UPD: arcface benchmark mode

--- arcface/arcface.py
+++ arcface/arcface.py
@@ -9,10 +9,10 @@ import ailia
 
 # import original modules
 sys.path.append('../util')
-from utils import check_file_existance
-from model_utils import check_and_download_models
-from image_utils import load_image, draw_result_on_img
-from webcamera_utils import adjust_frame_size
+from webcamera_utils import adjust_frame_size  # noqa: E402
+from image_utils import load_image, draw_result_on_img  # noqa: E402
+from model_utils import check_and_download_models  # noqa: E402
+from utils import check_file_existance  # noqa: E402
 
 
 # ======================
@@ -29,7 +29,7 @@ IMAGE_WIDTH = 128
 
 # (IMAGE_HEIGHT * 2 * WEBCAM_SCALE, IMAGE_WIDTH * 2 * WEBCAM_SCALE)
 # Scale to determine the input size of the webcam
-WEBCAM_SCALE = 1.5  
+WEBCAM_SCALE = 1.5
 
 # the threshold was calculated by the `test_performance` function in `test.py`
 # of the original repository
@@ -52,9 +52,15 @@ parser.add_argument(
     '-v', '--video', metavar=('VIDEO', 'IMAGE'),
     nargs=2,
     default=None,
-    help='Determines whether the face in the video file specified by VIDEO ' +\
-         'and the face in the image file specified by IMAGE are the same. ' +\
-         'If the VIDEO argument is set to 0, the webcam input will be used.' 
+    help='Determines whether the face in the video file specified by VIDEO ' +
+         'and the face in the image file specified by IMAGE are the same. ' +
+         'If the VIDEO argument is set to 0, the webcam input will be used.'
+)
+parser.add_argument(
+    '-b', '--benchmark',
+    action='store_true',
+    help='Running the inference on the same input 5 times ' +
+         'to measure execution performance. (Cannot be used in video mode)'
 )
 args = parser.parse_args()
 
@@ -97,7 +103,7 @@ def compare_images():
     imgs_1 = prepare_input_data(args.inputs[0])
     imgs_2 = prepare_input_data(args.inputs[1])
     imgs = np.concatenate([imgs_1, imgs_2], axis=0)
-    
+
     # net initialize
     env_id = ailia.get_gpu_environment_id()
     print(f'env_id: {env_id}')
@@ -105,17 +111,21 @@ def compare_images():
 
     # compute execution time
     print('Start inference...')
-    for i in range(5):
-        start = int(round(time.time() * 1000))
+    if args.benchmark:
+        print('BENCHMARK mode')
+        for i in range(5):
+            start = int(round(time.time() * 1000))
+            preds_ailia = net.predict(imgs)
+            end = int(round(time.time() * 1000))
+            print(f'\tailia processing time {end - start} ms')
+    else:
         preds_ailia = net.predict(imgs)
-        end = int(round(time.time() * 1000))
-        print(f'ailia processing time {end - start} ms')
 
     # postprocessing
     fe_1 = np.concatenate([preds_ailia[0], preds_ailia[1]], axis=0)
     fe_2 = np.concatenate([preds_ailia[2], preds_ailia[3]], axis=0)
     sim = cosin_metric(fe_1, fe_2)
-    
+
     print(f'Similarity of ({args.inputs[0]}, {args.inputs[1]}) : {sim:.3f}')
     if THRESHOLD > sim:
         print('They are not the same face!')
@@ -150,7 +160,7 @@ def compare_image_and_video():
             break
         if not ret:
             continue
-        
+
         frame, resized_frame = adjust_frame_size(
             frame, IMAGE_HEIGHT, IMAGE_WIDTH
         )
@@ -171,16 +181,16 @@ def compare_image_and_video():
             texts=[f"Similarity: {sim:06.3f}", f"SAME FACE: {bool_sim}"]
         )
         cv2.imshow('frame', frame)
-        
+
     capture.release()
     cv2.destroyAllWindows()
     print('Script finished successfully.')
 
-    
+
 def main():
     # model files check and download
     check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
-    
+
     if args.video is None:
         # still image mode
         # comparing two images specified args.inputs
