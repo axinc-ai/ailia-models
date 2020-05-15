e15db40b16f7bad158a6187d45d5d9b09f6d9d75
sngyo <shingyo-chi_9731@keio.jp>
2020-05-15 16:07:37 +0900

    UPD: etl refactoring benchmark

--- etl/etl.py
+++ etl/etl.py
@@ -1,18 +1,16 @@
 import sys
 import time
+import codecs
 import argparse
 
 import cv2
 
-import codecs
-
 import ailia
 # import original modules
 sys.path.append('../util')
-from utils import check_file_existance
-from model_utils import check_and_download_models
-from image_utils import load_image
-from webcamera_utils import adjust_frame_size
+from utils import check_file_existance   # noqa: E402
+from model_utils import check_and_download_models  # noqa: E402
+from webcamera_utils import adjust_frame_size  # noqa: E402
 
 
 # ======================
@@ -38,14 +36,20 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument(
     '-i', '--input', metavar='IMAGEFILE_PATH',
-    default=IMAGE_PATH, 
+    default=IMAGE_PATH,
     help='The input image path.'
 )
 parser.add_argument(
     '-v', '--video', metavar='VIDEO',
     default=None,
-    help='The input video path. ' +\
+    help='The input video path. ' +
          'If the VIDEO argument is set to 0, the webcam input will be used.'
+)
+parser.add_argument(
+    '-b', '--benchmark',
+    action='store_true',
+    help='Running the inference on the same input 5 times ' +
+         'to measure execution performance. (Cannot be used in video mode)'
 )
 args = parser.parse_args()
 
@@ -54,9 +58,9 @@ args = parser.parse_args()
 # Utils
 # ======================
 def preprocess_image(img):
-    if img.shape[2] == 3 :
+    if img.shape[2] == 3:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
-    elif img.shape[2] == 1 : 
+    elif img.shape[2] == 1:
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGRA)
     img = cv2.bitwise_not(img)
     return img
@@ -70,7 +74,7 @@ def recognize_from_image():
     etl_word = codecs.open(ETL_PATH, 'r', 'utf-8').readlines()
     img = cv2.imread(args.input, cv2.IMREAD_UNCHANGED)
     img = preprocess_image(img)
-    
+
     # net initialize
     env_id = ailia.get_gpu_environment_id()
     print(f'env_id: {env_id}')
@@ -83,26 +87,31 @@ def recognize_from_image():
     )
 
     # compute execution time
-    for i in range(5):
-        start = int(round(time.time() * 1000))
+    print('Start inference...')
+    if args.benchmark:
+        print('BENCHMARK mode')
+        for i in range(5):
+            start = int(round(time.time() * 1000))
+            classifier.compute(img, MAX_CLASS_COUNT)
+            end = int(round(time.time() * 1000))
+            print(f'\tailia processing time {end - start} ms')
+    else:
         classifier.compute(img, MAX_CLASS_COUNT)
-        end = int(round(time.time() * 1000))
-        print(f'ailia processing time {end - start} ms')
 
     # get result
     count = classifier.get_class_count()
     print(f'class_count: {count}')
 
-    for idx in range(count) :
+    for idx in range(count):
         print(f"+ idx={idx}")
         info = classifier.get_class(idx)
-        print(f"  category={info.category} [ {etl_word[info.category]} ]" )
+        print(f"  category={info.category} [ {etl_word[info.category]} ]")
         print(f"  prob={info.prob}")
 
 
 def recognize_from_video():
     etl_word = codecs.open(ETL_PATH, 'r', 'utf-8').readlines()
-    
+
     # net initialize
     env_id = ailia.get_gpu_environment_id()
     print(f'env_id: {env_id}')
@@ -122,7 +131,7 @@ def recognize_from_video():
             sys.exit(1)
     else:
         if check_file_existance(args.video):
-            capture = cv2.VideoCapture(args.video)        
+            capture = cv2.VideoCapture(args.video)
 
     while(True):
         ret, frame = capture.read()
@@ -133,28 +142,28 @@ def recognize_from_video():
 
         in_frame, frame = adjust_frame_size(frame, IMAGE_HEIGHT, IMAGE_WIDTH)
         frame = preprocess_image(frame)
-        
+
         # inference
         # compute execution time
         classifier.compute(frame, MAX_CLASS_COUNT)
-        
+
         # get result
         count = classifier.get_class_count()
 
         print('==============================================================')
         print(f'class_count: {count}')
-        for idx in range(count) :
+        for idx in range(count):
             print(f"+ idx={idx}")
             info = classifier.get_class(idx)
-            print(f"  category={info.category} [ {etl_word[info.category]} ]" )
+            print(f"  category={info.category} [ {etl_word[info.category]} ]")
             print(f"  prob={info.prob}")
         cv2.imshow('frame', in_frame)
         time.sleep(SLEEP_TIME)
-            
+
     capture.release()
     cv2.destroyAllWindows()
     print('Script finished successfully.')
-            
+
 
 def main():
     # model files check and download
