d23d630f486644daffa371b745885660954c5fc4
sngyo <shingyo-chi_9731@keio.jp>
2020-05-15 16:47:44 +0900

    UPD: hair_segmentation refactoring benchmark

--- hair_segmentation/hair_segmentation.py
+++ hair_segmentation/hair_segmentation.py
@@ -8,10 +8,10 @@ import cv2
 import ailia
 # import original modules
 sys.path.append('../util')
-from utils import check_file_existance
-from model_utils import check_and_download_models
-from image_utils import load_image
-from webcamera_utils import adjust_frame_size
+from utils import check_file_existance  # noqa: E402
+from model_utils import check_and_download_models  # noqa: E402
+from image_utils import load_image  # noqa: E402
+from webcamera_utils import adjust_frame_size  # noqa: E402
 
 
 # ======================
@@ -31,19 +31,19 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument(
     '-i', '--input', metavar='IMAGE',
-    default=IMAGE_PATH, 
+    default=IMAGE_PATH,
     help='The input image path.'
 )
 parser.add_argument(
     '-v', '--video', metavar='VIDEO',
     default=None,
-    help='The input video path. ' +\
+    help='The input video path. ' +
          'If the VIDEO argument is set to 0, the webcam input will be used.'
 )
 parser.add_argument(
     '-n', '--normal',
     action='store_true',
-    help='By default, the optimized model is used, but with this option, ' +\
+    help='By default, the optimized model is used, but with this option, ' +
     'you can switch to the normal (not optimized) model'
 )
 parser.add_argument(
@@ -51,7 +51,12 @@ parser.add_argument(
     default=SAVE_IMAGE_PATH,
     help='Save path for the output image.'
 )
-
+parser.add_argument(
+    '-b', '--benchmark',
+    action='store_true',
+    help='Running the inference on the same input 5 times ' +
+         'to measure execution performance. (Cannot be used in video mode)'
+)
 args = parser.parse_args()
 
 
@@ -102,18 +107,24 @@ def recognize_from_image():
     print(f'env_id: {env_id}')
     net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=env_id)
     net.set_input_shape(input_data.shape)
-    
+
     # compute execution time
-    for i in range(5):
-        start = int(round(time.time() * 1000))
+    print('Start inference...')
+    if args.benchmark:
+        print('BENCHMARK mode')
+        for i in range(5):
+            start = int(round(time.time() * 1000))
+            preds_ailia = net.predict(input_data)
+            end = int(round(time.time() * 1000))
+            print(f'\tailia processing time {end - start} ms')
+    else:
         preds_ailia = net.predict(input_data)
-        end = int(round(time.time() * 1000))
-        print(f'ailia processing time {end - start} ms')
 
     # postprocessing
     pred = preds_ailia.reshape((IMAGE_HEIGHT, IMAGE_WIDTH))
     dst = transfer(src_img, pred)
     cv2.imwrite(args.savepath, dst)
+    print('Script finished successfully.')
 
 
 def recognize_from_video():
@@ -131,8 +142,8 @@ def recognize_from_video():
             sys.exit(1)
     else:
         if check_file_existance(args.video):
-            capture = cv2.VideoCapture(args.video)        
-    
+            capture = cv2.VideoCapture(args.video)
+
     while(True):
         ret, frame = capture.read()
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -149,7 +160,7 @@ def recognize_from_video():
         if not flag_set_shape:
             net.set_input_shape(input_data.shape)
             flag_set_shape = True
-        
+
         preds_ailia = net.predict(input_data)
         pred = preds_ailia.reshape((IMAGE_HEIGHT, IMAGE_WIDTH))
         dst = transfer(input_image, pred)
