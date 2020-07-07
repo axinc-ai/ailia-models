1cdf3cd2186a72da83bf968653b15fe328757d34
客野一樹 <kyakuno@axell.co.jp>
2020-07-07 11:38:43 +0900

    Save output image

--- centernet/centernet.py
+++ centernet/centernet.py
@@ -28,8 +28,6 @@ REMOTE_PATH = 'https://storage.googleapis.com/ailia-models/centernet/'
 
 IMAGE_PATH = 'couple.jpg'
 SAVE_IMAGE_PATH = 'output.png'
-IMAGE_HEIGHT = 512  # for video mode
-IMAGE_WIDTH = 512  # for video mode
 
 COCO_CATEGORY = [
     "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train",
@@ -47,8 +45,6 @@ COCO_CATEGORY = [
     "scissors", "teddy bear", "hair drier", "toothbrush"
 ]
 THRESHOLD = 0.3 #Threshold for filteing for filtering (from 0.0 to 1.0)
-IOU = 0.45
-KEEP_PER_CLASS = 10
 K_VALUE = 40    #K value for topK function
 
 # ======================
@@ -84,35 +80,6 @@ args = parser.parse_args()
 # ======================
 # Secondaty Functions
 # ======================
-
-def nms(dets, thresh):
-    """Pure Python NMS baseline."""
-    x1 = dets[:, 0]
-    y1 = dets[:, 1]
-    x2 = dets[:, 2]
-    y2 = dets[:, 3]
-    scores = dets[:, 4]
-
-    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
-    order = scores.argsort()[::-1]
-
-    keep = []
-    while order.size > 0:
-        i = order[0]
-        keep.append(i)
-        xx1 = np.maximum(x1[i], x1[order[1:]])
-        yy1 = np.maximum(y1[i], y1[order[1:]])
-        xx2 = np.minimum(x2[i], x2[order[1:]])
-        yy2 = np.minimum(y2[i], y2[order[1:]])
-
-        w = np.maximum(0.0, xx2 - xx1 + 1)
-        h = np.maximum(0.0, yy2 - yy1 + 1)
-        inter = w * h
-        ovr = inter / (areas[i] + areas[order[1:]] - inter)
-
-        inds = np.where(ovr <= thresh)[0]
-        order = order[inds + 1]
-    return keep
 
 #def preprocess(img, resize=512, rgb_means=(104,117,123), swap=(2, 0, 1)):
 #    interp_method = cv2.INTER_LINEAR
@@ -175,62 +142,11 @@ def detect_objects(org_img, net):
         xmax = min(org_img.shape[1], xmax)
         ymax = min(org_img.shape[0], ymax)
 
-        # Map label_ids to names, if we have the map
-        label = COCO_CATEGORY[int(det[5])]# if args.label_file else ''
-        color = COLORS[int(det[5])]#(min(det[5] * 10 + 150, 255), min(det[5] * 4 + 150, 255), min(det[5] * 23 + 150, 255))
-        detection_description = label + ' | ' +str(round(det[4] * 100, 1)) + ' %'
-        print('\t' + detection_description)
-
-        # Draw bounding box
-        cv2.rectangle(org_img, (xmin, ymin), (xmax, ymax), color, 2)
-
-        # Draw label
-        (text_width, text_height) = cv2.getTextSize(detection_description, font, fontScale=font_scale, thickness=1)[0]
-        cv2.rectangle(org_img, ((xmin-2, ymin-text_height-4)), (xmin+text_width+2, ymin), color, cv2.FILLED)
-        cv2.putText(org_img, detection_description, (xmin + 2, ymin - 2), font, fontScale=font_scale, color=(0, 0, 0), thickness=1)
+        boxes.append([xmin,ymin,xmax,ymax])
+        scores.append(det[4])
+        cls_inds.append(det[5])
 
     return boxes, scores, cls_inds
-
-
-
-    # get sizes for posterior rescaling
-    h, w, _ = img.shape
-    scale = np.asarray([w,h,w,h])
-    
-    # initial preprocesses
-    img = preprocess(img)
-    
-    # feedforward
-    boxes, scores = detector.predict({'input.1': img})
-
-    boxes = boxes[0]
-    scores = scores[0]
-    allboxes = []
-    
-    # filter boxes for every class
-    for j in range(1, len(COCO_CATEGORY)):
-        inds = np.where(scores[:,j] > THRESHOLD)[0]
-        if len(inds) == 0:
-            continue
-        c_bboxes = boxes[inds]
-        c_scores = scores[inds, j]
-        c_dets = np.hstack((c_bboxes, c_scores[:, np.newaxis])).astype(np.float32, copy=False)
-        # rank ordered iou
-        keep = nms(c_dets, IOU) #min_thresh, device_id=0 if cfg.test_cfg.cuda else None)
-        keep = keep[:KEEP_PER_CLASS]
-        c_dets = c_dets[keep, :]
-        allboxes.extend([_.tolist()+[j] for _ in c_dets])
-    
-    
-    if len(allboxes)>0:
-        allboxes = np.array(allboxes)    
-        # split boxes and scores
-        boxes = allboxes[:,:4] * scale
-        scores = allboxes[:,4]
-        cls_inds = allboxes[:,5]
-        return boxes, scores, cls_inds
-    else:
-        return [], [], []
     
 def recognize_from_image(filename, detector):
     # load input image
@@ -256,13 +172,13 @@ def recognize_from_image(filename, detector):
     
     # show image 
     im2show = draw_detection(img, boxes, scores, cls_inds)
-    
+
+    cv2.imwrite(args.savepath, im2show)
     print('Script finished successfully.')
     
-    cv2.imshow('demo', im2show)
-    cv2.waitKey(5000)
-    cv2.destroyAllWindows()
-    
+    #cv2.imshow('demo', im2show)
+    #cv2.waitKey(5000)
+    #cv2.destroyAllWindows()
     
 def recognize_from_video(video, detector):
  
@@ -285,7 +201,6 @@ def recognize_from_video(video, detector):
 
         boxes, scores, cls_inds = detect_objects(img, detector)
 
-#         detector.compute(img, THRESHOLD, IOU)
         boxes, scores, cls_inds = detect_objects(img, detector)
         img = draw_detection(img, boxes, scores, cls_inds)
         cv2.imshow('frame', img)
