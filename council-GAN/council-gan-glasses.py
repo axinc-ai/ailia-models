b2f065a2dd85969c44740086cb422b667d5dc5b4
prolleytroblems <oxiloxil@live.com>
2020-07-27 17:58:13 +0900

    add yolo face recognition

--- council-GAN/council-gan-glasses.py
+++ council-GAN/council-gan-glasses.py
@@ -10,18 +10,18 @@ import ailia
 import onnxruntime
 
 # import original modules
-sys.path.append('../util')
+sys.path.append('../ailia-models/util')
 from utils import check_file_existance  # noqa: E402
 from model_utils import check_and_download_models  # noqa: E402
-
+from yolo_face import FaceLocator
 
 # ======================
 # PARAMETERS
 # ======================
 
 WEIGHT_PATH = 'councilGAN-glasses.onnx'
-MODEL_PATH = 'councilGAN-glasses.opt.onnx.prototxt'
-REMOTE_PATH = "https://storage.googleapis.com/ailia-models/councilGAN-glasses/"
+MODEL_PATH = 'councilGAN-glasses.onnx.prototxt'
+REMOTE_PATH = "https://storage.googleapis.com/ailia-models/council-gan/"
 
 IMAGE_PATH = 'sample.jpg'
 SAVE_IMAGE_PATH = 'output.png'
@@ -57,6 +57,16 @@ parser.add_argument(
          'to measure execution performance. (Cannot be used in video mode)'
 )
 parser.add_argument(
+    '-f', '--face_recognition',
+    action='store_true',
+    help='Run face recognition with yolo v3'
+)
+parser.add_argument(
+    '-d', '--dilation', metavar='DILATION',
+    default=1,
+    help='Dilation value for face recognition image size'
+)
+parser.add_argument(
     '-o', '--onnx',
     action='store_true',
     help='Run on ONNXruntime instead of Ailia'
@@ -81,9 +91,51 @@ def preprocess(image):
     return image.astype(np.float32)   
 
 def center_crop(image):
+#     Crop the image around the center to make square
     shape = image.shape[0:2]
     size = min(shape)
     return image[(shape[0]-size)//2:(shape[0]+size)//2, (shape[1]-size)//2:(shape[1]+size)//2, ...]
+
+def square_coords(coords, dilation=1.0):
+#     Make coordinates square for the network with dimension equal to the longer side * dilation, same center
+    top, left, bottom, right = coords
+    w = right-left
+    h = bottom-top
+    
+    dim = 1 if w>h else 0
+    
+    new_size = int(max(w, h)*dilation)
+    change_short = new_size - min(w, h)
+    change_long = new_size - max(w, h)
+    
+    out = list(coords)
+    out[0+dim] -= change_long//2
+    out[1-dim] -= change_short//2
+    out[2+dim] += change_long//2
+    out[3-dim] += change_short//2
+    
+    return out
+
+def get_slice(image, coords):
+    padded_slice = np.zeros((coords[2]-coords[0], coords[3]-coords[1], 3))
+    new_coords = np.zeros((4), dtype=np.int16)
+    padded_coords = np.zeros((4), dtype=np.int16)
+#     limit coords to the shape of the image, and get new coordinates relative to new padded shape for later replacement
+    for dim in [0,1]:
+        new_coords[0+dim] = 0 if coords[0+dim]<0 else coords[0+dim]
+        new_coords[2+dim] = image.shape[0+dim] if coords[2+dim]>image.shape[0+dim] else coords[2+dim]
+        padded_coords[0+dim] = new_coords[0+dim]-coords[0+dim]
+        padded_coords[2+dim] = padded_coords[0+dim] + new_coords[2+dim] - new_coords[0+dim]
+        
+#     get the new correct slice and put it in padded array
+    image_slice = image[sliceify(new_coords)]
+    padded_slice[sliceify(padded_coords)] = image_slice 
+    
+    return padded_slice, new_coords, padded_coords
+
+def sliceify(coords):
+#     Turn a list of (top, left, bottom right) into slices for indexing
+    return slice(coords[0], coords[2]), slice(coords[1], coords[3])
 
 # ======================
 # Postprocessing functions
@@ -95,65 +147,87 @@ def postprocess_image(image):
     out = np.clip(final_image, 0, 255).astype(np.uint8)
     return out
 
+def replace_face(img, replacement, coords):
+    img = img.copy()
+    img[sliceify(coords)] = cv2.resize(replacement, (coords[3]-coords[1], coords[2]-coords[0]))
+    return img
+
 # ======================
 # Main functions
 # ======================
-def process_image():
-    # prepare input data
-    img = preprocess(cv2.imread(args.input))
-    
-    # net initialize
+def transform_image():
+#     Full transormation on single image from filepath
+    image = cv2.imread(args.input)
     env_id = ailia.get_gpu_environment_id()
     print(f'env_id: {env_id}')
+    
     if not args.onnx:
         net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=env_id)
+    else:
+        net = onnxruntime.InferenceSession('councilGAN-glasses.onnx')
+    
+    if args.face_recognition:
+        locator = FaceLocator()
+    else:
+        locator = None
+        
+    if args.benchmark:
+        print('BENCHMARK mode')
+        for i in range(5):
+            start = int(round(time.time() * 1000))
 
-        # inference
+            out_image = process_frame(net, locator, image)
+            
+            end = int(round(time.time() * 1000))
+            print(f'\tailia processing time {end - start} ms')
+
+    else:
+        out_image = process_frame(net, locator, image)
+    
+    cv2.imwrite(args.savepath, out_image[...,::-1])
+    return True
+
+
+def process_frame(net, locator, image):
+#     Process a single frame with preloaded network and locator
+    if args.face_recognition:
+#         Run with face recognition using yolo
+        out_image = image.copy()[...,::-1]
+#         Get face coordinates with yolo
+        face_coords = locator.get_faces(image[...,::-1])
+#         Replace each set of coordinates with its glass-less transformation
+        for coords in face_coords:
+            coords = square_coords(coords, dilation=float(args.dilation))
+            
+            image_slice, coords, padded_coords = get_slice(image, coords)
+            
+            processed_slice = process_array(net, preprocess(image_slice))
+            processed_slice = processed_slice[sliceify(padded_coords)]
+            out_image = replace_face(out_image, processed_slice, coords)
+        
+    else:
+        out_image = process_array(net, preprocess(image))
+        
+    return out_image
+ 
+def process_array(net, img):
+#    Apply network to a correctly scaled and centered image 
+    if not args.onnx:
         print('Start inference...')
-        if args.benchmark:
-            print('BENCHMARK mode')
-            for i in range(5):
-                data = [img[None,...]] 
-                start = int(round(time.time() * 1000))
-                preds_ailia = net.predict(data)[0][0]
-                end = int(round(time.time() * 1000))
-                print(f'\tailia processing time {end - start} ms')
-        else:
-            data = [img[None,...]] 
-            preds_ailia = net.predict(data)
-            preds_ailia = postprocess_image(preds_ailia[0][0])
+        preds_ailia = postprocess_image(net.predict(img)[0][0])
     else:
         # teporary onnxruntime mode
-        sess = onnxruntime.InferenceSession(WEIGHT_PATH)
-        # inference
         print('Start inference in onnxruntime mode...')
-        if args.benchmark:
-            print('BENCHMARK mode')
-            for i in range(5):
-                start = int(round(time.time() * 1000))
+        inputs = [i.name for i in net.get_inputs()]
+        outputs = [o.name for o in net.get_outputs()]
 
-                inputs = [i.name for i in sess.get_inputs()]
-                outputs = [o.name for o in sess.get_outputs()]
+        data = [img[None,...]] 
+        out = net.run(outputs, {i: data for i, data in zip(inputs, data)})
 
-                data = [img[None,...]] 
-                out = sess.run(outputs, {i: data for i, data in zip(inputs, data)})
-
-                preds_ailia = postprocess_image(out[0][0])
-                end = int(round(time.time() * 1000))
-                print(f'\tailia processing time {end - start} ms')
-        else:
-            inputs = [i.name for i in sess.get_inputs()]
-            outputs = [o.name for o in sess.get_outputs()]
-
-            data = [img[None,...]] 
-            out = sess.run(outputs, {i: data for i, data in zip(inputs, data)})
-
-            preds_ailia = postprocess_image(out[0][0])
-
-    cv2.imwrite(args.savepath, preds_ailia[...,::-1])
-    print('Script finished successfully.')
-    
-
+        preds_ailia = postprocess_image(out[0][0])
+            
+    return preds_ailia
+          
 def process_video():
     # net initialize
     env_id = ailia.get_gpu_environment_id()
@@ -163,6 +237,11 @@ def process_video():
     else:
         net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=env_id)
 
+    if args.face_recognition:
+        locator = FaceLocator()
+    else:
+        locator = None
+        
     if args.video == '0':
         print('[INFO] Webcam mode is activated')
         capture = cv2.VideoCapture(0)
@@ -180,20 +259,9 @@ def process_video():
         if not ret:
             continue
             
-        img = preprocess(frame)
-        
-        if args.onnx:
-            inputs = [i.name for i in net.get_inputs()]
-            outputs = [o.name for o in net.get_outputs()]
-
-            data = [img[None,...]] 
-            out = net.run(outputs, {i: data for i, data in zip(inputs, data)})
-
-            preds_ailia = postprocess_image(out[0][0])
-        else:
-            preds_ailia = postprocess_image(net.predict(img)[0][0])
-
-        cv2.imshow('frame', preds_ailia[...,::-1])
+        img = process_frame(net, locator, frame)
+       
+        cv2.imshow('frame', img[...,::-1])
 
     capture.release()
     cv2.destroyAllWindows()
@@ -209,8 +277,9 @@ def main():
         process_video()
     else:
         # image mode
-        process_image()
+        transform_image()
 
 
 if __name__ == '__main__':
     main()
+    print('Script finished successfully.')
