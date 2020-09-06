2a778e7bc5573c59fb2e48606e6e7b4b1766a2f3
satoshi.ooe <ooe1123@gmail.com>
2020-09-06 10:37:14 +0900

    Implement yolov4  with onnxruntime

---
+++ yolov4/yolov4.py
@@ -0,0 +1,241 @@
+import sys
+import time
+import argparse
+
+import numpy as np
+import cv2
+
+import ailia
+import onnxruntime
+
+# import original modules
+sys.path.append('../util')
+from utils import check_file_existance  # noqa: E402
+from model_utils import check_and_download_models  # noqa: E402
+from webcamera_utils import adjust_frame_size  # noqa: E402C
+from detector_utils import plot_results#, load_image  # noqa: E402C
+from image_utils import load_image
+
+import yolov4_utils
+
+# ======================
+# Parameters
+# ======================
+WEIGHT_PATH = 'yolov4.onnx'
+MODEL_PATH = 'yolov4.onnx.prototxt'
+REMOTE_PATH = 'https://storage.googleapis.com/ailia-models/yolov3/'
+
+IMAGE_PATH = 'couple.jpg'
+SAVE_IMAGE_PATH = 'output.png'
+
+COCO_CATEGORY = [
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train",
+    "truck", "boat", "traffic light", "fire hydrant", "stop sign",
+    "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+    "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+    "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
+    "sports ball", "kite", "baseball bat", "baseball glove", "skateboard",
+    "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork",
+    "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange",
+    "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair",
+    "couch", "potted plant", "bed", "dining table", "toilet", "tv",
+    "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave",
+    "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase",
+    "scissors", "teddy bear", "hair drier", "toothbrush"
+]
+THRESHOLD = 0.4
+IOU = 0.45
+DETECTION_WIDTH = 416
+IMAGE_HEIGHT = 416
+IMAGE_WIDTH = 416
+
+# ======================
+# Arguemnt Parser Config
+# ======================
+parser = argparse.ArgumentParser(
+    description='Yolov4 model'
+)
+parser.add_argument(
+    '-i', '--input', metavar='IMAGE',
+    default=IMAGE_PATH,
+    help='The input image path.'
+)
+parser.add_argument(
+    '-v', '--video', metavar='VIDEO',
+    default=None,
+    help='The input video path. ' +
+         'If the VIDEO argument is set to 0, the webcam input will be used.'
+)
+parser.add_argument(
+    '-s', '--savepath', metavar='SAVE_IMAGE_PATH',
+    default=SAVE_IMAGE_PATH,
+    help='Save path for the output image.'
+)
+parser.add_argument(
+    '-b', '--benchmark',
+    action='store_true',
+    help='Running the inference on the same input 5 times ' +
+         'to measure execution performance. (Cannot be used in video mode)'
+)
+parser.add_argument(
+    '-dw', '--detection_width',
+    default=DETECTION_WIDTH,
+    help='The detection width and height for yolo. (default: 416)'
+)
+parser.add_argument(
+    '--ailia',
+    action='store_true',
+    help='execute ailia version.'
+)
+args = parser.parse_args()
+
+
+# ======================
+# Main functions
+# ======================
+def recognize_from_image():
+    # prepare input data
+    # img = load_image(args.input)
+    img = load_image(
+        args.input,
+        (IMAGE_HEIGHT, IMAGE_WIDTH),
+        normalize_type='None',
+    )
+    img = np.transpose(img, [2, 0, 1])
+    img = img.astype(np.float32) / 255
+    img = np.expand_dims(img, 0)
+    print(f'input image shape: {img.shape}')
+
+    # net initialize
+    if args.ailia:
+        env_id = ailia.get_gpu_environment_id()
+        print(f'env_id: {env_id}')
+        detector = ailia.Detector(
+            MODEL_PATH,
+            WEIGHT_PATH,
+            len(COCO_CATEGORY),
+            format=ailia.NETWORK_IMAGE_FORMAT_RGB,
+            channel=ailia.NETWORK_IMAGE_CHANNEL_FIRST,
+            range=ailia.NETWORK_IMAGE_RANGE_U_FP32,
+            algorithm=ailia.DETECTOR_ALGORITHM_YOLOV3,
+            env_id=env_id
+        )
+        if int(args.detection_width) != DETECTION_WIDTH:
+            detector.set_input_shape(int(args.detection_width), int(args.detection_width))
+    else:
+        session = onnxruntime.InferenceSession(WEIGHT_PATH)
+        print(session.get_providers())
+
+    # inferece
+    print('Start inference...')
+    if args.ailia:
+        if args.benchmark:
+            print('BENCHMARK mode')
+            for i in range(5):
+                start = int(round(time.time() * 1000))
+                detector.compute(img, THRESHOLD, IOU)
+                end = int(round(time.time() * 1000))
+                print(f'\tailia processing time {end - start} ms')
+        else:
+            detector.compute(img, THRESHOLD, IOU)
+
+        # plot result
+        res_img = plot_results(detector, img, COCO_CATEGORY)
+    else:
+        input_name = session.get_inputs()[0].name
+        output_box_array = session.get_outputs()[0].name
+        output_confs = session.get_outputs()[1].name
+        output = session.run([output_box_array, output_confs],
+                             {input_name: img})
+
+        boxes = yolov4_utils.post_processing(img, THRESHOLD, IOU, output)
+
+        # plot result
+        img = cv2.imread(args.input)
+        res_img = yolov4_utils.plot_results(img, boxes[0], COCO_CATEGORY)
+        # res_img = yolov4_utils.plot_boxes_cv2(img, boxes[0], class_names=COCO_CATEGORY)
+
+    # plot result
+    cv2.imwrite(args.savepath, res_img)
+    print('Script finished successfully.')
+
+
+def recognize_from_video():
+    # net initialize
+    """
+    env_id = ailia.get_gpu_environment_id()
+    print(f'env_id: {env_id}')
+    detector = ailia.Detector(
+        MODEL_PATH,
+        WEIGHT_PATH,
+        len(COCO_CATEGORY),
+        format=ailia.NETWORK_IMAGE_FORMAT_RGB,
+        channel=ailia.NETWORK_IMAGE_CHANNEL_FIRST,
+        range=ailia.NETWORK_IMAGE_RANGE_U_FP32,
+        algorithm=ailia.DETECTOR_ALGORITHM_YOLOV3,
+        env_id=env_id
+    )
+    if int(args.detection_width)!=DETECTION_WIDTH:
+        detector.set_input_shape(int(args.detection_width),int(args.detection_width))
+    """
+    session = onnxruntime.InferenceSession(WEIGHT_PATH)
+
+    if args.video == '0':
+        print('[INFO] Webcam mode is activated')
+        capture = cv2.VideoCapture(0)
+        if not capture.isOpened():
+            print("[ERROR] webcamera not found")
+            sys.exit(1)
+    else:
+        if check_file_existance(args.video):
+            capture = cv2.VideoCapture(args.video)
+
+    input_name = session.get_inputs()[0].name
+    output_box_array = session.get_outputs()[0].name
+    output_confs = session.get_outputs()[1].name
+    while (True):
+        ret, frame = capture.read()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        if not ret:
+            continue
+
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+        img = cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT))
+        img = np.transpose(img, [2, 0, 1])
+        img = img.astype(np.float32) / 255
+        img = np.expand_dims(img, 0)
+
+        output = session.run([output_box_array, output_confs],
+                             {input_name: img})
+
+        boxes = yolov4_utils.post_processing(img, THRESHOLD, IOU, output)
+        """
+        detector.compute(img, THRESHOLD, IOU)
+        res_img = plot_results(detector, frame, COCO_CATEGORY, False)
+        """
+        res_img = yolov4_utils.plot_results(frame, boxes[0], COCO_CATEGORY)
+        cv2.imshow('frame', res_img)
+
+    capture.release()
+    cv2.destroyAllWindows()
+    print('Script finished successfully.')
+
+
+def main():
+    # model files check and download
+    # check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
+
+    if not args.ailia:
+        print(onnxruntime.get_device())
+
+    if args.video is not None:
+        # video mode
+        recognize_from_video()
+    else:
+        # image mode
+        recognize_from_image()
+
+
+if __name__ == '__main__':
+    main()
---
+++ yolov4/yolov4_utils.py
@@ -0,0 +1,209 @@
+import time
+import math
+import numpy as np
+import cv2
+
+
+def nms_cpu(boxes, confs, nms_thresh=0.5, min_mode=False):
+    # print(boxes.shape)
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+
+    areas = (x2 - x1) * (y2 - y1)
+    order = confs.argsort()[::-1]
+
+    keep = []
+    while order.size > 0:
+        idx_self = order[0]
+        idx_other = order[1:]
+
+        keep.append(idx_self)
+
+        xx1 = np.maximum(x1[idx_self], x1[idx_other])
+        yy1 = np.maximum(y1[idx_self], y1[idx_other])
+        xx2 = np.minimum(x2[idx_self], x2[idx_other])
+        yy2 = np.minimum(y2[idx_self], y2[idx_other])
+
+        w = np.maximum(0.0, xx2 - xx1)
+        h = np.maximum(0.0, yy2 - yy1)
+        inter = w * h
+
+        if min_mode:
+            over = inter / np.minimum(areas[order[0]], areas[order[1:]])
+        else:
+            over = inter / (areas[order[0]] + areas[order[1:]] - inter)
+
+        inds = np.where(over <= nms_thresh)[0]
+        order = order[inds + 1]
+
+    return np.array(keep)
+
+
+def hsv_to_rgb(h, s, v):
+    bgr = cv2.cvtColor(
+        np.array([[[h, s, v]]], dtype=np.uint8), cv2.COLOR_HSV2BGR)[0][0]
+    return int(bgr[0]), int(bgr[1]), int(bgr[2]), 255
+
+
+def plot_results(img, boxes, category, logging=True):
+    height, width = img.shape[0], img.shape[1]
+    count = len(boxes)
+    if logging:
+        print(f'object_count={count}')
+
+    for idx in range(count):
+        box = boxes[idx]
+        x1 = box[0]
+        y1 = box[1]
+        x2 = box[2]
+        y2 = box[3]
+        cls_conf = box[5]
+        cls_id = box[6]
+        # print result
+        if logging:
+            print(f'+ idx={idx}')
+            print(
+                f'  category={cls_id}[ {category[cls_id]} ]'
+            )
+            print(f'  prob={cls_conf}')
+            print(f'  x={x1}')
+            print(f'  y={y1}')
+            print(f'  w={x2-x1}')
+            print(f'  h={y2-y1}')
+        top_left = (int(width * x1), int(height * y1))
+        bottom_right = (int(width * x2), int(height * y2))
+        text_position = (int(width * x1) + 4, int(height * y2 - 8))
+
+        # update image
+        color = hsv_to_rgb(256 * cls_id / len(category), 255, 255)
+        fontScale = width / 512.0
+        cv2.rectangle(img, top_left, bottom_right, color, 4)
+
+        cv2.putText(
+            img,
+            category[cls_id],
+            text_position,
+            cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale,
+            color,
+            1
+        )
+    return img
+
+
+def plot_boxes_cv2(img, boxes, savename=None, class_names=None, color=None):
+    import cv2
+    img = np.copy(img)
+    colors = np.array([[1, 0, 1], [0, 0, 1], [0, 1, 1], [0, 1, 0], [1, 1, 0], [1, 0, 0]], dtype=np.float32)
+
+    def get_color(c, x, max_val):
+        ratio = float(x) / max_val * 5
+        i = int(math.floor(ratio))
+        j = int(math.ceil(ratio))
+        ratio = ratio - i
+        r = (1 - ratio) * colors[i][c] + ratio * colors[j][c]
+        return int(r * 255)
+
+    width = img.shape[1]
+    height = img.shape[0]
+    for i in range(len(boxes)):
+        box = boxes[i]
+        x1 = int(box[0] * width)
+        y1 = int(box[1] * height)
+        x2 = int(box[2] * width)
+        y2 = int(box[3] * height)
+
+        if color:
+            rgb = color
+        else:
+            rgb = (255, 0, 0)
+        if len(box) >= 7 and class_names:
+            cls_conf = box[5]
+            cls_id = box[6]
+            print('%s: %f' % (class_names[cls_id], cls_conf))
+            classes = len(class_names)
+            offset = cls_id * 123457 % classes
+            red = get_color(2, offset, classes)
+            green = get_color(1, offset, classes)
+            blue = get_color(0, offset, classes)
+            if color is None:
+                rgb = (red, green, blue)
+            img = cv2.putText(img, class_names[cls_id], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1.2, rgb, 1)
+        img = cv2.rectangle(img, (x1, y1), (x2, y2), rgb, 1)
+    if savename:
+        print("save plot results to %s" % savename)
+        cv2.imwrite(savename, img)
+    return img
+
+
+def post_processing(img, conf_thresh, nms_thresh, output):
+    # anchors = [12, 16, 19, 36, 40, 28, 36, 75, 76, 55, 72, 146, 142, 110, 192, 243, 459, 401]
+    # num_anchors = 9
+    # anchor_masks = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    # strides = [8, 16, 32]
+    # anchor_step = len(anchors) // num_anchors
+
+    # [batch, num, 1, 4]
+    box_array = output[0]
+    # [batch, num, num_classes]
+    confs = output[1]
+
+    t1 = time.time()
+
+    if type(box_array).__name__ != 'ndarray':
+        box_array = box_array.cpu().detach().numpy()
+        confs = confs.cpu().detach().numpy()
+
+    num_classes = confs.shape[2]
+
+    # [batch, num, 4]
+    box_array = box_array[:, :, 0]
+
+    # [batch, num, num_classes] --> [batch, num]
+    max_conf = np.max(confs, axis=2)
+    max_id = np.argmax(confs, axis=2)
+
+    t2 = time.time()
+
+    bboxes_batch = []
+    for i in range(box_array.shape[0]):
+
+        argwhere = max_conf[i] > conf_thresh
+        l_box_array = box_array[i, argwhere, :]
+        l_max_conf = max_conf[i, argwhere]
+        l_max_id = max_id[i, argwhere]
+
+        bboxes = []
+        # nms for each class
+        for j in range(num_classes):
+
+            cls_argwhere = l_max_id == j
+            ll_box_array = l_box_array[cls_argwhere, :]
+            ll_max_conf = l_max_conf[cls_argwhere]
+            ll_max_id = l_max_id[cls_argwhere]
+
+            keep = nms_cpu(ll_box_array, ll_max_conf, nms_thresh)
+
+            if (keep.size > 0):
+                ll_box_array = ll_box_array[keep, :]
+                ll_max_conf = ll_max_conf[keep]
+                ll_max_id = ll_max_id[keep]
+
+                for k in range(ll_box_array.shape[0]):
+                    bboxes.append(
+                        [ll_box_array[k, 0], ll_box_array[k, 1], ll_box_array[k, 2], ll_box_array[k, 3], ll_max_conf[k],
+                         ll_max_conf[k], ll_max_id[k]])
+
+        bboxes_batch.append(bboxes)
+
+    t3 = time.time()
+
+    print('-----------------------------------')
+    print('       max and argmax : %f' % (t2 - t1))
+    print('                  nms : %f' % (t3 - t2))
+    print('Post processing total : %f' % (t3 - t1))
+    print('-----------------------------------')
+
+    return bboxes_batch
