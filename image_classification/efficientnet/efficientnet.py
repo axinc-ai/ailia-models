c9f7386ae8c253a5bae90efb13c993e12e99dc74
sngyo <shingyo-chi_9731@keio.jp>
2020-10-03 22:33:54 +0900

    FIX: [efficientnet] utils path

--- image_classification/efficientnet/efficientnet.py
+++ image_classification/efficientnet/efficientnet.py
@@ -8,11 +8,10 @@ import ailia
 import efficientnet_labels
 
 # import original modules
-sys.path.append("../util")
-from utils import check_file_existance  # noqa: E402
+sys.path.append('../../util')
 from model_utils import check_and_download_models  # noqa: E402
 from image_utils import load_image  # noqa: E402
-from webcamera_utils import adjust_frame_size  # noqa: E402
+from webcamera_utils import adjust_frame_size, get_capture  # noqa: E402
 
 
 # ======================
@@ -31,7 +30,7 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument(
     '-m', '--model', metavar='MODEL',
-    default="b7", choices=['b0', 'b7'], 
+    default="b7", choices=['b0', 'b7'],
     help="The input model path." +
          "you can set b0 or b7 to select efficientnet-b0 or efficientnet-b7"
 )
@@ -131,15 +130,7 @@ def recognize_from_video():
         range=ailia.NETWORK_IMAGE_RANGE_S_FP32
     )
 
-    if args.video == '0':
-        print('[INFO] Webcam mode is activated')
-        capture = cv2.VideoCapture(0)
-        if not capture.isOpened():
-            print("[ERROR] webcamera not found")
-            sys.exit(1)
-    else:
-        if check_file_existance(args.video):
-            capture = cv2.VideoCapture(args.video)
+    capture = get_capture(args.video)
 
     while(True):
         ret, frame = capture.read()
