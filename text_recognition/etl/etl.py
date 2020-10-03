ef4278ff9a360e21a2e7223b477bcd12db4f6acd
sngyo <shingyo-chi_9731@keio.jp>
2020-10-03 23:24:57 +0900

    UPD: refactoring multi-cameras (DONE)

--- text_recognition/etl/etl.py
+++ text_recognition/etl/etl.py
@@ -8,9 +8,8 @@ import cv2
 import ailia
 # import original modules
 sys.path.append('../../util')
-from utils import check_file_existance   # noqa: E402
 from model_utils import check_and_download_models  # noqa: E402
-from webcamera_utils import adjust_frame_size  # noqa: E402
+from webcamera_utils import adjust_frame_size, get_capture  # noqa: E402
 
 
 # ======================
@@ -123,15 +122,7 @@ def recognize_from_video():
         range=ailia.NETWORK_IMAGE_RANGE_U_FP32
     )
 
-    if args.video == '0':
-        print('[INFO] Webcam mode is activated')
-        capture = cv2.VideoCapture(0)
-        if not capture.isOpened():
-            print("[ERROR] webcamera not found")
-            sys.exit(1)
-    else:
-        if check_file_existance(args.video):
-            capture = cv2.VideoCapture(args.video)
+    capture = get_capture(args.video)
 
     while(True):
         ret, frame = capture.read()
