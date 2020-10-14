c53c87a7621ae9628924050e019220f00c4b1916
客野一樹 <kyakuno@axell.co.jp>
2020-10-14 11:42:17 +0900

    Added support for lw_human_pose, Move labels to st_gcn_labels.py

--- action_recognition/st_gcn/st_gcn.py
+++ action_recognition/st_gcn/st_gcn.py
@@ -16,6 +16,7 @@ from utils import check_file_existance  # noqa: E402
 from model_utils import check_and_download_models  # noqa: E402
 
 from st_gcn_util import naive_pose_tracker, render_video, render_image
+from st_gcn_labels import KINETICS_LABEL
 
 # ======================
 # Parameters
@@ -23,14 +24,6 @@ from st_gcn_util import naive_pose_tracker, render_video, render_image
 WEIGHT_PATH = 'st_gcn.onnx'
 MODEL_PATH = 'st_gcn.onnx.prototxt'
 REMOTE_PATH = 'https://storage.googleapis.com/ailia-models/st_gcn/'
-
-MODEL_POSE_PATH = 'pose_deploy.prototxt'
-WEIGHT_POSE_PATH = 'pose/coco/pose_iter_440000.caffemodel'
-REMOTE_POSE_PATH = 'http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/coco/'
-
-POSE_ALGORITHM_OPEN_POSE_SINGLE_SCALE = (12)
-# POSE_ALGORITHM = ailia.POSE_ALGORITHM_OPEN_POSE
-POSE_ALGORITHM = POSE_ALGORITHM_OPEN_POSE_SINGLE_SCALE
 
 VIDEO_PATH = 'skateboarding.mp4'
 
@@ -57,405 +50,10 @@ POSE_KEY = [
 
 PYOPENPOSE_PATH = '/usr/local/python'
 
-KINETICS_LABEL = [
-    'abseiling',
-    'air drumming',
-    'answering questions',
-    'applauding',
-    'applying cream',
-    'archery',
-    'arm wrestling',
-    'arranging flowers',
-    'assembling computer',
-    'auctioning',
-    'baby waking up',
-    'baking cookies',
-    'balloon blowing',
-    'bandaging',
-    'barbequing',
-    'bartending',
-    'beatboxing',
-    'bee keeping',
-    'belly dancing',
-    'bench pressing',
-    'bending back',
-    'bending metal',
-    'biking through snow',
-    'blasting sand',
-    'blowing glass',
-    'blowing leaves',
-    'blowing nose',
-    'blowing out candles',
-    'bobsledding',
-    'bookbinding',
-    'bouncing on trampoline',
-    'bowling',
-    'braiding hair',
-    'breading or breadcrumbing',
-    'breakdancing',
-    'brush painting',
-    'brushing hair',
-    'brushing teeth',
-    'building cabinet',
-    'building shed',
-    'bungee jumping',
-    'busking',
-    'canoeing or kayaking',
-    'capoeira',
-    'carrying baby',
-    'cartwheeling',
-    'carving pumpkin',
-    'catching fish',
-    'catching or throwing baseball',
-    'catching or throwing frisbee',
-    'catching or throwing softball',
-    'celebrating',
-    'changing oil',
-    'changing wheel',
-    'checking tires',
-    'cheerleading',
-    'chopping wood',
-    'clapping',
-    'clay pottery making',
-    'clean and jerk',
-    'cleaning floor',
-    'cleaning gutters',
-    'cleaning pool',
-    'cleaning shoes',
-    'cleaning toilet',
-    'cleaning windows',
-    'climbing a rope',
-    'climbing ladder',
-    'climbing tree',
-    'contact juggling',
-    'cooking chicken',
-    'cooking egg',
-    'cooking on campfire',
-    'cooking sausages',
-    'counting money',
-    'country line dancing',
-    'cracking neck',
-    'crawling baby',
-    'crossing river',
-    'crying',
-    'curling hair',
-    'cutting nails',
-    'cutting pineapple',
-    'cutting watermelon',
-    'dancing ballet',
-    'dancing charleston',
-    'dancing gangnam style',
-    'dancing macarena',
-    'deadlifting',
-    'decorating the christmas tree',
-    'digging',
-    'dining',
-    'disc golfing',
-    'diving cliff',
-    'dodgeball',
-    'doing aerobics',
-    'doing laundry',
-    'doing nails',
-    'drawing',
-    'dribbling basketball',
-    'drinking',
-    'drinking beer',
-    'drinking shots',
-    'driving car',
-    'driving tractor',
-    'drop kicking',
-    'drumming fingers',
-    'dunking basketball',
-    'dying hair',
-    'eating burger',
-    'eating cake',
-    'eating carrots',
-    'eating chips',
-    'eating doughnuts',
-    'eating hotdog',
-    'eating ice cream',
-    'eating spaghetti',
-    'eating watermelon',
-    'egg hunting',
-    'exercising arm',
-    'exercising with an exercise ball',
-    'extinguishing fire',
-    'faceplanting',
-    'feeding birds',
-    'feeding fish',
-    'feeding goats',
-    'filling eyebrows',
-    'finger snapping',
-    'fixing hair',
-    'flipping pancake',
-    'flying kite',
-    'folding clothes',
-    'folding napkins',
-    'folding paper',
-    'front raises',
-    'frying vegetables',
-    'garbage collecting',
-    'gargling',
-    'getting a haircut',
-    'getting a tattoo',
-    'giving or receiving award',
-    'golf chipping',
-    'golf driving',
-    'golf putting',
-    'grinding meat',
-    'grooming dog',
-    'grooming horse',
-    'gymnastics tumbling',
-    'hammer throw',
-    'headbanging',
-    'headbutting',
-    'high jump',
-    'high kick',
-    'hitting baseball',
-    'hockey stop',
-    'holding snake',
-    'hopscotch',
-    'hoverboarding',
-    'hugging',
-    'hula hooping',
-    'hurdling',
-    'hurling (sport)',
-    'ice climbing',
-    'ice fishing',
-    'ice skating',
-    'ironing',
-    'javelin throw',
-    'jetskiing',
-    'jogging',
-    'juggling balls',
-    'juggling fire',
-    'juggling soccer ball',
-    'jumping into pool',
-    'jumpstyle dancing',
-    'kicking field goal',
-    'kicking soccer ball',
-    'kissing',
-    'kitesurfing',
-    'knitting',
-    'krumping',
-    'laughing',
-    'laying bricks',
-    'long jump',
-    'lunge',
-    'making a cake',
-    'making a sandwich',
-    'making bed',
-    'making jewelry',
-    'making pizza',
-    'making snowman',
-    'making sushi',
-    'making tea',
-    'marching',
-    'massaging back',
-    'massaging feet',
-    'massaging legs', "massaging person's head", 'milking cow',
-    'mopping floor',
-    'motorcycling',
-    'moving furniture',
-    'mowing lawn',
-    'news anchoring',
-    'opening bottle',
-    'opening present',
-    'paragliding',
-    'parasailing',
-    'parkour',
-    'passing American football (in game)',
-    'passing American football (not in game)',
-    'peeling apples',
-    'peeling potatoes',
-    'petting animal (not cat)',
-    'petting cat',
-    'picking fruit',
-    'planting trees',
-    'plastering',
-    'playing accordion',
-    'playing badminton',
-    'playing bagpipes',
-    'playing basketball',
-    'playing bass guitar',
-    'playing cards',
-    'playing cello',
-    'playing chess',
-    'playing clarinet',
-    'playing controller',
-    'playing cricket',
-    'playing cymbals',
-    'playing didgeridoo',
-    'playing drums',
-    'playing flute',
-    'playing guitar',
-    'playing harmonica',
-    'playing harp',
-    'playing ice hockey',
-    'playing keyboard',
-    'playing kickball',
-    'playing monopoly',
-    'playing organ',
-    'playing paintball',
-    'playing piano',
-    'playing poker',
-    'playing recorder',
-    'playing saxophone',
-    'playing squash or racquetball',
-    'playing tennis',
-    'playing trombone',
-    'playing trumpet',
-    'playing ukulele',
-    'playing violin',
-    'playing volleyball',
-    'playing xylophone',
-    'pole vault',
-    'presenting weather forecast',
-    'pull ups',
-    'pumping fist',
-    'pumping gas',
-    'punching bag',
-    'punching person (boxing)',
-    'push up',
-    'pushing car',
-    'pushing cart',
-    'pushing wheelchair',
-    'reading book',
-    'reading newspaper',
-    'recording music',
-    'riding a bike',
-    'riding camel',
-    'riding elephant',
-    'riding mechanical bull',
-    'riding mountain bike',
-    'riding mule',
-    'riding or walking with horse',
-    'riding scooter',
-    'riding unicycle',
-    'ripping paper',
-    'robot dancing',
-    'rock climbing',
-    'rock scissors paper',
-    'roller skating',
-    'running on treadmill',
-    'sailing',
-    'salsa dancing',
-    'sanding floor',
-    'scrambling eggs',
-    'scuba diving',
-    'setting table',
-    'shaking hands',
-    'shaking head',
-    'sharpening knives',
-    'sharpening pencil',
-    'shaving head',
-    'shaving legs',
-    'shearing sheep',
-    'shining shoes',
-    'shooting basketball',
-    'shooting goal (soccer)',
-    'shot put',
-    'shoveling snow',
-    'shredding paper',
-    'shuffling cards',
-    'side kick',
-    'sign language interpreting',
-    'singing',
-    'situp',
-    'skateboarding',
-    'ski jumping',
-    'skiing (not slalom or crosscountry)',
-    'skiing crosscountry',
-    'skiing slalom',
-    'skipping rope',
-    'skydiving',
-    'slacklining',
-    'slapping',
-    'sled dog racing',
-    'smoking',
-    'smoking hookah',
-    'snatch weight lifting',
-    'sneezing',
-    'sniffing',
-    'snorkeling',
-    'snowboarding',
-    'snowkiting',
-    'snowmobiling',
-    'somersaulting',
-    'spinning poi',
-    'spray painting',
-    'spraying',
-    'springboard diving',
-    'squat',
-    'sticking tongue out',
-    'stomping grapes',
-    'stretching arm',
-    'stretching leg',
-    'strumming guitar',
-    'surfing crowd',
-    'surfing water',
-    'sweeping floor',
-    'swimming backstroke',
-    'swimming breast stroke',
-    'swimming butterfly stroke',
-    'swing dancing',
-    'swinging legs',
-    'swinging on something',
-    'sword fighting',
-    'tai chi',
-    'taking a shower',
-    'tango dancing',
-    'tap dancing',
-    'tapping guitar',
-    'tapping pen',
-    'tasting beer',
-    'tasting food',
-    'testifying',
-    'texting',
-    'throwing axe',
-    'throwing ball',
-    'throwing discus',
-    'tickling',
-    'tobogganing',
-    'tossing coin',
-    'tossing salad',
-    'training dog',
-    'trapezing',
-    'trimming or shaving beard',
-    'trimming trees',
-    'triple jump',
-    'tying bow tie',
-    'tying knot (not on a tie)',
-    'tying tie',
-    'unboxing',
-    'unloading truck',
-    'using computer',
-    'using remote controller (not gaming)',
-    'using segway',
-    'vault',
-    'waiting in line',
-    'walking the dog',
-    'washing dishes',
-    'washing feet',
-    'washing hair',
-    'washing hands',
-    'water skiing',
-    'water sliding',
-    'watering plants',
-    'waxing back',
-    'waxing chest',
-    'waxing eyebrows',
-    'waxing legs',
-    'weaving basket',
-    'welding',
-    'whistling',
-    'windsurfing',
-    'wrapping present',
-    'wrestling',
-    'writing',
-    'yawning',
-    'yoga',
-    'zumba'
+MODEL_LISTS = [
+    'openpose',
+    'pyopenpose',
+    'lw_human_pose'
 ]
 
 # ======================
@@ -488,9 +86,9 @@ parser.add_argument(
     help='FPS of video.'
 )
 parser.add_argument(
-    '--op',
-    action='store_true',
-    help='Use Openpose Python API.',
+    '-a', '--arch', metavar='ARCH',
+    default='openpose', choices=MODEL_LISTS,
+    help='model lists: ' + ' | '.join(MODEL_LISTS)
 )
 parser.add_argument(
     '--img-save',
@@ -499,7 +97,7 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-if args.op:
+if args.arch == "pyopenpose":
     sys.path.insert(0, PYOPENPOSE_PATH)
     try:
         from openpose import pyopenpose as op
@@ -507,6 +105,18 @@ if args.op:
         print('Can not find Openpose Python API.')
         sys.exit(-1)
 
+if args.arch == "lw_human_pose":
+    POSE_ALGORITHM = ailia.POSE_ALGORITHM_LW_HUMAN_POSE
+    WEIGHT_POSE_PATH = 'lightweight-human-pose-estimation.opt.onnx'
+    MODEL_POSE_PATH = 'lightweight-human-pose-estimation.opt.onnx.prototxt'
+    REMOTE_POSE_PATH = f'https://storage.googleapis.com/ailia-models/lightweight-human-pose-estimation/'
+else:
+    POSE_ALGORITHM_OPEN_POSE_SINGLE_SCALE = (12)
+    # POSE_ALGORITHM = ailia.POSE_ALGORITHM_OPEN_POSE
+    POSE_ALGORITHM = POSE_ALGORITHM_OPEN_POSE_SINGLE_SCALE
+    MODEL_POSE_PATH = 'pose_deploy.prototxt'
+    WEIGHT_POSE_PATH = 'pose/coco/pose_iter_440000.caffemodel'
+    REMOTE_POSE_PATH = 'http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/coco/'
 
 # ======================
 # Secondaty Functions
@@ -569,7 +179,7 @@ def recognize_offline(input, pose, net):
         H, W, _ = img.shape
 
         # pose estimate
-        if args.op:
+        if args.arch == "pyopenpose":
             datum = op.Datum()
             datum.cvInputData = img
             pose.emplaceAndPop([datum])
@@ -677,7 +287,7 @@ def recognize_realtime(video, pose, net):
         H, W, _ = img.shape
 
         # pose estimate
-        if args.op:
+        if args.arch == "pyopenpose":
             datum = op.Datum()
             datum.cvInputData = img
             pose.emplaceAndPop([datum])
@@ -746,7 +356,7 @@ def main():
     # model files check and download
     print("=== ST-GCN model ===")
     check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
-    if not args.op:
+    if args.arch == "pyopenpose":
         print("=== OpenPose model ===")
         check_and_download_models(WEIGHT_POSE_PATH, MODEL_POSE_PATH, REMOTE_POSE_PATH)
 
@@ -755,7 +365,7 @@ def main():
     print(f'env_id: {env_id}')
     net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=env_id)
 
-    if args.op:
+    if args.arch == "pyopenpose":
         pose = op.WrapperPython()
         params = dict(model_folder='.', model_pose='COCO')
         pose.configure(params)
@@ -764,7 +374,8 @@ def main():
         pose = ailia.PoseEstimator(
             MODEL_POSE_PATH, WEIGHT_POSE_PATH, env_id=env_id, algorithm=POSE_ALGORITHM
         )
-        pose.set_threshold(0.1)
+        if args.arch == "openpose":
+            pose.set_threshold(0.1)
 
     if args.video is not None:
         # realtime mode
