d88a3fc35592d3d3cab107155935a92dd5e318a8
satoshi.ooe <ooe1123@gmail.com>
2020-11-30 23:48:31 +0900

    Implement pixel-link

---
+++ object_detection/pixel_link/pixel_link.py
@@ -0,0 +1,175 @@
+import sys, os
+import time
+import argparse
+
+import numpy as np
+import cv2
+
+import ailia
+
+# import original modules
+sys.path.append('../../util')
+from model_utils import check_and_download_models  # noqa: E402
+from detector_utils import plot_results, load_image  # noqa: E402C
+from webcamera_utils import get_capture  # noqa: E402
+
+from pixel_link_utils import decode_batch, mask_to_bboxes, draw_bbox
+
+# ======================
+# Parameters
+# ======================
+
+WEIGHT_PATH = 'pixellink-vgg16-4s.onnx'
+MODEL_PATH = 'pixellink-vgg16-4s.onnx.prototxt'
+REMOTE_PATH = \
+    'https://storage.googleapis.com/ailia-models/pixel_link/'
+
+IMAGE_PATH = 'img_249.jpg'
+SAVE_IMAGE_PATH = 'output.png'
+
+# ======================
+# Arguemnt Parser Config
+# ======================
+
+parser = argparse.ArgumentParser(
+    description='Pixel-Link model'
+)
+parser.add_argument(
+    '-i', '--input', metavar='IMAGE',
+    default=IMAGE_PATH,
+    help='The input image path.'
+)
+parser.add_argument(
+    '-v', '--video', metavar='VIDEO',
+    default=None,
+    help='The input video path. ' +
+         'If the VIDEO argument is set to 0, the webcam input will be used.'
+)
+parser.add_argument(
+    '-s', '--savepath', metavar='SAVE_IMAGE_PATH', default=SAVE_IMAGE_PATH,
+    help='Save path for the output image.'
+)
+parser.add_argument(
+    '-b', '--benchmark',
+    action='store_true',
+    help='Running the inference on the same input 5 times ' +
+         'to measure execution performance. (Cannot be used in video mode)'
+)
+parser.add_argument(
+    '--onnx',
+    action='store_true',
+    help='execute onnxruntime version.'
+)
+args = parser.parse_args()
+
+
+# ======================
+# Secondaty Functions
+# ======================
+
+
+def post_processing(pixel_pos_scores, link_pos_scores, image_shape):
+    mask = decode_batch(pixel_pos_scores, link_pos_scores)[0, ...]
+    bboxes = mask_to_bboxes(mask, image_shape)
+
+    return bboxes
+
+
+# ======================
+# Main functions
+# ======================
+
+
+def predict(img, net):
+    img = img.astype(np.int32)
+
+    # feedforward
+    if not args.onnx:
+        net.set_input_shape(img.shape)
+        output = net.predict({'import/test/Placeholder:0': img})
+    else:
+        img_name = net.get_inputs()[0].name
+        pixel_pos_scores_name = net.get_outputs()[0].name
+        link_pos_scores_name = net.get_outputs()[1].name
+        output = net.run([pixel_pos_scores_name, link_pos_scores_name],
+                         {img_name: img})
+
+    pixel_pos_scores, link_pos_scores = output
+    bboxes = post_processing(pixel_pos_scores, link_pos_scores, img.shape)
+
+    return bboxes
+
+
+def recognize_from_image(filename, net):
+    # prepare input data
+    img = load_image(filename)
+    print(f'input image shape: {img.shape}')
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+
+    # inference
+    print('Start inference...')
+    if args.benchmark:
+        print('BENCHMARK mode')
+        for i in range(5):
+            start = int(round(time.time() * 1000))
+            bboxes = predict(img, net)
+            end = int(round(time.time() * 1000))
+            print(f'\tailia processing time {end - start} ms')
+    else:
+        bboxes = predict(img, net)
+
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    res_img = draw_bbox(img, bboxes)
+
+    # plot result
+    cv2.imwrite(args.savepath, res_img)
+    print('Script finished successfully.')
+
+
+def recognize_from_video(video, net):
+    capture = get_capture(video)
+
+    while (True):
+        ret, frame = capture.read()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        if not ret:
+            continue
+
+        bboxes = predict(frame, net)
+
+        # plot result
+        res_img = draw_bbox(frame, bboxes)
+
+        # show
+        cv2.imshow('frame', res_img)
+
+    capture.release()
+    cv2.destroyAllWindows()
+    print('Script finished successfully.')
+
+
+def main():
+    # model files check and download
+    check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
+
+    # load model
+    env_id = ailia.get_gpu_environment_id()
+    print(f'env_id: {env_id}')
+
+    # initialize
+    if not args.onnx:
+        net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=env_id)
+    else:
+        import onnxruntime
+        net = onnxruntime.InferenceSession(WEIGHT_PATH)
+
+    if args.video is not None:
+        recognize_from_video(args.video, net)
+    else:
+        recognize_from_image(args.input, net)
+
+
+if __name__ == '__main__':
+    main()
---
+++ object_detection/pixel_link/pixel_link_utils.py
@@ -0,0 +1,181 @@
+import numpy as np
+import cv2
+
+__all__ = [
+    'decode_batch',
+    'mask_to_bboxes',
+    'draw_bbox',
+]
+
+
+def decode_batch(
+        pixel_cls_scores, pixel_link_scores,
+        pixel_conf_threshold=0.8, link_conf_threshold=0.8):
+    batch_size = pixel_cls_scores.shape[0]
+    batch_mask = []
+    for image_idx in range(batch_size):
+        image_pos_pixel_scores = pixel_cls_scores[image_idx, :, :]
+        image_pos_link_scores = pixel_link_scores[image_idx, :, :, :]
+        mask = decode_image(
+            image_pos_pixel_scores, image_pos_link_scores,
+            pixel_conf_threshold, link_conf_threshold
+        )
+        batch_mask.append(mask)
+    return np.asarray(batch_mask, np.int32)
+
+
+def get_neighbours(x, y):
+    return [
+        (x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x - 1, y), \
+        (x + 1, y), (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)
+    ]
+
+
+def is_valid_cord(x, y, w, h):
+    return x >= 0 and x < w and y >= 0 and y < h
+
+
+def decode_image(
+        pixel_scores, link_scores,
+        pixel_conf_threshold, link_conf_threshold):
+    #
+    pixel_mask = pixel_scores >= pixel_conf_threshold
+    link_mask = link_scores >= link_conf_threshold
+    done_mask = np.zeros(pixel_mask.shape, np.bool)
+    result_mask = np.zeros(pixel_mask.shape, np.int32)
+    points = list(zip(*np.where(pixel_mask)))
+    h, w = np.shape(pixel_mask)
+    group_id = 0
+    for point in points:
+        if done_mask[point]:
+            continue
+        group_id += 1
+        group_q = [point]
+        result_mask[point] = group_id
+        while len(group_q):
+            y, x = group_q[-1]
+            group_q.pop()
+            if not done_mask[y, x]:
+                done_mask[y, x], result_mask[y, x] = True, group_id
+                for n_idx, (nx, ny) in enumerate(get_neighbours(x, y)):
+                    if is_valid_cord(nx, ny, w, h) and pixel_mask[ny, nx] and (
+                            link_mask[y, x, n_idx] or link_mask[ny, nx, 7 - n_idx]):
+                        group_q.append((ny, nx))
+    return result_mask
+
+
+def find_contours(mask, method=None):
+    if method is None:
+        method = cv2.CHAIN_APPROX_SIMPLE
+    mask = np.asarray(mask, dtype=np.uint8)
+    mask = mask.copy()
+    try:
+        contours, _ = cv2.findContours(mask, mode=cv2.RETR_CCOMP,
+                                       method=method)
+    except:
+        _, contours, _ = cv2.findContours(mask, mode=cv2.RETR_CCOMP,
+                                          method=method)
+    return contours
+
+
+def min_area_rect(cnt):
+    """
+    Args:
+        xs: numpy ndarray with shape=(N,4). N is the number of oriented bboxes. 4 contains [x1, x2, x3, x4]
+        ys: numpy ndarray with shape=(N,4), [y1, y2, y3, y4]
+            Note that [(x1, y1), (x2, y2), (x3, y3), (x4, y4)] can represent an oriented bbox.
+    Return:
+        the oriented rects sorrounding the box, in the format:[cx, cy, w, h, theta].
+    """
+    rect = cv2.minAreaRect(cnt)
+    cx, cy = rect[0]
+    w, h = rect[1]
+    theta = rect[2]
+    box = [cx, cy, w, h, theta]
+    return box, w * h
+
+
+def rect_to_xys(rect, image_shape):
+    """Convert rect to xys, i.e., eight points
+    The `image_shape` is used to to make sure all points return are valid, i.e., within image area
+    """
+    h, w = image_shape[0:2]
+
+    def get_valid_x(x):
+        if x < 0:
+            return 0
+        if x >= w:
+            return w - 1
+        return x
+
+    def get_valid_y(y):
+        if y < 0:
+            return 0
+        if y >= h:
+            return h - 1
+        return y
+
+    rect = ((rect[0], rect[1]), (rect[2], rect[3]), rect[4])
+    points = cv2.boxPoints(rect)
+    points = np.int0(points)
+    for i_xy, (x, y) in enumerate(points):
+        x = get_valid_x(x)
+        y = get_valid_y(y)
+        points[i_xy, :] = [x, y]
+    points = np.reshape(points, -1)
+    return points
+
+
+def mask_to_bboxes(
+        mask, image_shape, min_area=300,
+        min_height=10):
+    image_h, image_w = image_shape[0:2]
+    bboxes = []
+    max_bbox_idx = mask.max()
+    mask = cv2.resize(mask, (image_w, image_h),
+                      interpolation=cv2.INTER_NEAREST)
+
+    for bbox_idx in range(1, max_bbox_idx + 1):
+        bbox_mask = mask == bbox_idx
+        cnts = find_contours(bbox_mask)
+        if len(cnts) == 0:
+            continue
+        cnt = cnts[0]
+        rect, rect_area = min_area_rect(cnt)
+
+        w, h = rect[2:-1]
+        if min(w, h) < min_height:
+            continue
+
+        if rect_area < min_area:
+            continue
+
+        xys = rect_to_xys(rect, image_shape)
+        bboxes.append(xys)
+
+    return bboxes
+
+
+def points_to_contour(points):
+    contours = [[list(p)] for p in points]
+    return np.asarray(contours, dtype=np.int32)
+
+
+def points_to_contours(points):
+    return np.asarray([points_to_contour(points)])
+
+
+def draw_contours(img, contours, idx=-1, color=1, border_width=1):
+    cv2.drawContours(img, contours, idx, color, border_width)
+    return img
+
+
+def draw_bbox(img, bboxes):
+    COLOR_GREEN = (0, 255, 0)
+
+    for bbox in bboxes:
+        points = [int(v) for v in bbox]
+        points = np.reshape(points, (4, 2))
+        cnts = points_to_contours(points)
+        cv2.drawContours(img, cnts, -1, COLOR_GREEN, thickness=3)
+    return img
