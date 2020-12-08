529b113a07d18d852c82ca03a8a27f7fde5eb7e1
sngyo <shingyo-chi_9731@keio.jp>
2020-12-08 14:45:06 +0900

    UPD: utils try to import ailia

--- util/utils.py
+++ util/utils.py
@@ -2,6 +2,16 @@ import os
 import sys
 import argparse
 
+try:
+    import ailia
+    AILIA_EXIST = True
+except ImportError:
+    # TODO: create logger.py --> @sngyo
+    print('[WARNING]: ailia package cannot be found under `sys.path`')
+    print('[WARNING]: default env_id is set to 0, you can change the id by '
+          '[--env_id N]')
+    AILIA_EXIST = False
+
 
 def check_file_existance(filename):
     if os.path.isfile(filename):
@@ -49,16 +59,9 @@ def get_base_parser(description, default_img, default_save, parse=False):
         help=('Running the inference on the same input 5 times to measure '
               'execution performance. (Cannot be used in video mode)')
     )
-    # NOTE: Since we do not import ailia library here,
-    #       default=ailia.get_gpu_environment_id() cannot be set here.
-    #       Therefore, following code should be used in each script
-    #           if args.env_id is None:
-    #               env_id = ailia.get_gpu_environment_id()
-    #           else:
-    #               env_id = args.env_id
-    # TODO: ailia library directory can be separated from each project?
     parser.add_argument(
-        '-e', '--env_id', default=None,
+        '-e', '--env_id', type=int,
+        default=ailia.get_gpu_environment_id() if AILIA_EXIST else 0,
         help=('A specific environment id can be specified. By default, '
               'the return value of ailia.get_gpu_environment_id will be used')
     )
