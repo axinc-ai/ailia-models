765797b8e40a9f4c1e581a50333140a1d9c4ecd6
sngyo <shingyo-chi_9731@keio.jp>
2020-12-13 20:06:11 +0900

    UPD: refactoring nlp models

--- neural_language_processing/bert_maskedlm/bert_maskedlm.py
+++ neural_language_processing/bert_maskedlm/bert_maskedlm.py
@@ -1,14 +1,14 @@
 import sys
-import torch
-import numpy
-import argparse
 import time
 
-from transformers import BertTokenizer, BertJapaneseTokenizer, BertForMaskedLM
+import torch
+import numpy
+from transformers import BertTokenizer, BertJapaneseTokenizer
 
 import ailia
 
 sys.path.append('../../util')
+from utils import get_base_parser, update_parser  # noqa: E402
 from model_utils import check_and_download_models  # noqa: E402
 
 
@@ -26,27 +26,20 @@ NUM_PREDICT = 5
 
 SENTENCE = '私は[MASK]で動く。'
 
-parser = argparse.ArgumentParser(
-    description='bert masklm sample.'
-)
 
+parser = get_base_parser('bert masklm sample.', None, None)
+# overwrite
 parser.add_argument(
     '--input', '-i', metavar='TEXT',
-    default=SENTENCE, 
+    default=SENTENCE,
     help='input text'
-)
-parser.add_argument(
-    '-b', '--benchmark',
-    action='store_true',
-    help='Running the inference on the same input 5 times ' +
-         'to measure execution performance. (Cannot be used in video mode)'
 )
 parser.add_argument(
     '-a', '--arch', metavar='ARCH',
     default='bert-base-japanese-whole-word-masking', choices=MODEL_LISTS,
     help='model lists: ' + ' | '.join(MODEL_LISTS)
 )
-args = parser.parse_args()
+args = update_parser(parser)
 
 
 # ======================
@@ -65,19 +58,21 @@ def main():
     # model files check and download
     check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
 
-    if args.arch=='bert-base-cased' or args.arch=='bert-base-uncased':
+    if args.arch == 'bert-base-cased' or args.arch == 'bert-base-uncased':
         tokenizer = BertTokenizer.from_pretrained(args.arch)
     else:
-        tokenizer = BertJapaneseTokenizer.from_pretrained('cl-tohoku/'+'bert-base-japanese-whole-word-masking')
+        tokenizer = BertJapaneseTokenizer.from_pretrained(
+            'cl-tohoku/'+'bert-base-japanese-whole-word-masking'
+        )
     text = args.input
     print("Input text : "+text)
 
     tokenized_text = tokenizer.tokenize(text)
-    print("Tokenized text : ",tokenized_text)
+    print("Tokenized text : ", tokenized_text)
 
     masked_index = -1
-    for i in range(0,len(tokenized_text)):
-        if tokenized_text[i]=='[MASK]':
+    for i in range(0, len(tokenized_text)):
+        if tokenized_text[i] == '[MASK]':
             masked_index = i
             break
     if masked_index == -1:
@@ -85,16 +80,20 @@ def main():
         sys.exit(1)
 
     indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
-    print("Indexed tokens : ",indexed_tokens)
+    print("Indexed tokens : ", indexed_tokens)
 
-    ailia_model = ailia.Net(MODEL_PATH,WEIGHT_PATH)
+    ailia_model = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=args.env_id)
 
     indexed_tokens = numpy.array(indexed_tokens)
-    token_type_ids = numpy.zeros((1,len(tokenized_text)))
-    attention_mask = numpy.zeros((1,len(tokenized_text)))
-    attention_mask[:,0:len(tokenized_text)]=1
+    token_type_ids = numpy.zeros((1, len(tokenized_text)))
+    attention_mask = numpy.zeros((1, len(tokenized_text)))
+    attention_mask[:, 0:len(tokenized_text)] = 1
 
-    inputs_onnx = {"token_type_ids":token_type_ids,"input_ids":indexed_tokens,"attention_mask":attention_mask}
+    inputs_onnx = {
+        "token_type_ids": token_type_ids,
+        "input_ids": indexed_tokens,
+        "attention_mask": attention_mask,
+    }
 
     print("Predicting...")
     if args.benchmark:
@@ -107,7 +106,8 @@ def main():
     else:
         outputs = ailia_model.predict(inputs_onnx)
 
-    predictions = torch.from_numpy(outputs[0][0, masked_index]).topk(NUM_PREDICT)
+    predictions = torch.from_numpy(
+        outputs[0][0, masked_index]).topk(NUM_PREDICT)
 
     print("Predictions : ")
     for i, index_t in enumerate(predictions.indices):
@@ -116,6 +116,7 @@ def main():
         print(i, token)
 
     print('Script finished successfully.')
+
 
 if __name__ == "__main__":
     main()
