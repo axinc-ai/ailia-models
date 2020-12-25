cb84df74e202ea366cab78f848bba671d57c62c6
sngyo <shingyo-chi_9731@keio.jp>
2020-12-25 17:23:34 +0900

    UPD: directory input [depth_estimation] + update some utils

--- util/utils.py
+++ util/utils.py
@@ -170,12 +170,13 @@ def update_parser(parser):
         args.input = [args.input]
     else:
         logger.error('specified input is not file path nor directory path')
+        sys.exit(0)
 
     # -------------------------------------------------------------------------
     return args
 
 
-def get_savepath(arg_path, src_path, prefix='', post_fix='_res'):
+def get_savepath(arg_path, src_path, prefix='', post_fix='_res', ext=None):
     """Get savepath
     NOTE: we may have better option...
     TODO: args.save_dir & args.save_path ?
@@ -186,20 +187,27 @@ def get_savepath(arg_path, src_path, prefix='', post_fix='_res'):
         argument parser's savepath
     src_path : str
         the path of source path
-    prefix : str
-    postfix : str
+    prefix : str, default is ''
+    postfix : str, default is '_res'
+    ext : str, default is None
+        if you need to specify the extension, use this argument
+        the argument has to start with '.' like '.png' or '.jpg'
 
     Returns
     -------
     new_path : str
     """
+
     if '.' in arg_path:
         # 1. args.savepath is actually the image path
-        new_path = arg_path
+        arg_base, arg_ext = os.path.splitext(arg_path)
+        new_ext = arg_ext if ext is None else ext
+        new_path = arg_base + new_ext
     else:
         # 2. args.savepath is save directory path
         src_base, src_ext = os.path.splitext(os.path.basename(src_path))
+        new_ext = src_ext if ext is None else ext
         new_path = os.path.join(
-            arg_path, prefix + src_base + post_fix + src_ext
+            arg_path, prefix + src_base + post_fix + new_ext
         )
     return new_path
