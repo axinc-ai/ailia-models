71138d0a9951b656728de8c20b18c8a5111f17e5
sngyo <shingyo-chi_9731@keio.jp>
2021-01-06 17:02:09 +0900

    UPD: refactoring, input directory mode [facemesh]

--- face_recognition/facemesh/facemesh.py
+++ face_recognition/facemesh/facemesh.py
@@ -1,6 +1,5 @@
 import sys
 import time
-import argparse
 
 import cv2
 import numpy as np
@@ -9,10 +8,13 @@ import ailia
 import facemesh_utils as fut
 
 sys.path.append('../../util')
-from utils import get_base_parser, update_parser  # noqa: E402
-from webcamera_utils import adjust_frame_size, get_capture  # noqa: E402
-from image_utils import load_image  # noqa: E402
+from utils import get_base_parser, update_parser, get_savepath  # noqa: E402
+import webcamera_utils  # noqa: E402
 from model_utils import check_and_download_models  # noqa: E402
+
+# logger
+from logging import getLogger   # noqa: E402
+logger = getLogger(__name__)
 
 
 # ======================
@@ -28,7 +30,9 @@ IMAGE_WIDTH = 128
 # Argument Parser Config
 # ======================
 parser = get_base_parser(
-    'Face Mesh, an on-device real-time face recognition.', IMAGE_PATH, SAVE_IMAGE_PATH,
+    'Face Mesh, an on-device real-time face recognition.',
+    IMAGE_PATH,
+    SAVE_IMAGE_PATH,
 )
 parser.add_argument(
     '-n', '--normal',
@@ -63,11 +67,11 @@ LANDMARK_REMOTE_PATH = f'https://storage.googleapis.com/ailia-models/{LANDMARK_M
 # ======================
 def draw_roi(img, roi):
     for i in range(roi.shape[0]):
-        (x1,x2,x3,x4), (y1,y2,y3,y4) = roi[i]
-        cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)), (0,0,0), 2)
-        cv2.line(img, (int(x1), int(y1)), (int(x3), int(y3)), (0,255,0), 2)
-        cv2.line(img, (int(x2), int(y2)), (int(x4), int(y4)), (0,0,0), 2)
-        cv2.line(img, (int(x3), int(y3)), (int(x4), int(y4)), (0,0,0), 2)
+        (x1, x2, x3, x4), (y1, y2, y3, y4) = roi[i]
+        cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 0), 2)
+        cv2.line(img, (int(x1), int(y1)), (int(x3), int(y3)), (0, 255, 0), 2)
+        cv2.line(img, (int(x2), int(y2)), (int(x4), int(y4)), (0, 0, 0), 2)
+        cv2.line(img, (int(x3), int(y3)), (int(x4), int(y4)), (0, 0, 0), 2)
 
 
 def draw_landmarks(img, points, color=(0, 0, 255), size=2):
@@ -81,76 +85,96 @@ def draw_landmarks(img, points, color=(0, 0, 255), size=2):
 # Main functions
 # ======================
 def recognize_from_image():
-    # prepare input data
-    src_img = cv2.imread(args.input)
-    _, img128, scale, pad = fut.resize_pad(src_img[:,:,::-1])
-    input_data = img128.astype('float32') / 127.5 - 1.0
-    input_data = np.expand_dims(np.moveaxis(input_data, -1, 0), 0)
-
     # net initialize
-    env_id = ailia.get_gpu_environment_id()
-    print(f'env_id: {env_id}')
-    detector = ailia.Net(DETECTION_MODEL_PATH, DETECTION_WEIGHT_PATH, env_id=env_id)
-    estimator = ailia.Net(LANDMARK_MODEL_PATH, LANDMARK_WEIGHT_PATH, env_id=env_id)
+    detector = ailia.Net(
+        DETECTION_MODEL_PATH, DETECTION_WEIGHT_PATH, env_id=args.env_id
+    )
+    estimator = ailia.Net(
+        LANDMARK_MODEL_PATH, LANDMARK_WEIGHT_PATH, env_id=args.env_id
+    )
 
-    # inference
-    print('Start inference...')
-    if args.benchmark:
-        print('BENCHMARK mode')
-        for _ in range(5):
-            start = int(round(time.time() * 1000))
+    # input image loop
+    for image_path in args.input:
+        # prepare input data
+        logger.info(image_path)
+        src_img = cv2.imread(image_path)
+        _, img128, scale, pad = fut.resize_pad(src_img[:, :, ::-1])
+        input_data = img128.astype('float32') / 127.5 - 1.0
+        input_data = np.expand_dims(np.moveaxis(input_data, -1, 0), 0)
+
+        # inference
+        logger.info('Start inference...')
+        if args.benchmark:
+            logger.info('BENCHMARK mode')
+            for _ in range(5):
+                start = int(round(time.time() * 1000))
+                # Face detection
+                preds = detector.predict([input_data])
+                detections = fut.detector_postprocess(preds)
+
+                # Face landmark estimation
+                if detections[0].size != 0:
+                    imgs, affines, box = fut.estimator_preprocess(
+                        src_img[:, :, ::-1], detections, scale, pad
+                    )
+                    draw_roi(src_img, box)
+                    estimator.set_input_shape(imgs.shape)
+                    landmarks, confidences = estimator.predict([imgs])
+                    normalized_landmarks = landmarks / 192.0
+
+                    # postprocessing
+                    landmarks = fut.denormalize_landmarks(
+                        normalized_landmarks, affines
+                    )
+                    for i in range(len(landmarks)):
+                        landmark, confidence = landmarks[i], confidences[i]
+                        # if confidence > 0:
+                        # Can be > 1, no idea what it represents
+                        draw_landmarks(src_img, landmark[:, :2], size=1)
+                end = int(round(time.time() * 1000))
+                logger.info(f'\tailia processing time {end - start} ms')
+        else:
             # Face detection
             preds = detector.predict([input_data])
             detections = fut.detector_postprocess(preds)
 
             # Face landmark estimation
             if detections[0].size != 0:
-                imgs, affines, box = fut.estimator_preprocess(src_img[:,:,::-1], detections, scale, pad)
+                imgs, affines, box = fut.estimator_preprocess(
+                    src_img[:, :, ::-1], detections, scale, pad
+                )
                 draw_roi(src_img, box)
                 estimator.set_input_shape(imgs.shape)
                 landmarks, confidences = estimator.predict([imgs])
                 normalized_landmarks = landmarks / 192.0
 
                 # postprocessing
-                landmarks = fut.denormalize_landmarks(normalized_landmarks, affines)
+                landmarks = fut.denormalize_landmarks(
+                    normalized_landmarks, affines
+                )
                 for i in range(len(landmarks)):
+                    # FIXME: confidence unused
                     landmark, confidence = landmarks[i], confidences[i]
-                    # if confidence > 0: # Can be > 1, no idea what it represents
-                    draw_landmarks(src_img, landmark[:,:2], size=1)
-            end = int(round(time.time() * 1000))
-            print(f'\tailia processing time {end - start} ms')
-    else:
-        # Face detection
-        preds = detector.predict([input_data])
-        detections = fut.detector_postprocess(preds)
+                    # if confidence > 0:
+                    # Can be > 1, no idea what it represents
+                    draw_landmarks(src_img, landmark[:, :2], size=1)
 
-        # Face landmark estimation
-        if detections[0].size != 0:
-            imgs, affines, box = fut.estimator_preprocess(src_img[:,:,::-1], detections, scale, pad)
-            draw_roi(src_img, box)
-            estimator.set_input_shape(imgs.shape)
-            landmarks, confidences = estimator.predict([imgs])
-            normalized_landmarks = landmarks / 192.0
-
-            # postprocessing
-            landmarks = fut.denormalize_landmarks(normalized_landmarks, affines)
-            for i in range(len(landmarks)):
-                landmark, confidence = landmarks[i], confidences[i]
-                # if confidence > 0: # Can be > 1, no idea what it represents
-                draw_landmarks(src_img, landmark[:,:2], size=1)
-
-    cv2.imwrite(args.savepath, src_img)
-    print('Script finished successfully.')
+        savepath = get_savepath(args.savepath, image_path)
+        logger.info(f'saved at : {savepath}')
+        cv2.imwrite(savepath, src_img)
+    logger.info('Script finished successfully.')
 
 
 def recognize_from_video():
     # net initialize
-    env_id = ailia.get_gpu_environment_id()
-    print(f'env_id: {env_id}')
-    detector = ailia.Net(DETECTION_MODEL_PATH, DETECTION_WEIGHT_PATH, env_id=env_id)
-    estimator = ailia.Net(LANDMARK_MODEL_PATH, LANDMARK_WEIGHT_PATH, env_id=env_id)
+    detector = ailia.Net(
+        DETECTION_MODEL_PATH, DETECTION_WEIGHT_PATH, env_id=args.env_id
+    )
+    estimator = ailia.Net(
+        LANDMARK_MODEL_PATH, LANDMARK_WEIGHT_PATH, env_id=args.env_id
+    )
 
-    capture = get_capture(args.video)
+    capture = webcamera_utils.get_capture(args.video)
 
     # create video writer if savepath is specified as video format
     if args.savepath != SAVE_IMAGE_PATH:
@@ -168,9 +192,9 @@ def recognize_from_video():
         if (cv2.waitKey(1) & 0xFF == ord('q')) or not ret:
             break
 
-        frame = np.ascontiguousarray(frame[:,::-1,:])
+        frame = np.ascontiguousarray(frame[:, ::-1, :])
 
-        _, img128, scale, pad = fut.resize_pad(frame[:,:,::-1])
+        _, img128, scale, pad = fut.resize_pad(frame[:, :, ::-1])
         input_data = img128.astype('float32') / 127.5 - 1.0
         input_data = np.expand_dims(np.moveaxis(input_data, -1, 0), 0)
 
@@ -181,18 +205,23 @@ def recognize_from_video():
 
         # Face landmark estimation
         if detections[0].size != 0:
-            imgs, affines, box = fut.estimator_preprocess(frame[:,:,::-1], detections, scale, pad)
+            imgs, affines, box = fut.estimator_preprocess(
+                frame[:, :, ::-1], detections, scale, pad
+            )
             draw_roi(frame, box)
             estimator.set_input_shape(imgs.shape)
             landmarks, confidences = estimator.predict([imgs])
             normalized_landmarks = landmarks / 192.0
 
             # postprocessing
-            landmarks = fut.denormalize_landmarks(normalized_landmarks, affines)
+            landmarks = fut.denormalize_landmarks(
+                normalized_landmarks, affines
+            )
             for i in range(len(landmarks)):
                 landmark, confidence = landmarks[i], confidences[i]
-                # if confidence > 0: # Can be > 1, no idea what it represents
-                draw_landmarks(frame, landmark[:,:2], size=1)
+                # if confidence > 0:
+                # Can be > 1, no idea what it represents
+                draw_landmarks(frame, landmark[:, :2], size=1)
 
         cv2.imshow('frame', frame)
 
@@ -202,14 +231,19 @@ def recognize_from_video():
 
     capture.release()
     cv2.destroyAllWindows()
-    print('Script finished successfully.')
-    pass
+    if writer is not None:
+        writer.release()
+    logger.info('Script finished successfully.')
 
 
 def main():
     # model files check and download
-    check_and_download_models(DETECTION_WEIGHT_PATH, DETECTION_MODEL_PATH, DETECTION_REMOTE_PATH)
-    check_and_download_models(LANDMARK_WEIGHT_PATH, LANDMARK_MODEL_PATH, LANDMARK_REMOTE_PATH)
+    check_and_download_models(
+        DETECTION_WEIGHT_PATH, DETECTION_MODEL_PATH, DETECTION_REMOTE_PATH
+    )
+    check_and_download_models(
+        LANDMARK_WEIGHT_PATH, LANDMARK_MODEL_PATH, LANDMARK_REMOTE_PATH
+    )
 
     if args.video is not None:
         # video mode
