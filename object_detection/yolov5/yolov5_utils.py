6f369202e24395ce225cb1d678ee90808e9dee58
客野一樹 <kyakuno@axell.co.jp>
2021-01-22 15:57:47 +0900

    Migrate to numpy

--- object_detection/yolov5/yolov5_utils.py
+++ object_detection/yolov5/yolov5_utils.py
@@ -14,21 +14,21 @@ def sigmoid(x):
 
 
 def bbox_iou(box1, box2):
-    #box1 = box1.numpy()
-    #box2 = box2.numpy()
-
-    #https://qiita.com/kzkadc/items/8e39ab0559e753b74ab0
+    box1 = box1.numpy()
+    box2 = box2.numpy()
 
     b1_x1, b1_y1, b1_x2, b1_y2 = box1[:, 0], box1[:, 1], box1[:, 2], box1[:, 3]
     b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
 
-    inter_rect_x1 = torch.max(b1_x1, b2_x1)
-    inter_rect_y1 = torch.max(b1_y1, b2_y1)
-    inter_rect_x2 = torch.min(b1_x2, b2_x2)
-    inter_rect_y2 = torch.min(b1_y2, b2_y2)
+    inter_rect_x1 = np.maximum(b1_x1, b2_x1)
+    inter_rect_y1 = np.maximum(b1_y1, b2_y1)
+    inter_rect_x2 = np.minimum(b1_x2, b2_x2)
+    inter_rect_y2 = np.minimum(b1_y2, b2_y2)
 
-    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * \
-                 torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+    inter_area1 = np.clip(inter_rect_x2 - inter_rect_x1 + 1, 0, None)
+    inter_area2 = np.clip(inter_rect_y2 - inter_rect_y1 + 1, 0, None)
+
+    inter_area = inter_area1 * inter_area2
 
     b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
     b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
