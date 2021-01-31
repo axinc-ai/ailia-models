f0cf1b9190d84f6439066e9d71402b4725c57a25
ytknzw <ytknzw@gmail.com>
2021-01-31 19:48:46 +0900

    Replace torch with numpy in faceutils.mask.

--- style_transfer/psgan/faceutils/mask/main.py
+++ style_transfer/psgan/faceutils/mask/main.py
@@ -1,30 +1,31 @@
 import ailia
 from PIL import Image
-import torch
-import torchvision.transforms as transforms
+import numpy as np
 
 
 class FaceParser:
     def __init__(self, device="cpu", args=None, face_parser_path=None):
         mapper = [0, 1, 2, 3, 4, 5, 0, 11, 12, 0, 6, 8, 7, 9, 13, 0, 0, 10, 0]
         self.device = device
-        self.dic = torch.tensor(mapper, device=device)
+        self.dic = mapper
         self.net = _initialize_net(args, face_parser_path)
         self.args = args
-        self.to_tensor = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-        ])
 
     def parse(self, image: Image):
         assert image.shape[:2] == (512, 512)
-        with torch.no_grad():
-            image = self.to_tensor(image).to(self.device)
-            image = torch.unsqueeze(image, 0)
-            out = _parse_face(image, self.net, self.args)[0]
-            parsing = torch.from_numpy(out.squeeze(0).argmax(0))
-        parsing = torch.nn.functional.embedding(parsing, self.dic)
-        return parsing.float()
+        # Reshaping and normalisation
+        image = image.transpose((2, 0, 1))/255
+        # Standardisation
+        means = np.expand_dims([0.485, 0.456, 0.406], (1,2))
+        stds = np.expand_dims([0.229, 0.224, 0.225], (1,2))
+        image = (image - means)/stds
+        image = np.expand_dims(image, 0).astype('float32')
+        # Parsing
+        out = _parse_face(image, self.net, self.args)[0]
+        parsing = out.squeeze(0).argmax(0)
+        parsing = np.take(self.dic, parsing)
+
+        return parsing.astype('float32')
 
 
 def _initialize_net(args, face_parser_path):
@@ -34,7 +35,6 @@ def _initialize_net(args, face_parser_path):
         net = ailia.Net(face_parser_path[0], face_parser_path[1], env_id=env_id)
     else:
         import onnxruntime
-
         net = onnxruntime.InferenceSession(face_parser_path[1])
 
     return net
@@ -42,11 +42,7 @@ def _initialize_net(args, face_parser_path):
 
 def _parse_face(input_image, net, args):
     if not args.onnx:
-        return net.predict([_to_numpy(input_image)])
+        return net.predict([input_image])
     else:
-        inputs = {net.get_inputs()[0].name: _to_numpy(input_image)}
+        inputs = {net.get_inputs()[0].name: input_image}
         return net.run(None, inputs)
-
-
-def _to_numpy(tensor):
-    return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
