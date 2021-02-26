56451094828b453e10160795f5658e886a3e1416
hasuike tomonori <ktenoishiht@gmail.com>
2021-02-26 21:09:34 +0900

    add write prediction for video

--- object_detection/yolov4/yolov4.py
+++ object_detection/yolov4/yolov4.py
@@ -1,3 +1,5 @@
+import os
+import math
 import sys
 import time
 
@@ -167,6 +169,9 @@ def recognize_from_video():
     else:
         writer = None
 
+    frame_count = 0
+    frame_digit = int(math.log10(capture.get(cv2.CAP_PROP_FRAME_COUNT)) + 1)
+    video_name = os.path.splitext(os.path.basename(args.video))[0]
     while (True):
         ret, frame = capture.read()
         if (cv2.waitKey(1) & 0xFF == ord('q')) or not ret:
@@ -191,6 +196,14 @@ def recognize_from_video():
         # save results
         if writer is not None:
             writer.write(res_img)
+        
+        # write prediction
+        if args.write_prediction:
+            savepath = get_savepath(args.savepath, video_name, post_fix = '_%s' % (str(frame_count).zfill(frame_digit) + '_res'), ext='.png')
+            pred_file = '%s.txt' % savepath.rsplit('.', 1)[0]
+            write_predictions(pred_file, detect_object, frame, COCO_CATEGORY)
+
+        frame_count += 1
 
     capture.release()
     cv2.destroyAllWindows()
