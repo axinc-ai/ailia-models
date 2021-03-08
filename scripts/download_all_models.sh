eff1f2e253c652c6d59c79344f15978741f4afb5
satoshi.ooe <ooe1123@gmail.com>
2021-03-08 22:51:03 +0900

    Implement hand3d

--- scripts/download_all_models.sh
+++ scripts/download_all_models.sh
@@ -44,6 +44,7 @@ cd ../../hand_detection/yolov3-hand; python3 yolov3-hand.py ${OPTION}
 cd ../../hand_detection/hand_detection_pytorch python3 hand_detection_pytorch.py ${OPTION}
 cd ../../hand_detection/blazepalm; python3 blazepalm.py ${OPTION}
 cd ../../hand_recognition/blazehand; python3 blazehand.py ${OPTION}
+cd ../../hand_recognition/hand3d; python3 hand3d.py ${OPTION}
 cd ../../image_captioning/illustration2vec; python3 illustration2vec.py ${OPTION}
 cd ../../image_captioning/image_captioning_pytorch; python3 image_captioning_pytorch.py ${OPTION}
 cd ../../image_classification/efficientnet; python3 efficientnet.py ${OPTION}
