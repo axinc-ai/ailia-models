065e5845607ad5549e21421bc6e60dae6b0f48cb
satoshi.ooe <ooe1123@gmail.com>
2021-04-03 16:22:22 +0900

    Implement human dtection, 2D pose estimation

--- pose_estimation/gast/gast.py
+++ pose_estimation/gast/gast.py
@@ -5,6 +5,7 @@ import json
 
 import numpy as np
 import cv2
+from tqdm import tqdm
 
 import ailia
 
@@ -27,6 +28,11 @@ logger = getLogger(__name__)
 # Parameters
 # ======================
 
+WEIGHT_YOLOV3_PATH = 'yolov3.opt.onnx'
+MODEL_YOLOV3_PATH = 'yolov3.opt.onnx.prototxt'
+REMOTE_YOLOV3_PATH = 'https://storage.googleapis.com/ailia-models/yolov3/'
+WEIGHT_POSE_PATH = 'pose_hrnet_w48_384x288.onnx'
+MODEL_POSE_PATH = 'pose_hrnet_w48_384x288.onnx.prototxt'
 WEIGHT_27FRAME_17JOINT_PATH = '27_frame_17_joint_model.onnx'
 MODEL_27FRAME_17JOINT_PATH = '27_frame_17_joint_model.onnx.prototxt'
 WEIGHT_27FRAME_19JOINT_PATH = '27_frame_19_joint_model.onnx'
@@ -97,22 +103,192 @@ def load_json(file_path, num_joints, num_person=2):
         return keypoints, scores, label, label_index
 
 
-def recognize_from_video(net, params):
-    video_file = args.input[0]
-    cap = get_capture(video_file)
+def gen_kpts(frames, yolo_model, pose_model, num_peroson=1):
+    # collect keypoints coordinate
+    logger.info('Generating 2D pose ...')
+
+    people_sort = ObjSort()
+
+    kpts_result = []
+    scores_result = []
+    for i in tqdm(range(len(frames))):
+        frame = frames[i]
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+        bboxs, scores = yolo_human_det(img, yolo_model)
+        if not bboxs.any():
+            continue
+
+        # Using Sort to track people
+        people_track = people_sort.update(bboxs)
+
+        # Track the first two people in the video and remove the ID
+        if people_track.shape[0] == 1:
+            people_track_ = people_track[-1, :-1].reshape(1, 4)
+        elif people_track.shape[0] >= 2:
+            people_track_ = people_track[-num_peroson:, :-1].reshape(num_peroson, 4)
+            people_track_ = people_track_[::-1]
+        else:
+            continue
+
+        track_bboxs = []
+        for bbox in people_track_:
+            bbox = [round(i, 2) for i in list(bbox)]
+            track_bboxs.append(bbox)
+
+        inputs, origin_img, center, scale = preprocess(frame, track_bboxs, num_peroson)
+        inputs = inputs[:, [2, 1, 0]]
+
+        output = pose_model.predict({'input': inputs})[0]
+
+        # compute coordinate
+        preds, maxvals = get_final_preds(output, np.asarray(center), np.asarray(scale))
+
+        kpts = np.zeros((num_peroson, 17, 2), dtype=np.float32)
+        scores = np.zeros((num_peroson, 17), dtype=np.float32)
+        for i, kpt in enumerate(preds):
+            kpts[i] = kpt
+
+        for i, score in enumerate(maxvals):
+            scores[i] = score.squeeze()
+
+        kpts_result.append(kpts)
+        scores_result.append(scores)
+
+    keypoints = np.array(kpts_result)
+    scores = np.array(scores_result)
+
+    keypoints = keypoints.transpose(1, 0, 2, 3)  # (T, M, N, 2) --> (M, T, N, 2)
+    scores = scores.transpose(1, 0, 2)  # (T, M, N) --> (M, T, N)
+
+    return keypoints, scores
+
+
+def yolo_human_det(img, detector, confidence=0.70, nms_thresh=0.4):
+    detector.compute(img, confidence, nms_thresh)
+
+    h, w = img.shape[:2]
+    bboxs = []
+    scores = []
+
+    count = detector.get_object_count()
+    for idx in range(count):
+        obj = detector.get_object(idx)
+        if obj.category != 0:
+            continue
+
+        bboxs.append(np.array([
+            obj.x * w,
+            obj.y * h,
+            (obj.x + obj.w) * w,
+            (obj.x + obj.h) * h,
+        ]))
+        scores.append(obj.prob)
+
+    bboxs = np.asarray(bboxs)
+    scores = np.array(scores).reshape(-1, 1)
+
+    return bboxs, scores
+
+
+def box_to_center_scale(box, model_image_width, model_image_height):
+    """convert a box to center,scale information required for pose transformation
+    Parameters
+    ----------
+    box : (x1, y1, x2, y2)
+    model_image_width : int
+    model_image_height : int
+
+    Returns
+    -------
+    (numpy array, numpy array)
+        Two numpy arrays, coordinates for the center of the box and the scale of the box
+    """
+    center = np.zeros((2), dtype=np.float32)
+    x1, y1, x2, y2 = box[:4]
+    box_width, box_height = x2 - x1, y2 - y1
+
+    center[0] = x1 + box_width * 0.5
+    center[1] = y1 + box_height * 0.5
+
+    aspect_ratio = model_image_width * 1.0 / model_image_height
+    pixel_std = 200
+
+    if box_width > aspect_ratio * box_height:
+        box_height = box_width * 1.0 / aspect_ratio
+    elif box_width < aspect_ratio * box_height:
+        box_width = box_height * aspect_ratio
+    scale = np.array(
+        [box_width * 1.0 / pixel_std, box_height * 1.0 / pixel_std],
+        dtype=np.float32)
+    if center[0] != -1:
+        scale = scale * 1.25
+
+    return center, scale
+
+
+def preprocess(img, bboxs, num_pos=2):
+    IMAGE_SIZE = (288, 384)
+
+    inputs = []
+    centers = []
+    scales = []
+    for bbox in bboxs[:num_pos]:
+        c, s = box_to_center_scale(bbox, img.shape[0], img.shape[1])
+        centers.append(c)
+        scales.append(s)
+        r = 0
+
+        trans = get_affine_transform(c, s, r, IMAGE_SIZE)
+        input = cv2.warpAffine(
+            img,
+            trans,
+            (IMAGE_SIZE[0], IMAGE_SIZE[1]),
+            flags=cv2.INTER_LINEAR)
+
+        input = normalize_image(input.astype(np.float32), 'ImageNet')
+        input = input.transpose(2, 0, 1)  # HWC -> CHW
+        input = np.expand_dims(input, axis=0)
+        inputs.append(input)
+
+    inputs = np.vstack(inputs)
+    return inputs, img, centers, scales
+
+
+def recognize_from_video(net, info):
     keypoints_file = args.keypoints_file
 
-    num_joints = params["num_joints"]
+    video_file = args.input[0]
+    cap = get_capture(video_file)
+    assert cap.isOpened(), 'Cannot capture source'
+
+    # Get the width and height of video
+    width = int(round(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
+    height = int(round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
+    # Load video frame
+    video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frames = []
+    for i in range(video_length):
+        ret, frame = cap.read()
+        if not ret:
+            continue
+        frames.append(frame)
+
+    num_person = info["num_person"]
+    keypoints, scores = gen_kpts(
+        frames, info["yolo_model"], info["pose_net"],
+        num_peroson=num_person)
+    # keypoints, scores, valid_frames = h36m_coco_format(keypoints, scores)
+    # re_kpts = revise_kpts(keypoints, scores, valid_frames)
+    # num_person = len(re_kpts)
+
+    num_joints = info["num_joints"]
 
     logger.info('Loading 2D keypoints ...')
     keypoints, scores, _, _ = load_json(keypoints_file, num_joints)
     keypoints = keypoints[0]
 
     keypoints, valid_frames = coco_h36m(keypoints)
-
-    # Get the width and height of video
-    width = int(round(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
-    height = int(round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
     # normalize keypoints
     input_keypoints = normalize_screen_coordinates(
@@ -170,27 +346,49 @@ def recognize_from_video(net, params):
 
 
 def main():
-    info = {
+    # model files check and download
+    logger.info("=== YOLOv3 model ===")
+    check_and_download_models(WEIGHT_YOLOV3_PATH, MODEL_YOLOV3_PATH, REMOTE_YOLOV3_PATH)
+    logger.info("=== HRNet model ===")
+    check_and_download_models(WEIGHT_POSE_PATH, MODEL_POSE_PATH, REMOTE_PATH)
+
+    model_info = {
         17: (
             WEIGHT_27FRAME_17JOINT_PATH, MODEL_27FRAME_17JOINT_PATH),
         19: (
             WEIGHT_27FRAME_19JOINT_PATH, MODEL_27FRAME_19JOINT_PATH),
     }
-    # model files check and download
-    # weight_path, model_path = info[0]
-    weight_path, model_path = info[17]
+    logger.info("=== GAST model ===")
+    weight_path, model_path = model_info[17]
     check_and_download_models(weight_path, model_path, REMOTE_PATH)
 
     num_joints = 17
-    params = {
-        "num_joints": num_joints,
-    }
+    num_person = 2
 
     # net initialize
     import onnxruntime
     net = onnxruntime.InferenceSession(weight_path)
 
-    recognize_from_video(net, params)
+    detector = ailia.Detector(
+        MODEL_YOLOV3_PATH,
+        WEIGHT_YOLOV3_PATH,
+        80,
+        format=ailia.NETWORK_IMAGE_FORMAT_RGB,
+        channel=ailia.NETWORK_IMAGE_CHANNEL_FIRST,
+        range=ailia.NETWORK_IMAGE_RANGE_U_FP32,
+        algorithm=ailia.DETECTOR_ALGORITHM_YOLOV3,
+        env_id=args.env_id,
+    )
+    pose_net = ailia.Net(MODEL_POSE_PATH, WEIGHT_POSE_PATH, env_id=args.env_id)
+
+    info = {
+        "yolo_model": detector,
+        "pose_net": pose_net,
+        "num_joints": num_joints,
+        "num_person": num_person,
+    }
+
+    recognize_from_video(net, info)
 
 
 if __name__ == '__main__':
--- pose_estimation/gast/gast_utils.py
+++ pose_estimation/gast/gast_utils.py
@@ -1,20 +1,26 @@
-import matplotlib
+import math
 
+import matplotlib
 matplotlib.use('Agg')
 
 import numpy as np
+from scipy.optimize import linear_sum_assignment
 import cv2
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, writers
 from mpl_toolkits.mplot3d import axes3d, Axes3D
+from filterpy.kalman import KalmanFilter
 
 __all__ = [
     'DataLoader',
+    'ObjSort',
     'coco_h36m',
     'get_joints_info',
     'normalize_screen_coordinates',
     'receptive_field',
     'camera_to_world',
+    'get_affine_transform',
+    'get_final_preds',
     'render_animation',
 ]
 
@@ -66,6 +72,217 @@ class Skeleton:
 
     def parents(self):
         return self._parents
+
+
+def iou(bb_test, bb_gt):
+    """
+    Computes IUO between two bboxes in the form [x1,y1,x2,y2]
+    """
+    xx1 = np.maximum(bb_test[0], bb_gt[0])
+    yy1 = np.maximum(bb_test[1], bb_gt[1])
+    xx2 = np.minimum(bb_test[2], bb_gt[2])
+    yy2 = np.minimum(bb_test[3], bb_gt[3])
+    w = np.maximum(0., xx2 - xx1)
+    h = np.maximum(0., yy2 - yy1)
+    wh = w * h
+    o = wh / ((bb_test[2] - bb_test[0]) * (bb_test[3] - bb_test[1])
+              + (bb_gt[2] - bb_gt[0]) * (bb_gt[3] - bb_gt[1]) - wh)
+
+    return o
+
+
+def convert_bbox_to_z(bbox):
+    """
+    Takes a bounding box in the form [x1,y1,x2,y2] and returns z in the form
+      [x,y,s,r] where x,y is the centre of the box and s is the scale/area and r is
+      the aspect ratio
+    """
+    w = bbox[2] - bbox[0]
+    h = bbox[3] - bbox[1]
+    x = bbox[0] + w / 2.
+    y = bbox[1] + h / 2.
+    s = w * h  # scale is just area
+    r = w / float(h)
+    return np.array([x, y, s, r]).reshape((4, 1))
+
+
+def convert_x_to_bbox(x, score=None):
+    """
+    Takes a bounding box in the centre form [x,y,s,r] and returns it in the form
+      [x1,y1,x2,y2] where x1,y1 is the top left and x2,y2 is the bottom right
+    """
+    w = np.sqrt(x[2] * x[3])
+    h = x[2] / w
+    if (score == None):
+        return np.array([x[0] - w / 2., x[1] - h / 2., x[0] + w / 2., x[1] + h / 2.]).reshape((1, 4))
+    else:
+        return np.array([x[0] - w / 2., x[1] - h / 2., x[0] + w / 2., x[1] + h / 2., score]).reshape((1, 5))
+
+
+class KalmanBoxTracker(object):
+    """
+    This class represents the internel state of individual tracked objects observed as bbox.
+    """
+    count = 0
+
+    def __init__(self, bbox):
+        """
+        Initialises a tracker using initial bounding box.
+        """
+        # define constant velocity model
+        self.kf = KalmanFilter(dim_x=7, dim_z=4)
+        self.kf.F = np.array(
+            [[1, 0, 0, 0, 1, 0, 0], [0, 1, 0, 0, 0, 1, 0], [0, 0, 1, 0, 0, 0, 1], [0, 0, 0, 1, 0, 0, 0],
+             [0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 1]])
+        self.kf.H = np.array(
+            [[1, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0]])
+
+        self.kf.R[2:, 2:] *= 10.
+        self.kf.P[4:, 4:] *= 1000.  # give high uncertainty to the unobservable initial velocities
+        self.kf.P *= 10.
+        self.kf.Q[-1, -1] *= 0.01
+        self.kf.Q[4:, 4:] *= 0.01
+
+        self.kf.x[:4] = convert_bbox_to_z(bbox)
+        self.time_since_update = 0
+        self.id = KalmanBoxTracker.count
+        KalmanBoxTracker.count += 1
+        self.history = []
+        self.hits = 0
+        self.hit_streak = 0
+        self.age = 0
+
+    def update(self, bbox):
+        """
+        Updates the state vector with observed bbox.
+        """
+        self.time_since_update = 0
+        self.history = []
+        self.hits += 1
+        self.hit_streak += 1
+        self.kf.update(convert_bbox_to_z(bbox))
+
+    def predict(self):
+        """
+        Advances the state vector and returns the predicted bounding box estimate.
+        """
+        if ((self.kf.x[6] + self.kf.x[2]) <= 0):
+            self.kf.x[6] *= 0.0
+        self.kf.predict()
+        self.age += 1
+        if (self.time_since_update > 0):
+            self.hit_streak = 0
+        self.time_since_update += 1
+        self.history.append(convert_x_to_bbox(self.kf.x))
+        return self.history[-1]
+
+    def get_state(self):
+        """
+        Returns the current bounding box estimate.
+        """
+        return convert_x_to_bbox(self.kf.x)
+
+
+def associate_detections_to_trackers(detections, trackers, iou_threshold=0.3):
+    """
+    Assigns detections to tracked object (both represented as bounding boxes)
+
+    Returns 3 lists of matches, unmatched_detections and unmatched_trackers
+    """
+    if (len(trackers) == 0):
+        return np.empty((0, 2), dtype=int), np.arange(len(detections)), np.empty((0, 5), dtype=int)
+    iou_matrix = np.zeros((len(detections), len(trackers)), dtype=np.float32)
+
+    for d, det in enumerate(detections):
+        for t, trk in enumerate(trackers):
+            iou_matrix[d, t] = iou(det, trk)
+    matched_indices = linear_sum_assignment(-iou_matrix)
+    matched_indices = np.asarray(matched_indices)
+    matched_indices = matched_indices.transpose()
+
+    unmatched_detections = []
+    for d, det in enumerate(detections):
+        if (d not in matched_indices[:, 0]):
+            unmatched_detections.append(d)
+    unmatched_trackers = []
+    for t, trk in enumerate(trackers):
+        if (t not in matched_indices[:, 1]):
+            unmatched_trackers.append(t)
+
+    # filter out matched with low IOU
+    matches = []
+    for m in matched_indices:
+        if (iou_matrix[m[0], m[1]] < iou_threshold):
+            unmatched_detections.append(m[0])
+            unmatched_trackers.append(m[1])
+        else:
+            matches.append(m.reshape(1, 2))
+    if (len(matches) == 0):
+        matches = np.empty((0, 2), dtype=int)
+    else:
+        matches = np.concatenate(matches, axis=0)
+
+    return matches, np.array(unmatched_detections), np.array(unmatched_trackers)
+
+
+class ObjSort(object):
+    def __init__(self, max_age=1, min_hits=3):
+        """
+        Sets key parameters for SORT
+        """
+        self.max_age = max_age
+        self.min_hits = min_hits
+        self.trackers = []
+        self.frame_count = 0
+
+    def update(self, dets):
+        """
+        Params:
+          dets - a numpy array of detections in the format [[x1,y1,x2,y2,score],[x1,y1,x2,y2,score],...]
+        Requires: this method must be called once for each frame even with empty detections.
+        Returns the a similar array, where the last column is the object ID.
+
+        NOTE: The number of objects returned may differ from the number of detections provided.
+        """
+        self.frame_count += 1
+        # get predicted locations from existing trackers.
+        trks = np.zeros((len(self.trackers), 5))
+        to_del = []
+        ret = []
+        for t, trk in enumerate(trks):
+            pos = self.trackers[t].predict()[0]
+            trk[:] = [pos[0], pos[1], pos[2], pos[3], 0]
+            if np.any(np.isnan(pos)):
+                to_del.append(t)
+        trks = np.ma.compress_rows(np.ma.masked_invalid(trks))
+        for t in reversed(to_del):
+            self.trackers.pop(t)
+        matched, unmatched_dets, unmatched_trks = associate_detections_to_trackers(dets, trks)
+
+        # update matched trackers with assigned detections
+        for t, trk in enumerate(self.trackers):
+            if t not in unmatched_trks:
+                d = matched[np.where(matched[:, 1] == t)[0], 0]  # d: [n]
+                trk.update(dets[d, :][0])
+
+        # create and initialise new trackers for unmatched detections
+        for i in unmatched_dets:
+            trk = KalmanBoxTracker(dets[i, :])
+            self.trackers.append(trk)
+        i = len(self.trackers)
+        for trk in reversed(self.trackers):
+            d = trk.get_state()[0]
+            if ((trk.time_since_update < 1) and
+                    (trk.hit_streak >= self.min_hits or self.frame_count <= self.min_hits)):
+                # +1 as MOT benchmark requires positive
+                ret.append(np.concatenate((d, [trk.id + 1])).reshape(1, -1))
+            i -= 1
+            # remove dead tracklet
+            if (trk.time_since_update > self.max_age):
+                self.trackers.pop(i)
+        if (len(ret) > 0):
+            return np.concatenate(ret)
+        return np.empty((0, 5))
 
 
 def coco_h36m(keypoints):
@@ -234,6 +451,132 @@ def downsample_tensor(X, factor):
     return np.mean(X[:length].reshape(-1, factor, *X.shape[1:]), axis=1)
 
 
+def affine_transform(pt, t):
+    new_pt = np.array([pt[0], pt[1], 1.]).T
+    new_pt = np.dot(t, new_pt)
+    return new_pt[:2]
+
+
+def get_3rd_point(a, b):
+    direct = a - b
+    return b + np.array([-direct[1], direct[0]], dtype=np.float32)
+
+
+def get_dir(src_point, rot_rad):
+    sn, cs = np.sin(rot_rad), np.cos(rot_rad)
+
+    src_result = [0, 0]
+    src_result[0] = src_point[0] * cs - src_point[1] * sn
+    src_result[1] = src_point[0] * sn + src_point[1] * cs
+
+    return src_result
+
+
+def transform_preds(coords, center, scale, output_size):
+    target_coords = np.zeros(coords.shape)
+    trans = get_affine_transform(center, scale, 0, output_size, inv=1)
+    for p in range(coords.shape[0]):
+        target_coords[p, 0:2] = affine_transform(coords[p, 0:2], trans)
+    return target_coords
+
+
+def get_affine_transform(
+        center, scale, rot, output_size,
+        shift=np.array([0, 0], dtype=np.float32), inv=0):
+    if not isinstance(scale, np.ndarray) and not isinstance(scale, list):
+        print(scale)
+        scale = np.array([scale, scale])
+
+    scale_tmp = scale * 200.0
+    src_w = scale_tmp[0]
+    dst_w = output_size[0]
+    dst_h = output_size[1]
+
+    rot_rad = np.pi * rot / 180
+    src_dir = get_dir([0, src_w * -0.5], rot_rad)
+    dst_dir = np.array([0, dst_w * -0.5], np.float32)
+
+    src = np.zeros((3, 2), dtype=np.float32)
+    dst = np.zeros((3, 2), dtype=np.float32)
+    src[0, :] = center + scale_tmp * shift
+    src[1, :] = center + src_dir + scale_tmp * shift
+    dst[0, :] = [dst_w * 0.5, dst_h * 0.5]
+    dst[1, :] = np.array([dst_w * 0.5, dst_h * 0.5]) + dst_dir
+
+    src[2:, :] = get_3rd_point(src[0, :], src[1, :])
+    dst[2:, :] = get_3rd_point(dst[0, :], dst[1, :])
+
+    if inv:
+        trans = cv2.getAffineTransform(np.float32(dst), np.float32(src))
+    else:
+        trans = cv2.getAffineTransform(np.float32(src), np.float32(dst))
+
+    return trans
+
+
+def get_max_preds(batch_heatmaps):
+    '''
+    get predictions from score maps
+    heatmaps: numpy.ndarray([batch_size, num_joints, height, width])
+    '''
+    assert isinstance(batch_heatmaps, np.ndarray), \
+        'batch_heatmaps should be numpy.ndarray'
+    assert batch_heatmaps.ndim == 4, 'batch_images should be 4-ndim'
+
+    batch_size = batch_heatmaps.shape[0]
+    num_joints = batch_heatmaps.shape[1]
+    width = batch_heatmaps.shape[3]
+    heatmaps_reshaped = batch_heatmaps.reshape((batch_size, num_joints, -1))
+    idx = np.argmax(heatmaps_reshaped, 2)
+    maxvals = np.amax(heatmaps_reshaped, 2)
+
+    maxvals = maxvals.reshape((batch_size, num_joints, 1))
+    idx = idx.reshape((batch_size, num_joints, 1))
+
+    preds = np.tile(idx, (1, 1, 2)).astype(np.float32)
+
+    preds[:, :, 0] = (preds[:, :, 0]) % width
+    preds[:, :, 1] = np.floor((preds[:, :, 1]) / width)
+
+    pred_mask = np.tile(np.greater(maxvals, 0.0), (1, 1, 2))
+    pred_mask = pred_mask.astype(np.float32)
+
+    preds *= pred_mask
+    return preds, maxvals
+
+
+def get_final_preds(batch_heatmaps, center, scale):
+    coords, maxvals = get_max_preds(batch_heatmaps)
+
+    heatmap_height = batch_heatmaps.shape[2]
+    heatmap_width = batch_heatmaps.shape[3]
+
+    # post-processing
+    for n in range(coords.shape[0]):
+        for p in range(coords.shape[1]):
+            hm = batch_heatmaps[n][p]
+            px = int(math.floor(coords[n][p][0] + 0.5))
+            py = int(math.floor(coords[n][p][1] + 0.5))
+            if 1 < px < heatmap_width - 1 and 1 < py < heatmap_height - 1:
+                diff = np.array(
+                    [
+                        hm[py][px + 1] - hm[py][px - 1],
+                        hm[py + 1][px] - hm[py - 1][px]
+                    ]
+                )
+                coords[n][p] += np.sign(diff) * .25
+
+    preds = coords.copy()
+
+    # Transform back
+    for i in range(coords.shape[0]):
+        preds[i] = transform_preds(
+            coords[i], center[i], scale[i], [heatmap_width, heatmap_height]
+        )
+
+    return preds, maxvals
+
+
 def render_animation(
         keypoints, keypoints_metadata, poses, skeleton, fps, bitrate,
         azim, output,
@@ -276,7 +619,7 @@ def render_animation(
         trajectories.append(data[:, 0, [0, 1]])
     poses = list(poses.values())
 
-    # Load video using ffmpeg
+    # Load video frame
     all_frames = []
     while True:
         ret, frame = capture.read()
