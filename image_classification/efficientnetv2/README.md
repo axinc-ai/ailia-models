33c5df6a195051c57f2d6384ecd24bee17eedf20
畠山剛 <stayhungry.stayfoolish.1990@gmail.com>
2021-06-30 00:51:45 +0900

    Implement efficientnetv2

---
+++ image_classification/efficientnetv2/README.md
@@ -0,0 +1,60 @@
+# efficientnetv2
+
+## Input
+
+![Input](input.jpg)
+
+Shape : (1,224,224,3)
+
+## Output
+
+```
+class_count=3
++ idx=0
+  category=409[analog clock ]
+  prob=9.556556701660156
++ idx=1
+  category=892[wall clock ]
+  prob=7.525008201599121
++ idx=2
+  category=426[barometer ]
+  prob=4.037744522094727
+```
+
+## Usage
+Automatically downloads the onnx and prototxt files on the first run.
+It is necessary to be connected to the Internet while downloading.
+
+For the sample image,
+``` bash
+$ python3 efficientnetv2.py
+```
+
+If you want to specify the input image, put the image path after the `--input` option.  
+```bash
+$ python3 efficientnetv2.py --input IMAGE_PATH
+```
+
+By adding the `--video` option, you can input the video.   
+If you pass `0` as an argument to VIDEO_PATH, you can use the webcam input instead of the video file.
+```bash
+$ python3 efficientnetv2.py --video VIDEO_PATH
+```
+
+
+## Reference
+
+[EfficientNetV2]( https://github.com/google/automl/tree/master/efficientnetv2 )
+
+
+## Model Format
+
+ONNX opset = 11
+
+## Framework
+
+Tensorflow
+
+## Netron
+
+[efficientnetv2.prototxt](https://netron.app/?url=https://storage.googleapis.com/ailia-models/efficientnetv2/efficientnetv2-b0.onnx.prototxt)
---
+++ image_classification/efficientnetv2/efficientnetv2.py
@@ -0,0 +1,132 @@
+import sys
+import time
+import cv2
+
+import ailia
+import efficientnetv2_labels
+
+# import original modules
+sys.path.append('../../util')
+from utils import get_base_parser, update_parser  # noqa: E402
+from model_utils import check_and_download_models  # noqa: E402
+from image_utils import load_image  # noqa: E402
+from classifier_utils import plot_results, print_results  # noqa: E402
+import webcamera_utils  # noqa: E402
+
+# logger
+from logging import getLogger   # noqa: E402
+logger = getLogger(__name__)
+
+
+# ======================
+# Parameters
+# ======================
+WEIGHT_PATH = 'efficientnetv2-b0.opt.onnx'
+MODEL_PATH = 'efficientnetv2-b0.opt.onnx.prototxt'
+REMOTE_PATH = 'https://storage.googleapis.com/ailia-models/efficientnetv2/'
+
+IMAGE_PATH = 'input.jpg'
+IMAGE_HEIGHT = 224
+IMAGE_WIDTH = 224
+SLEEP_TIME = 0
+
+# ======================
+# Arguemnt Parser Config
+# ======================
+parser = get_base_parser('Image classification model: efficientnetv2', IMAGE_PATH, None)
+args = update_parser(parser)
+
+
+# ======================
+# Main functions
+# ======================
+def recognize_from_image():
+    # net initialize
+    net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=args.env_id)
+
+    # input image loop
+    for image_path in args.input:
+        # prepare input data
+        logger.info(image_path)
+        input_data = load_image(
+            image_path,
+            (IMAGE_HEIGHT, IMAGE_WIDTH),
+            normalize_type='ImageNet',
+            gen_input_ailia=True,
+        )
+        input_data = input_data.transpose(0,2,3,1)
+
+        # inference
+        logger.info('Start inference...')
+        if args.benchmark:
+            logger.info('BENCHMARK mode')
+            for i in range(args.benchmark_count):
+                start = int(round(time.time() * 1000))
+                preds_ailia = net.predict(input_data)
+                end = int(round(time.time() * 1000))
+                logger.info(f'\tailia processing time {end - start} ms')
+        else:
+            preds_ailia = net.predict(input_data)
+
+        # postprocessing
+        print_results(preds_ailia, efficientnetv2_labels.imagenet_category)
+    logger.info('Script finished successfully.')
+
+
+def recognize_from_video():
+    # net initialize
+    net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=args.env_id)
+
+    capture = webcamera_utils.get_capture(args.video)
+
+    # create video writer if savepath is specified as video format
+    if args.savepath is not None:
+        f_h = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        f_w = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        writer = webcamera_utils.get_writer(args.savepath, f_h, f_w)
+    else:
+        writer = None
+
+    while(True):
+        ret, frame = capture.read()
+        if (cv2.waitKey(1) & 0xFF == ord('q')) or not ret:
+            break
+
+        _, input_data = webcamera_utils.preprocess_frame(
+            frame, IMAGE_HEIGHT, IMAGE_WIDTH, normalize_type='ImageNet'
+        )
+        input_data = input_data.transpose(0, 2, 3, 1)
+
+        # inference
+        preds_ailia = net.predict(input_data)
+
+        # postprocessing
+        plot_results(frame, preds_ailia, efficientnetv2_labels.imagenet_category)
+        cv2.imshow('frame', frame)
+        time.sleep(SLEEP_TIME)
+
+        # save results
+        if writer is not None:
+            writer.write(frame)
+
+    capture.release()
+    cv2.destroyAllWindows()
+    if writer is not None:
+        writer.release()
+    logger.info('Script finished successfully.')
+
+
+def main():
+    # model files check and download
+    check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
+
+    if args.video is not None:
+        # video mode
+        recognize_from_video()
+    else:
+        # image mode
+        recognize_from_image()
+
+
+if __name__ == '__main__':
+    main()
