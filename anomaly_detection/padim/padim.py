3f0b185c20fbd2af8dc78c1527229d74132113ad
mucunwuxian <mucun.wuxian@gmail.com>
2021-07-05 11:48:51 +0900

    add TTA function, remaining task is code organization...

--- anomaly_detection/padim/padim.py
+++ anomaly_detection/padim/padim.py
@@ -126,7 +126,7 @@ def preprocess(img, mask=False):
     return img
 
 
-def preprocess_aug(img, mask=False, angle_range=[-10, 10], 
+def preprocess_aug(img, mask=False, angle_range=[-10, 10],
                    angle=None, pad_h=None, pad_w=None):
     h, w = img.shape[:2]
     size = IMAGE_RESIZE
@@ -139,27 +139,30 @@ def preprocess_aug(img, mask=False, angle_range=[-10, 10],
         size = (int(size * w / h), size)
     img = np.array(Image.fromarray(img).resize(
         size, resample=Image.ANTIALIAS if not mask else Image.NEAREST))
+    
+    # for visualize
+    img_resized = img.copy()
 
     # random rotate
-    if (angle is None):
-        angle = np.random.randint(angle_range[0], angle_range[0] + 1)
-    rot_mat = cv2.getRotationMatrix2D((w / 2, h / 2), angle, 1)
-    if (mask is True):
-        interpolation = cv2.INTER_NEAREST
-    else:
-        interpolation = cv2.INTER_LINEAR
-    img = cv2.warpAffine(src=img,
-                         M=rot_mat,
-                         dsize=(w, h),
-                         borderMode=cv2.BORDER_REPLICATE,
-                         flags=interpolation)
+    if not mask:
+        h, w = img.shape[:2]
+        if (angle is None):
+            angle = np.random.randint(angle_range[0], angle_range[0] + 1)
+        rot_mat = cv2.getRotationMatrix2D((w/2, h/2), angle, 1)
+        img = cv2.warpAffine(src=img,
+                             M=rot_mat,
+                             dsize=(w, h),
+                             borderMode=cv2.BORDER_REPLICATE,
+                             flags=cv2.INTER_LINEAR)
 
     # random crop
-    if (pad_h is None):
-        pad_h = np.random.randint(0, (h - crop_size))
-    if (pad_w is None):
-        pad_w = np.random.randint(0, (w - crop_size))
-    img = img[pad_h:pad_h + crop_size, pad_w:pad_w + crop_size, :]
+    if not mask:
+        h, w = img.shape[:2]
+        if (pad_h is None):
+            pad_h = np.random.randint(0, (h - crop_size))
+        if (pad_w is None):
+            pad_w = np.random.randint(0, (w - crop_size))
+        img = img[pad_h:pad_h + crop_size, pad_w:pad_w + crop_size, :]
 
     # normalize
     if not mask:
@@ -170,7 +173,7 @@ def preprocess_aug(img, mask=False, angle_range=[-10, 10],
     img = img.transpose(2, 0, 1)  # HWC -> CHW
     img = np.expand_dims(img, axis=0)
 
-    return img, angle, pad_h, pad_w
+    return img, img_resized, angle, pad_h, pad_w
 
 
 def postprocess(outputs):
@@ -201,26 +204,29 @@ def get_train_outputs(net, create_net, params):
         logger.error("train images not found in '%s'" % train_dir)
         sys.exit(-1)
 
-    logger.info('extract train set features')
-
-    if args.aug:
-        aug_num = args.aug_num
-    else:
+    if (not args.aug):
+        logger.info('extract train set features with no augmentation')
         aug_num = 1
+    else:
+        logger.info('extract train set features with augmentation')
+        aug_num = args.aug_num
     mean = None
     N = 0
     for i_aug in range(aug_num):
         for i_img in range(0, len(train_imgs), batch_size):
             # prepare input data
             imgs = []
+            logger.info('from (%s ~ %s) on augmenation lap %d' % 
+                        (train_imgs[i_img], 
+                         train_imgs[min(len(train_imgs) - 1,
+                                        i_img + batch_size)], i_aug))
             for image_path in train_imgs[i_img:i_img + batch_size]:
-                logger.info(image_path)
                 img = load_image(image_path)
                 img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
                 if (not args.aug):
                     img = preprocess(img)
                 else:
-                    img, _, _, _ = preprocess_aug(img)
+                    img, _, _, _, _ = preprocess_aug(img)
                 imgs.append(img)
 
             # countup N
@@ -233,6 +239,7 @@ def get_train_outputs(net, create_net, params):
                 net = create_net()
             net.set_input_shape(imgs.shape)
 
+            # inference
             _ = net.predict(imgs)
 
             train_outputs = OrderedDict([
@@ -249,7 +256,7 @@ def get_train_outputs(net, create_net, params):
             idx = params['idx']
             embedding_vectors = embedding_vectors[:, idx, :, :]
 
-            # calculate multivariate Gaussian distribution
+            # reshape 2d pixels to 1d features
             B, C, H, W = embedding_vectors.shape
             embedding_vectors = embedding_vectors.reshape(B, C, H * W)
 
@@ -259,7 +266,8 @@ def get_train_outputs(net, create_net, params):
                 cov = np.zeros((C, C, H * W), dtype=np.float32)
                 I = np.identity(C)
 
-            # add up mean and covariance matrix
+            # calculate multivariate Gaussian distribution
+            # (add up mean and covariance matrix)
             mean += np.sum(embedding_vectors, axis=0)
             for i in range(H * W):
                 # https://github.com/numpy/numpy/blob/v1.21.0/numpy/lib/function_base.py#L2324-L2543
@@ -298,7 +306,8 @@ def plot_fig(file_list, test_imgs, scores, anormal_scores, gt_imgs, threshold, s
     for i in range(num):
         image_path = file_list[i]
         img = test_imgs[i]
-        img = denormalization(img)
+        if (not args.aug):
+            img = denormalization(img)
         gt = gt_imgs[i]
         gt = gt.transpose(1, 2, 0).squeeze()
         heat_map = scores[i] * 255
@@ -308,6 +317,8 @@ def plot_fig(file_list, test_imgs, scores, anormal_scores, gt_imgs, threshold, s
         kernel = morphology.disk(4)
         mask = morphology.opening(mask, kernel)
         mask *= 255
+        print('np.shape(img) =', np.shape(img))
+        print('np.shape(mask) =', np.shape(mask))
         vis_img = mark_boundaries(img, mask, color=(1, 0, 0), mode='thick')
 
         fig_img, ax_img = plt.subplots(1, 5, figsize=(12, 3))
@@ -366,11 +377,12 @@ def recognize_from_image(net, create_net, params):
     gt_type_dir = args.gt_dir if args.gt_dir else None
     test_imgs = []
     gt_imgs = []
+    angle_list = []
+    pad_h_list = []
+    pad_w_list = []
 
-    # input image loop
-    test_outputs = OrderedDict([
-        ('layer1', []), ('layer2', []), ('layer3', [])
-    ])
+    N = 0    
+    dist_list = []
     if args.aug:
         aug_num = args.aug_num
     else:
@@ -385,8 +397,13 @@ def recognize_from_image(net, create_net, params):
                 img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
                 if (not args.aug):
                     img = preprocess(img)
+                    test_imgs.append(img[0])
                 else:
-                    img, angle, pad_h, pad_w = preprocess_aug(img)
+                    img, img_resized, angle, pad_h, pad_w = preprocess_aug(img)
+                    test_imgs.append(img_resized)
+                    angle_list.append(angle)
+                    pad_h_list.append(pad_h)
+                    pad_w_list.append(pad_w)
                 imgs.append(img)
 
                 # ground truth
@@ -399,15 +416,23 @@ def recognize_from_image(net, create_net, params):
                         gt_img = cv2.cvtColor(gt_img, cv2.COLOR_BGRA2RGB)
                         if (not args.aug):
                             gt_img = preprocess(gt_img, mask=True)
+                            if gt_img is not None:
+                                gt_img = gt_img[0, [0]]
+                            else:
+                                gt_img = np.zeros((1, IMAGE_SIZE, IMAGE_SIZE))
                         else:
-                            gt_img, _, _, _ = preprocess_aug(gt_img, angle=angle,
-                                                             pad_h=pad_h, pad_w=pad_w)
-                        gt_img = np.mean(gt_img, axis=1, keepdims=True)
+                            gt_img, _, _, _, _ = preprocess_aug(gt_img, mask=True,
+                                                                angle=angle,
+                                                                pad_h=pad_h, pad_w=pad_w)
+                            if gt_img is not None:
+                                gt_img = gt_img[0, [0]]
+                            else:
+                                gt_img = np.zeros((1, IMAGE_RESIZE, IMAGE_RESIZE))
 
-                gt_img = gt_img[0] if gt_img is not None else np.zeros((1, IMAGE_SIZE, IMAGE_SIZE))
-
-                test_imgs.append(img[0])
                 gt_imgs.append(gt_img)
+
+            # countup N
+            N += len(imgs)
 
             imgs = np.vstack(imgs)
 
@@ -417,7 +442,6 @@ def recognize_from_image(net, create_net, params):
             net.set_input_shape(imgs.shape)
 
             # inference
-            logger.info('Start inference...')
             if args.benchmark:
                 logger.info('BENCHMARK mode')
                 total_time = 0
@@ -432,37 +456,69 @@ def recognize_from_image(net, create_net, params):
             else:
                 _ = net.predict(imgs)
 
+            test_outputs = OrderedDict([
+                ('layer1', []), ('layer2', []), ('layer3', [])
+            ])
             for key, name in zip(test_outputs.keys(), params["feat_names"]):
                 test_outputs[key].append(net.get_blob_data(name))
+            for k, v in test_outputs.items():
+                test_outputs[k] = v[0]
 
-    logger.info('postprocessing...')
+            embedding_vectors = postprocess(test_outputs)
 
-    for k, v in test_outputs.items():
-        test_outputs[k] = np.vstack(v)
+            # randomly select d dimension
+            embedding_vectors = embedding_vectors[:, idx, :, :]
 
-    embedding_vectors = postprocess(test_outputs)
+            # reshape 2d pixels to 1d features
+            B, C, H, W = embedding_vectors.shape
+            embedding_vectors = embedding_vectors.reshape(B, C, H * W)
 
-    # randomly select d dimension
-    embedding_vectors = embedding_vectors[:, idx, :, :]
+            # calculate distance matrix
+            dist_tmp = np.zeros([B, (H*W)])
+            for i in range(H * W):
+                mean = train_outputs[0][:, i]
+                conv_inv = np.linalg.inv(train_outputs[1][:, :, i])
+                dist = [mahalanobis(sample[:, i], mean, conv_inv) for sample in embedding_vectors]
+                dist_tmp[:, i] = dist
+            dist_list.append(dist_tmp)
 
-    # calculate distance matrix
-    B, C, H, W = embedding_vectors.shape
-    embedding_vectors = embedding_vectors.reshape(B, C, H * W)
-    dist_list = []
-    for i in range(H * W):
-        mean = train_outputs[0][:, i]
-        conv_inv = np.linalg.inv(train_outputs[1][:, :, i])
-        dist = [mahalanobis(sample[:, i], mean, conv_inv) for sample in embedding_vectors]
-        dist_list.append(dist)
+    dist_list = np.vstack(dist_list)
+    dist_list = dist_list.reshape(N, H, W)
 
-    dist_list = np.array(dist_list).transpose(1, 0).reshape(B, H, W)
-
-    # upsample
-    score_map = np.asarray([
-        np.array(Image.fromarray(s).resize(
-            (IMAGE_SIZE, IMAGE_SIZE), resample=Image.BILINEAR)
-        ) for s in dist_list
-    ])
+    if (not args.aug):
+        # upsample
+        score_map = np.asarray([
+            np.array(Image.fromarray(s).resize(
+                (IMAGE_SIZE, IMAGE_SIZE), resample=Image.BILINEAR)
+            ) for s in dist_list
+        ])
+    else:
+        # upsample and reverse augmentation
+        score_map = np.zeros([N, IMAGE_RESIZE, IMAGE_RESIZE])
+        for i in range(score_map.shape[0]):
+            score_map_tmp = dist_list[i]
+            score_map_tmp = Image.fromarray(score_map_tmp)
+            score_map_tmp = score_map_tmp.resize((IMAGE_SIZE, IMAGE_SIZE),
+                                                resample=Image.BILINEAR)
+            score_map_tmp = np.array(score_map_tmp)
+            # reverse crop
+            pad_top = pad_h_list[i]
+            pad_left = pad_w_list[i]
+            pad_bottom = IMAGE_RESIZE - IMAGE_SIZE - pad_h
+            pad_right = IMAGE_RESIZE - IMAGE_SIZE - pad_w
+            score_map_tmp = np.pad(score_map_tmp, ((pad_top, pad_bottom),
+                                                   (pad_left, pad_right)))
+            # reverse rotate
+            angle = angle_list[i]
+            rot_mat = cv2.getRotationMatrix2D((IMAGE_RESIZE/2, IMAGE_RESIZE/2), -angle, 1)
+            score_map_tmp = cv2.warpAffine(src=score_map_tmp,
+                                           M=rot_mat,
+                                           dsize=(IMAGE_RESIZE, IMAGE_RESIZE),
+                                           borderMode=cv2.BORDER_REPLICATE,
+                                           flags=cv2.INTER_LINEAR)
+            score_map[i] = score_map_tmp
+        score_map = score_map.reshape(args.aug_num, -1, IMAGE_RESIZE, IMAGE_RESIZE)
+        score_map = np.mean(score_map, axis=0)
 
     # apply gaussian smoothing on the score map
     for i in range(score_map.shape[0]):
@@ -474,13 +530,16 @@ def recognize_from_image(net, create_net, params):
     scores = (score_map - min_score) / (max_score - min_score)
 
     # Calculated anormal score
-    anormal_scores=np.zeros((score_map.shape[0]))
+    anormal_scores = np.zeros((score_map.shape[0]))
     for i in range(score_map.shape[0]):
-        anormal_scores[i]=score_map[i].max()
+        anormal_scores[i] = score_map[i].max()
 
     if args.threshold is None:
         # get optimal threshold
-        gt_mask = np.asarray(gt_imgs)
+        if (not args.aug):
+            gt_mask = np.asarray(gt_imgs)
+        else:
+            gt_mask = np.asarray(gt_imgs[:int(len(gt_imgs)/args.aug_num)])
         precision, recall, thresholds = precision_recall_curve(gt_mask.flatten(), scores.flatten())
         a = 2 * precision * recall
         b = precision + recall
