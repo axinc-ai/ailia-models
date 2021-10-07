5d02c2024ece6a7632004914f4eb63f602f112bf
Nathan Boyer <nathan.boyer@ailab.ics.keio.ac.jp>
2021-10-07 17:30:16 +0900

    Implementation of in-shop_retriever

---
+++ deep_fashion/mmfashion/in-shop-clothes-retriever/README.md
@@ -0,0 +1,63 @@
+# MMFashion
+
+## Input
+
+![Input](06_1_front.jpg)
+
+(Image from https://github.com/open-mmlab/mmfashion/blob/master/demo/imgs/06_1_front.jpg)
+
+Shape : (1, 3, 224, 224)  
+
+## Output
+
+![Output](output.png)
+
+- Top-k results
+
+## Usage
+Automatically downloads the onnx and prototxt files on the first run.
+It is necessary to be connected to the Internet while downloading.
+
+For the sample image,
+``` bash
+$ python3 in-shop_retriever.py
+```
+
+If you want to specify the input image, put the image path after the `--input` option.  
+You can use `--savepath` option to change the name of the output file to save.
+```bash
+$ python3 in-shop_retriever.py --input IMAGE_PATH --savepath SAVE_IMAGE_PATH
+```
+
+By adding the `--video` option, you can input the video.   
+If you pass `0` as an argument to VIDEO_PATH, you can use the webcam input instead of the video file.
+```bash
+$ python3 in-shop_retriever.py --video VIDEO_PATH
+```
+
+By specifying the path to the root folder of the dataset of images with the `--gallery` option,
+you can use your own gallery of images for the retrieval task. This option should be used in combination with the `--img_file` option, that takes as argument the path to the .txt file that includes all the filenames of your gallery of images.
+```bash
+$ python3 in-shop_retriever.py --gallery PATH_TO_THE_ROOT_FOLDER_OF_YOUR_GALLERY --img_file IMAGES_FILENAME_OF_THE_GALLERY.txt
+```
+
+By specifying the `-k` option, you can choose how many images you want to retrieve from the gallery.
+```bash
+$ python3 in-shop_retriever.py -k 5
+```
+
+## Reference
+
+- [MMFashion](https://github.com/open-mmlab/mmfashion)
+
+## Framework
+
+Pytorch 1.9.1
+
+## Model Format
+
+ONNX opset=11
+
+## Netron
+
+[in-shop_retriever.onnx.prototxt](https://storage.googleapis.com/ailia-models/mmfashion/in-shop_retriever.onnx.prototxt)
---
+++ deep_fashion/mmfashion/in-shop-clothes-retriever/in-shop_retriever.py
@@ -0,0 +1,228 @@
+import sys
+import time
+import os
+
+import onnx
+import onnxruntime
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import cosine
+
+import ailia
+
+# import original modules
+sys.path.append('../../../util')
+from utils import get_base_parser, update_parser, get_savepath  # noqa: E402
+from model_utils import check_and_download_models  # noqa: E402
+from detector_utils import plot_results, load_image  # noqa: E402
+import webcamera_utils  # noqa: E402
+
+# logger
+from logging import getLogger  # noqa: E402
+
+logger = getLogger(__name__)
+
+# ======================
+# Parameters
+# ======================
+WEIGHT_PATH = './in-shop_retriever.onnx'
+MODEL_PATH = './in-shop_retriever.onnx.prototxt'
+#REMOTE_PATH = 'https://storage.googleapis.com/ailia-models/mmfashion/'
+
+IMAGE_PATH = './06_1_front.jpg'
+SAVE_IMAGE_PATH = 'output.png'
+
+NORM_MEAN = [123.675, 116.28, 103.53]
+NORM_STD = [58.395, 57.12, 57.375]
+
+# ======================
+# Argument Parser Config
+# ======================
+parser = get_base_parser('MMFashion model - In-shop Clothes Retriever', IMAGE_PATH, SAVE_IMAGE_PATH)
+parser.add_argument(
+    '-d', '--debug',
+    action='store_true',
+    help='Debugger'
+)
+parser.add_argument(
+    '--onnx_runtime',
+    action='store_true',
+    help='Inference using onnx runtime'
+)
+parser.add_argument(
+    '--gallery',
+    default='/Users/nathan/Desktop/mmfashion/data/In-shop/Img', type=str,
+    help="Path to the root folder of the images' gallery"
+)
+parser.add_argument(
+    '--img_file',
+    default='/Users/nathan/Desktop/mmfashion/data/In-shop/Anno/gallery_img.txt', type=str,
+    help="Path to the images' filename of the gallery (.txt file)"
+)
+parser.add_argument(
+    '-k', '--topk',
+    default=5, type=int,
+    help='Retrieve the top k results'
+)
+args = update_parser(parser)
+
+def check(onnx_model):
+    onnx.checker.check_model(onnx_model)
+
+def preprocess(img):
+    h, w = img.shape[:2]
+
+    # scale
+    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_LINEAR)
+    
+    # normalize
+    img = img.astype(np.float32)
+    mean = np.array(NORM_MEAN)
+    std = np.array(NORM_STD)
+    mean = np.float64(mean.reshape(1, -1))
+    stdinv = 1 / np.float64(std.reshape(1, -1))
+    cv2.subtract(img, mean, img)  # inplace
+    cv2.multiply(img, stdinv, img)  # inplace
+
+    img = img.transpose(2, 0, 1)
+    img = np.expand_dims(img, 0)
+    return img
+
+def recognize_from_image_onnx(filename, ort_session):
+    # prepare input data
+    img = load_image(filename)
+    #logger.info((f'input image shape: {img.shape}')
+    img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+    img = preprocess(img)
+
+    ort_inputs = {ort_session.get_inputs()[0].name: img}
+
+    # inference
+    logger.info('Start inference...')
+    if args.benchmark:
+        logger.info('BENCHMARK mode')
+        for i in range(5):
+            start = int(round(time.time() * 1000))
+            ort_outs = ort_session.run(None, ort_inputs)
+            pred = ort_outs[0]
+            end = int(round(time.time() * 1000))
+            logger.info(f'\tonnx runtime processing time {end - start} ms')
+    else:
+        ort_outs = ort_session.run(None, ort_inputs)
+        pred = ort_outs[0]
+
+    #logger.info(f'output shape: {pred.shape}')
+
+def recognize_from_image(filename, net):
+    # prepare input data
+    img = load_image(filename)
+    #logger.info(f'input image shape: {img.shape}')
+    img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+    img = preprocess(img)
+
+    # inference
+    logger.info('Start inference...')
+    if args.benchmark:
+        logger.info('BENCHMARK mode')
+        for i in range(5):
+            start = int(round(time.time() * 1000))
+            preds_ailia = net.predict(img)
+            end = int(round(time.time() * 1000))
+            logger.info(f'\tailia processing time {end - start} ms')
+    else:
+        preds_ailia = net.predict(img)
+
+    #logger.info(f'output shape: {preds_ailia.shape}')
+
+    gallery_idx2im = {}
+    gallery_imgs = open(args.img_file,'r').readlines()
+    for i, img in enumerate(gallery_imgs):
+        gallery_idx2im[i] = img.strip('\n')
+
+    gallery_embeds = process_embeds(gallery_imgs, net)
+
+    show_retrieved_images(preds_ailia, gallery_embeds, gallery_idx2im, args.topk, filename)
+
+def process_embeds(filenames, model):
+    embeds = []
+    logger.info('Exploring the gallery... (it may take a while)')
+
+    for filename in filenames:
+        # prepare input data
+        img = load_image(os.path.join(args.gallery, filename)[:-1])
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+        img = preprocess(img)
+
+        if len(embeds) % 500 == 0: 
+            print(f"{len(embeds)}/{len(filenames)}")
+
+        # inference
+        embed = model.predict(img)
+        embeds.append(embed)
+
+    return embeds
+
+def show_topk_retrieved_images(retrieved_idxes, gallery_idx2im, input_image):
+        fig = plt.figure(figsize=(15, 2.5))
+        k=1
+        show_img(input_image, fig, 'Input image', len(retrieved_idxes)+1, k)
+
+        for idx in retrieved_idxes:
+            k+=1
+            retrieved_img = gallery_idx2im[idx]
+            filename = os.path.join(args.gallery, retrieved_img)
+            print(filename)
+            show_img(filename, fig, f'Top-{k-1} result', len(retrieved_idxes)+1, k)
+
+        savepath = get_savepath(args.savepath, input_image)
+        plt.savefig(savepath, bbox_inches='tight')
+        logger.info(f'saved at : {savepath}')
+
+def show_retrieved_images(query_feat, gallery_embeds, gallery_idx2im, topk, filename):
+        query_dist = []
+        for i, feat in enumerate(gallery_embeds):
+            cosine_dist = cosine(
+                feat.reshape(1, -1), query_feat.reshape(1, -1))
+            query_dist.append(cosine_dist)
+
+        query_dist = np.array(query_dist)
+        order = np.argsort(query_dist)
+
+        logger.info('Retrieved Top%d Results' % topk)
+        show_topk_retrieved_images(order[:topk], gallery_idx2im, filename)
+
+def show_img(filename, fig, title, topk, idx):
+    img = plt.imread(filename)
+    ax = fig.add_subplot(1, topk, idx)
+    plt.axis('off')
+    imgplot = plt.imshow(img)
+    ax.set_title(title)
+
+def main():
+    # model files check and download
+    #check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
+    
+    # debug
+    if args.debug:
+        onnx_model = onnx.load(WEIGHT_PATH)
+        check(onnx_model)
+    else:
+        if args.onnx_runtime:
+            # image mode
+            # input image loop
+            ort_session = onnxruntime.InferenceSession(WEIGHT_PATH)
+
+            for image_path in args.input:
+                recognize_from_image_onnx(image_path, ort_session)
+            logger.info('Script finished successfully.')
+        else:
+            # net initialize
+            net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=args.env_id)
+            # input image loop
+            for image_path in args.input:
+                recognize_from_image(image_path, net)
+            logger.info('Script finished successfully.')
+
+if __name__ == '__main__':
+    main()
