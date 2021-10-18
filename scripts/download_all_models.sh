0bf0acc6b8e96cb96d6078f44f8f361035799ebc
satoshi.ooe <ooe1123@gmail.com>
2021-10-18 22:51:58 +0900

    Add README

--- scripts/download_all_models.sh
+++ scripts/download_all_models.sh
@@ -134,6 +134,7 @@ cd ../../object_detection_3d/mediapipe_objectron; python3 mediapipe_objectron.py
 cd ../../object_tracking/deepsort; python3 deepsort.py ${OPTION}
 cd ../../object_tracking/person_reid_baseline_pytorch; python3 person_reid_baseline_pytorch.py ${OPTION}
 cd ../../object_tracking/abd_net; python3 abd_net.py ${OPTION}
+cd ../../object_tracking/siam-mot; python3 siam-mot.py ${OPTION}
 cd ../../point_segmentation/pointnet_pytorch python3 pointnet_pytorch.py ${OPTION}
 cd ../../pose_estimation/lightweight-human-pose-estimation; python3 lightweight-human-pose-estimation.py ${OPTION}
 cd ../../pose_estimation/openpose; python3 openpose.py ${OPTION}
