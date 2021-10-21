42523e41950f69c04c09f79464bc02165af7f9be
satoshi.ooe <ooe1123@gmail.com>
2021-10-21 22:59:35 +0900

    replace cython_bbox by pytorch function

--- object_tracking/bytetrack/bytetrack_utils.py
+++ object_tracking/bytetrack/bytetrack_utils.py
@@ -3,7 +3,6 @@ import cv2
 
 __all__ = [
     'batched_nms',
-    'convert_to_coco_format',
 ]
 
 
@@ -63,35 +62,3 @@ def batched_nms(boxes, scores, idxs, threshold, max_num=0):
         keep = keep[:max_num]
 
     return keep
-
-
-def xyxy2xywh(bboxes):
-    bboxes[:, 2] = bboxes[:, 2] - bboxes[:, 0]
-    bboxes[:, 3] = bboxes[:, 3] - bboxes[:, 1]
-    return bboxes
-
-
-def convert_to_coco_format(outputs, labels, scale):
-    data_list = []
-    for output in outputs:
-        if output is None:
-            continue
-
-        bboxes = output[:, 0:4]
-
-        bboxes /= scale
-        bboxes = xyxy2xywh(bboxes)
-
-        cls = output[:, 6]
-        scores = output[:, 4] * output[:, 5]
-        for ind in range(bboxes.shape[0]):
-            label = labels[int(cls[ind])]
-            pred_data = {
-                "category_id": label,
-                "bbox": bboxes[ind].numpy().tolist(),
-                "score": scores[ind].numpy().item(),
-                "segmentation": [],
-            }  # COCO json format
-            data_list.append(pred_data)
-
-    return data_list
--- object_tracking/bytetrack/tracker/matching.py
+++ object_tracking/bytetrack/tracker/matching.py
@@ -2,7 +2,8 @@ import numpy as np
 import scipy
 import lap
 from scipy.spatial.distance import cdist
-from cython_bbox import bbox_overlaps as bbox_ious
+
+# from cython_bbox import bbox_overlaps
 
 from . import kalman_filter
 
@@ -49,6 +50,89 @@ def linear_assignment(cost_matrix, thresh):
     return matches, unmatched_a, unmatched_b
 
 
+def fp16_clamp(x, min=None, max=None):
+    import torch
+    if not x.is_cuda and x.dtype == torch.float16:
+        # clamp for cpu float16, tensor fp16 has no clamp implementation
+        return x.float().clamp(min, max).half()
+
+    return x.clamp(min, max)
+
+
+def bbox_overlaps(bboxes1, bboxes2, mode='iou', is_aligned=False, eps=1e-6):
+    assert mode in ['iou', 'iof', 'giou'], f'Unsupported mode {mode}'
+    # Either the boxes are empty or the length of boxes' last dimension is 4
+    assert (bboxes1.size(-1) == 4 or bboxes1.size(0) == 0)
+    assert (bboxes2.size(-1) == 4 or bboxes2.size(0) == 0)
+
+    # Batch dim must be the same
+    # Batch dim: (B1, B2, ... Bn)
+    assert bboxes1.shape[:-2] == bboxes2.shape[:-2]
+    batch_shape = bboxes1.shape[:-2]
+
+    rows = bboxes1.size(-2)
+    cols = bboxes2.size(-2)
+    if is_aligned:
+        assert rows == cols
+
+    if rows * cols == 0:
+        if is_aligned:
+            return bboxes1.new(batch_shape + (rows,))
+        else:
+            return bboxes1.new(batch_shape + (rows, cols))
+
+    area1 = (bboxes1[..., 2] - bboxes1[..., 0]) * (
+            bboxes1[..., 3] - bboxes1[..., 1])
+    area2 = (bboxes2[..., 2] - bboxes2[..., 0]) * (
+            bboxes2[..., 3] - bboxes2[..., 1])
+
+    import torch
+    if is_aligned:
+        lt = torch.max(bboxes1[..., :2], bboxes2[..., :2])  # [B, rows, 2]
+        rb = torch.min(bboxes1[..., 2:], bboxes2[..., 2:])  # [B, rows, 2]
+
+        wh = fp16_clamp(rb - lt, min=0)
+        overlap = wh[..., 0] * wh[..., 1]
+
+        if mode in ['iou', 'giou']:
+            union = area1 + area2 - overlap
+        else:
+            union = area1
+        if mode == 'giou':
+            enclosed_lt = torch.min(bboxes1[..., :2], bboxes2[..., :2])
+            enclosed_rb = torch.max(bboxes1[..., 2:], bboxes2[..., 2:])
+    else:
+        lt = torch.max(bboxes1[..., :, None, :2],
+                       bboxes2[..., None, :, :2])  # [B, rows, cols, 2]
+        rb = torch.min(bboxes1[..., :, None, 2:],
+                       bboxes2[..., None, :, 2:])  # [B, rows, cols, 2]
+
+        wh = fp16_clamp(rb - lt, min=0)
+        overlap = wh[..., 0] * wh[..., 1]
+
+        if mode in ['iou', 'giou']:
+            union = area1[..., None] + area2[..., None, :] - overlap
+        else:
+            union = area1[..., None]
+        if mode == 'giou':
+            enclosed_lt = torch.min(bboxes1[..., :, None, :2],
+                                    bboxes2[..., None, :, :2])
+            enclosed_rb = torch.max(bboxes1[..., :, None, 2:],
+                                    bboxes2[..., None, :, 2:])
+
+    eps = union.new_tensor([eps])
+    union = torch.max(union, eps)
+    ious = overlap / union
+    if mode in ['iou', 'iof']:
+        return ious
+    # calculate gious
+    enclose_wh = fp16_clamp(enclosed_rb - enclosed_lt, min=0)
+    enclose_area = enclose_wh[..., 0] * enclose_wh[..., 1]
+    enclose_area = torch.max(enclose_area, eps)
+    gious = ious - (enclose_area - union) / enclose_area
+    return gious
+
+
 def ious(atlbrs, btlbrs):
     """
     Compute cost based on IoU
@@ -61,10 +145,10 @@ def ious(atlbrs, btlbrs):
     if ious.size == 0:
         return ious
 
-    ious = bbox_ious(
-        np.ascontiguousarray(atlbrs, dtype=np.float),
-        np.ascontiguousarray(btlbrs, dtype=np.float)
-    )
+    import torch
+    atlbrs = torch.from_numpy(np.asarray(atlbrs))
+    btlbrs = torch.from_numpy(np.asarray(btlbrs))
+    ious = bbox_overlaps(atlbrs, btlbrs).numpy()
 
     return ious
 
@@ -91,28 +175,6 @@ def iou_distance(atracks, btracks):
     return cost_matrix
 
 
-def v_iou_distance(atracks, btracks):
-    """
-    Compute cost based on IoU
-    :type atracks: list[STrack]
-    :type btracks: list[STrack]
-
-    :rtype cost_matrix np.ndarray
-    """
-
-    if (len(atracks) > 0 and isinstance(atracks[0], np.ndarray)) or (
-            len(btracks) > 0 and isinstance(btracks[0], np.ndarray)):
-        atlbrs = atracks
-        btlbrs = btracks
-    else:
-        atlbrs = [track.tlwh_to_tlbr(track.pred_bbox) for track in atracks]
-        btlbrs = [track.tlwh_to_tlbr(track.pred_bbox) for track in btracks]
-    _ious = ious(atlbrs, btlbrs)
-    cost_matrix = 1 - _ious
-
-    return cost_matrix
-
-
 def embedding_distance(tracks, detections, metric='cosine'):
     """
     :param tracks: list[STrack]
@@ -125,8 +187,6 @@ def embedding_distance(tracks, detections, metric='cosine'):
     if cost_matrix.size == 0:
         return cost_matrix
     det_features = np.asarray([track.curr_feat for track in detections], dtype=np.float)
-    # for i, track in enumerate(tracks):
-    # cost_matrix[i, :] = np.maximum(0.0, cdist(track.smooth_feat.reshape(1,-1), det_features, metric))
     track_features = np.asarray([track.smooth_feat for track in tracks], dtype=np.float)
     cost_matrix = np.maximum(0.0, cdist(track_features, det_features, metric))  # Nomalized features
     return cost_matrix
@@ -162,6 +222,7 @@ def fuse_motion(kf, cost_matrix, tracks, detections, only_position=False, lambda
 def fuse_iou(cost_matrix, tracks, detections):
     if cost_matrix.size == 0:
         return cost_matrix
+
     reid_sim = 1 - cost_matrix
     iou_dist = iou_distance(tracks, detections)
     iou_sim = 1 - iou_dist
