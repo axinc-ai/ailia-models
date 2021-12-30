59805374611eea64a6f3ed6d2be640d8b11895da
宮本蒼太 <miyamotost1016@yahoo.co.jp>
2021-12-31 00:10:17 +0900

    Eliminate torch dependency

--- image_classification/alexnet/alexnet.py
+++ image_classification/alexnet/alexnet.py
@@ -15,6 +15,8 @@ from classifier_utils import plot_results, print_results  # noqa: E402
 from logging import getLogger  # noqa: E402
 logger = getLogger(__name__)
 
+from PIL import Image
+
 
 # ======================
 # PARAMETERS
@@ -38,40 +40,64 @@ args = update_parser(parser)
 # Main functions
 # ======================
 def _get_image(filename):
-    from PIL import Image
     input_image = Image.open(filename)
     input_batch = _preprocess_image(input_image)
     return input_batch
 
 
 def _preprocess_image(img):
-    from PIL import Image
-    from torchvision import transforms
     if args.video is not None:
         img = Image.fromarray(img)
-    preprocess = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-    input_tensor = preprocess(img)
-    input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
-    input_batch = input_batch.to('cpu').detach().numpy().copy()
+
+    # alternative to transforms.Resize(256)
+    size = 256
+    w = np.asarray(img).shape[1]
+    h = np.asarray(img).shape[0]
+    short, long = (w, h) if w <= h else (h, w)
+    requested_new_short = size if isinstance(size, int) else size[0]
+    new_short, new_long = requested_new_short, int(requested_new_short * long / short)
+    new_w, new_h = (new_short, new_long) if w <= h else (new_long, new_short)
+    img = np.asarray(img)
+    mode_to_nptype = {"I": np.int32, "I;16": np.int16, "F": np.float32}
+    img = np.array(img, mode_to_nptype.get('RGB', np.uint8))
+    img = cv2.resize(img, (new_w, new_h))
+
+    # alternative to transforms.CenterCrop(224)
+    center_w = img.shape[1]/2
+    center_h = img.shape[0]/2
+    side = 224
+    x = center_w - side/2
+    y = center_h - side/2
+    img = img[int(y):int(y+side), int(x):int(x+side), :]
+
+    # alternative to transform.ToTensor()
+    img = img.transpose((2, 0, 1))
+    img = img / 255
+
+    # alternative to transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    img = np.array(img)
+    img = (img - mean[:, None, None]) / std[:, None, None]
+
+    input_tensor = img
+    input_batch = np.expand_dims(input_tensor, axis=0) # create a mini-batch as expected by the model
     return input_batch
 
 
 def _get_prob(output, topk=5):
-    from PIL import Image
-    from torchvision import transforms
-    import torch
-    output = torch.from_numpy(output)
-    prob = torch.nn.functional.softmax(output[0], dim=0)
-    #prob = prob.to('cpu').detach().numpy().copy()
-    topk_prob, topk_catid = torch.topk(prob, topk)
-    topk_prob = topk_prob.to('cpu').detach().numpy().copy()
-    topk_catid = topk_catid.to('cpu').detach().numpy().copy()
+    prob = _softmax(output[0])
+    idx = np.argsort(-prob)[:topk]
+    y = prob[idx]
+    topk_prob = y
+    topk_catid = idx
     return topk_prob, topk_catid
+
+
+def _softmax(x, axis=None):
+    e_x = np.exp(x - np.max(x, axis=axis, keepdims=True))
+    e_x = e_x / np.sum(e_x, axis=axis, keepdims=True)
+    return e_x
 
 
 def _get_labels():
