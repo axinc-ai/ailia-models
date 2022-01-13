1653778575a805ad5b5d2b3f19e011325c620de8
satoshi.ooe <ooe1123@gmail.com>
2022-01-13 23:07:11 +0900

    Implement VoiceFilter

---
+++ audio_processing/voicefilter/audio_utils.py
@@ -0,0 +1,103 @@
+# adapted from Keith Ito's tacotron implementation
+# https://github.com/keithito/tacotron/blob/master/util/audio.py
+
+import librosa
+import numpy as np
+
+
+class Dotdict(dict):
+    """
+    a dictionary that supports dot notation
+    as well as dictionary access notation
+    usage: d = DotDict() or d = DotDict({'val1':'first'})
+    set attributes: d.val2 = 'second' or d['val2'] = 'second'
+    get attributes: d.val2 or d['val2']
+    """
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+    def __init__(self, dct=None):
+        dct = dict() if not dct else dct
+        for key, value in dct.items():
+            if hasattr(value, 'keys'):
+                value = Dotdict(value)
+            self[key] = value
+
+
+hp = Dotdict({
+    "audio": {
+        "n_fft": 1200,
+        # num_freq: 601  # n_fft//2 + 1
+        "sample_rate": 16000,
+        "hop_length": 160,
+        "win_length": 400,
+        "min_level_db": -100.0,
+        "ref_level_db": 20.0,
+    },
+    "embedder": {
+        "n_fft": 512,
+        "num_mels": 40,
+    }
+})
+
+
+class Audio:
+    def __init__(self):
+        self.mel_basis = librosa.filters.mel(
+            sr=hp.audio.sample_rate,
+            n_fft=hp.embedder.n_fft,
+            n_mels=hp.embedder.num_mels)
+
+    def get_mel(self, y):
+        y = librosa.core.stft(
+            y=y, n_fft=hp.embedder.n_fft,
+            hop_length=hp.audio.hop_length,
+            win_length=hp.audio.win_length,
+            window='hann')
+        magnitudes = np.abs(y) ** 2
+        mel = np.log10(np.dot(self.mel_basis, magnitudes) + 1e-6)
+
+        return mel
+
+    def wav2spec(self, y):
+        D = self.stft(y)
+        S = self.amp_to_db(np.abs(D)) - hp.audio.ref_level_db
+        S, D = self.normalize(S), np.angle(D)
+        S, D = S.T, D.T  # to make [time, freq]
+
+        return S, D
+
+    def spec2wav(self, spectrogram, phase):
+        spectrogram, phase = spectrogram.T, phase.T
+        # used during inference only
+        # spectrogram: enhanced output
+        # phase: use noisy input's phase, so no GLA is required
+        S = self.db_to_amp(self.denormalize(spectrogram) + hp.audio.ref_level_db)
+
+        return self.istft(S, phase)
+
+    def stft(self, y):
+        return librosa.stft(
+            y=y, n_fft=hp.audio.n_fft,
+            hop_length=hp.audio.hop_length,
+            win_length=hp.audio.win_length)
+
+    def istft(self, mag, phase):
+        stft_matrix = mag * np.exp(1j * phase)
+        return librosa.istft(
+            stft_matrix,
+            hop_length=hp.audio.hop_length,
+            win_length=hp.audio.win_length)
+
+    def amp_to_db(self, x):
+        return 20.0 * np.log10(np.maximum(1e-5, x))
+
+    def db_to_amp(self, x):
+        return np.power(10.0, x * 0.05)
+
+    def normalize(self, S):
+        return np.clip(S / -hp.audio.min_level_db, -1.0, 0.0) + 1.0
+
+    def denormalize(self, S):
+        return (np.clip(S, 0.0, 1.0) - 1.0) * -hp.audio.min_level_db
---
+++ audio_processing/voicefilter/voicefilter.py
@@ -0,0 +1,150 @@
+import os
+import sys
+import time
+
+import numpy as np
+import librosa
+import soundfile as sf
+
+import ailia
+from audio_utils import Audio
+
+# import original modules
+sys.path.append('../../util')
+from utils import get_base_parser, update_parser, get_savepath  # noqa: E402
+from model_utils import check_and_download_models  # noqa: E402
+# logger
+from logging import getLogger  # noqa: E402
+
+logger = getLogger(__name__)
+
+# ======================
+# Parameters
+# ======================
+
+WEIGHT_PATH = 'model.onnx'
+MODEL_PATH = 'model.onnx.prototxt'
+WEIGHT_EMB_PATH = 'embedder.onnx'
+MODEL_EMB_PATH = 'embedder.onnx.prototxt'
+REMOTE_PATH = 'https://storage.googleapis.com/ailia-models/voicefilter/'
+
+WAVE_PATH = "1089-134691-0000.flac"
+SAVE_PATH = 'output.wav'
+
+# Audio
+SAMPLING_RATE = 16000
+
+# ======================
+# Arguemnt Parser Config
+# ======================
+
+parser = get_base_parser(
+    'VoiceFilter', WAVE_PATH, SAVE_PATH, input_ftype='audio'
+)
+parser.add_argument(
+    '-r', '--reference_file',
+    default=None, type=str,
+    help='path of reference wav file'
+)
+parser.add_argument(
+    '--onnx',
+    action='store_true',
+    help='execute onnxruntime version.'
+)
+args = update_parser(parser)
+
+
+# ======================
+# Secondaty Functions
+# ======================
+
+def read_wave(path):
+    # prepare input data
+    wav, source_sr = librosa.load(path, sr=None)
+    # Resample the wav if needed
+    if source_sr is not None and source_sr != SAMPLING_RATE:
+        wav = librosa.resample(wav, source_sr, SAMPLING_RATE)
+
+    return wav
+
+
+# ======================
+# Main functions
+# ======================
+
+def audio_recognition(net, embedder):
+    reference_file = args.reference_file
+    if not reference_file or not os.path.exists(reference_file):
+        logger.error('reference_file:%s is NG.' % reference_file)
+        sys.exit(-1)
+
+    audio = Audio()
+
+    # prepare reference wav
+    dvec_wav = read_wave(reference_file)
+    dvec_mel = audio.get_mel(dvec_wav)
+    output = embedder.predict([dvec_mel])
+    dvec = output[0]
+    dvec = np.expand_dims(dvec, axis=0)
+
+    for soundf_path in args.input:
+        logger.info(soundf_path)
+
+        # prepare mix wav
+        mixed_wav = read_wave(soundf_path)
+        mag, phase = audio.wav2spec(mixed_wav)
+        mag = np.expand_dims(mag, axis=0)
+
+        # inference
+        logger.info('Start inference...')
+        if args.benchmark:
+            logger.info('BENCHMARK mode')
+            total_time_estimation = 0
+            for i in range(args.benchmark_count):
+                start = int(round(time.time() * 1000))
+                output = net.predict([mag, dvec])
+                end = int(round(time.time() * 1000))
+                estimation_time = (end - start)
+
+                # Loggin
+                logger.info(f'\tailia processing estimation time {estimation_time} ms')
+                if i != 0:
+                    total_time_estimation = total_time_estimation + estimation_time
+
+            logger.info(f'\taverage time estimation {total_time_estimation / (args.benchmark_count - 1)} ms')
+        else:
+            output = net.predict([mag, dvec])
+
+        mask = output[0]
+
+        est_mag = mag * mask
+        est_wav = audio.spec2wav(est_mag[0], phase)
+
+        savepath = get_savepath(args.savepath, soundf_path, ext='.wav')
+        logger.info(f'saved at : {savepath}')
+        sf.write(savepath, est_wav, SAMPLING_RATE, 'PCM_24')
+
+    logger.info('Script finished successfully.')
+
+
+def main():
+    # model files check and download
+    logger.info('Checking voicefilter model...')
+    check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
+    logger.info('Checking embedder model...')
+    check_and_download_models(WEIGHT_EMB_PATH, MODEL_EMB_PATH, REMOTE_PATH)
+
+    env_id = args.env_id
+
+    if not args.onnx:
+        net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=env_id)
+        embedder = ailia.Net(MODEL_EMB_PATH, WEIGHT_EMB_PATH, env_id=env_id)
+    else:
+        import onnxruntime
+        net = onnxruntime.InferenceSession(WEIGHT_PATH)
+
+    audio_recognition(net, embedder)
+
+
+if __name__ == '__main__':
+    main()
