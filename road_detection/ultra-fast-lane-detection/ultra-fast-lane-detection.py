d32f9373a142a04ea2af8c1ebf27970abf3c8d0c
Kazuki Kyakuno <kyakuno@axell.co.jp>
2022-02-16 14:50:50 +0900

    Added license file

--- road_detection/ultra-fast-lane-detection/ultra-fast-lane-detection.py
+++ road_detection/ultra-fast-lane-detection/ultra-fast-lane-detection.py
@@ -25,13 +25,7 @@ SAVE_IMAGE_PATH = 'output.jpg'
 INPUT_IMAGE_PATH = 'input.jpg'
 # INPUT_IMAGE_PATH = '02370.jpg'
 
-WEIGHT_PATH = 'culane_18.onnx'
-MODEL_PATH = 'culane_18.onnx.prototxt'
-
-# WEIGHT_PATH = 'tusimple_18.onnx'
-# MODEL_PATH = 'tusimple_18.onnx.prototxt'
-# INPUT_IMAGE_PATH = '02255.jpg'
-REMOTE_PATH = 'https://storage.googleapis.com/ailia-models/ultra-fast-lane-detection/'
+MODEL_LISTS = ['culane','tusimple']
 
 # ======================
 # Argument Parser Config
@@ -56,7 +50,25 @@ parser.add_argument(
     '-o', '--onnx', action='store_true',
     help="Option to use onnxrutime to run or not."
 )
+parser.add_argument(
+    '-a', '--arch', metavar='ARCH',
+    default='culane', choices=MODEL_LISTS,
+    help='model lists: ' + ' | '.join(MODEL_LISTS)
+)
 args = update_parser(parser)
+
+# # ======================
+# # Select model
+# # ======================
+
+if args.arch=="culane":
+    WEIGHT_PATH = 'culane_18.onnx'
+    MODEL_PATH = 'culane_18.onnx.prototxt'
+else:
+    WEIGHT_PATH = 'tusimple_18.onnx'
+    MODEL_PATH = 'tusimple_18.onnx.prototxt'
+
+REMOTE_PATH = 'https://storage.googleapis.com/ailia-models/ultra-fast-lane-detection/'
 
 # # ======================
 # # Main functions
@@ -84,59 +96,57 @@ griding_num = 200
 culane_row_anchor = [121, 131, 141, 150, 160, 170, 180, 189, 199, 209, 219, 228, 238, 248, 258, 267, 277, 287]
 cls_num_per_lane = 18
 def recognize_from_image(net):
+    # input image loop
+    for image_path in args.input:
+        img = cv2.imread(image_path)
 
-    print(args.input)
-    img = cv2.imread(args.input[0])
+        input_name = 'input.1'
+        output_name = '200'
 
-    input_name = 'input.1'
-    output_name = '200'
+        input_tensor = preprocessing(img)
 
-    input_tensor = preprocessing(img)
+        if args.onnx:
+            y = net.run( [output_name], { input_name : input_tensor })[0][0]
+        else:
+            y = net.run(input_tensor)[0][0]
 
-    if args.onnx:
-        y = net.run( [output_name], { input_name : input_tensor })[0][0]
-    else:
-        disparity_map = model.run(imgs)[0]
+        col_sample = np.linspace(0, 800 - 1, griding_num)
+        col_sample_w = col_sample[1] - col_sample[0]
+        img_w, img_h = 1640, 590
+        row_anchor = culane_row_anchor
 
-    col_sample = np.linspace(0, 800 - 1, griding_num)
-    col_sample_w = col_sample[1] - col_sample[0]
-    img_w, img_h = 1640, 590
-    row_anchor = culane_row_anchor
+        print(np.array(y).shape)
+        out_j = y
+        out_j = out_j[:, ::-1, :]
+        prob = scipy.special.softmax(out_j[:-1, :, :], axis=0)
 
-    print(np.array(y).shape)
-    out_j = y
-    out_j = out_j[:, ::-1, :]
-    prob = scipy.special.softmax(out_j[:-1, :, :], axis=0)
+        idx = np.arange(griding_num) + 1
+        idx = idx.reshape(-1, 1, 1)
+        print(idx.shape)
+        loc = np.sum(prob * idx, axis=0)
+        out_j = np.argmax(out_j, axis=0)
+        loc[out_j == griding_num] = 0
+        out_j = loc
 
-    idx = np.arange(griding_num) + 1
-    idx = idx.reshape(-1, 1, 1)
-    print(idx.shape)
-    loc = np.sum(prob * idx, axis=0)
-    out_j = np.argmax(out_j, axis=0)
-    loc[out_j == griding_num] = 0
-    out_j = loc
+        print(out_j.shape)
 
-    print(out_j.shape)
+        # import pdb; pdb.set_trace()
+        vis = cv2.imread(args.input[0])
+        for i in range(out_j.shape[1]):
+            if np.sum(out_j[:, i] != 0) > 2:
+                for k in range(out_j.shape[0]):
+                    if out_j[k, i] > 0:
+                        ppp = (int(out_j[k, i] * col_sample_w * img_w / 800) - 1, int(img_h * (row_anchor[cls_num_per_lane-1-k]/288)) - 1 )
+                        cv2.circle(vis,ppp,5,(0,255,0),-1)
 
-    # import pdb; pdb.set_trace()
-    vis = cv2.imread(args.input[0])
-    for i in range(out_j.shape[1]):
-        if np.sum(out_j[:, i] != 0) > 2:
-            for k in range(out_j.shape[0]):
-                if out_j[k, i] > 0:
-                    ppp = (int(out_j[k, i] * col_sample_w * img_w / 800) - 1, int(img_h * (row_anchor[cls_num_per_lane-1-k]/288)) - 1 )
-                    cv2.circle(vis,ppp,5,(0,255,0),-1)
+        
+        # print(y.shape)
+        save_img = cv2.resize( vis , ( int(vis.shape[1] / 4 ) , int(vis.shape[0] / 4 )))
 
-    
+        savepath = get_savepath(args.savepath, image_path)
+        logger.info(f'saved at : {savepath}')
+        cv2.imwrite(savepath, save_img)
 
-    print('----------------------------------------')
-    # print(y.shape)
-    save_img = cv2.resize( vis , ( int(vis.shape[1] / 4 ) , int(vis.shape[0] / 4 )))
-    cv2.imwrite("output.png",save_img)
-    # print(prob)
-    print(out_j)
-
-    print('----------------------------------------')
     logger.info('Script finished successfully.')
 
 def recognize_from_video(net):
