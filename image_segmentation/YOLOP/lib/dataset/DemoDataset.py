ac0642a68c8b9ad0a25bc77164b32cc6e66105ec
yuki <a@a.a>
2022-03-22 07:38:00 +0900

    delete augumentation.py

--- image_segmentation/YOLOP/lib/dataset/DemoDataset.py
+++ image_segmentation/YOLOP/lib/dataset/DemoDataset.py
@@ -12,7 +12,8 @@ import numpy as np
 from PIL import Image, ExifTags
 from tqdm import tqdm
 
-from ..utils import letterbox_for_img, clean_str
+#from ..utils import letterbox_for_img, clean_str
+from ..utils import  clean_str
 
 img_formats = ['.bmp', '.jpg', '.jpeg', '.png', '.tif', '.tiff', '.dng']
 vid_formats = ['.mov', '.avi', '.mp4', '.mpg', '.mpeg', '.m4v', '.wmv', '.mkv']
@@ -140,6 +141,7 @@ class LoadStreams:  # multiple IP or RTSP cameras
 
         # check for common shapes
 
+        print("---------------")
         s = np.stack([letterbox_for_img(x, self.img_size, auto=self.auto)[0].shape for x in self.imgs], 0)  # shapes
         self.rect = np.unique(s, axis=0).shape[0] == 1  # rect inference if all shapes equal
         if not self.rect:
@@ -171,6 +173,8 @@ class LoadStreams:  # multiple IP or RTSP cameras
         img0 = self.imgs.copy()
 
         h0, w0 = img0[0].shape[:2]
+        
+        print("---------------")
         img, _, pad = letterbox_for_img(img0[0], self.img_size, auto=self.rect and self.auto)
 
         # Stack
@@ -185,3 +189,41 @@ class LoadStreams:  # multiple IP or RTSP cameras
 
     def __len__(self):
         return len(self.sources)  # 1E12 frames = 32 streams at 30 FPS for 30 years
+
+def letterbox_for_img(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True):
+    # Resize image to a 32-pixel-multiple rectangle https://github.com/ultralytics/yolov3/issues/232
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+
+
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, 32), np.mod(dh, 32)  # wh padding
+
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_AREA)
+
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
+
+
--- image_segmentation/YOLOP/lib/utils/__init__.py
+++ image_segmentation/YOLOP/lib/utils/__init__.py
@@ -1,2 +1,1 @@
 from .utils import initialize_weights, DataLoaderX, torch_distributed_zero_first, clean_str
-from .augmentations import augment_hsv, random_perspective, cutout, letterbox,letterbox_for_img
