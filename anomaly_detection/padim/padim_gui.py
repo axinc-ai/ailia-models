d12b2208fd71dccd1623c3d55412726bd59951f2
Kazuki Kyakuno <kyakuno@axell.co.jp>
2022-06-03 15:20:44 +0900

    Added base code of gui

---
+++ anomaly_detection/padim/padim_gui.py
@@ -0,0 +1,328 @@
+# ailia MODELS launcher
+
+import os
+import cv2
+import numpy
+import subprocess
+import shutil
+import sys
+import glob
+import ailia
+
+from PIL import Image, ImageTk
+
+# for macOS, please install "brew install python-tk@3.9"
+import tkinter as tk
+from tkinter import ttk
+import tkinter.filedialog
+
+# ======================
+# Global settings
+# ======================
+
+input_index = 0
+output_index = 0
+model_index = 0
+
+# ======================
+# Environment
+# ======================
+
+def input_changed(event):
+    global input_index
+    selection = event.widget.curselection()
+    if selection:
+        input_index = selection[0]
+    else:
+        input_index = 0   
+    print("input",train_list[input_index])
+    load_detail(train_list[input_index])
+
+def output_changed(event):
+    global output_index
+    selection = event.widget.curselection()
+    if selection:
+        output_index = selection[0]
+    else:
+        output_index = 0   
+    print("output",test_list[output_index])
+    load_detail(test_list[output_index])
+
+
+# ======================
+# Change model
+# ======================
+
+
+def model_changed(event):
+    global model_index
+    selection = event.widget.curselection()
+    if selection:
+        model_index = selection[0]
+    else:
+        model_index = 0
+    load_detail(model_index)
+
+def create_photo_image(path,w=320,h=240):
+    image_bgr = cv2.imread(path)
+    #image_bgr = cv2.resize(image_bgr,(w,h))
+    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB) # imreadはBGRなのでRGBに変換
+    image_pil = Image.fromarray(image_rgb) # RGBからPILフォーマットへ変換
+    image_pil.thumbnail((w,h), Image.ANTIALIAS)
+    image_tk  = ImageTk.PhotoImage(image_pil) # ImageTkフォーマットへ変換
+    return image_tk
+
+def load_image(path):
+    global canvas, canvas_item, image_tk
+    image_tk = create_photo_image(path)
+    if canvas_item == None:
+        canvas_item = canvas.create_image(0, 0, image=image_tk, anchor=tk.NW)
+    else:
+        canvas.itemconfig(canvas_item,image=image_tk)
+
+def load_detail(image_path):
+    image_exist = False
+    for ext in [".jpg",".png"]:
+        if os.path.exists(image_path):
+            load_image(image_path)
+            image_exist = True
+            break
+
+
+# ======================
+# Run model
+# ======================
+
+proc = None
+
+def open_model(model):
+    global proc
+
+    if not (proc==None):
+        proc.kill()
+        proc=None
+
+    model_request = model_list[model_index]
+
+    dir = "./"+model["category"]+"/"+model["model"]+"/"
+    cmd = sys.executable
+
+    args_dict = vars(args)
+
+    if "Camera:" in input_list[input_index]:
+        video_name = input_index
+    else:
+        video_name = input_list[input_index]
+    
+    if "Display:" in output_list[output_index]:
+        save_name = "temp.png"
+        save_path = "./"+model_request["category"]+"/"+model_request["model"]+"/"+"temp.png"
+    else:
+        save_name = output_list[output_index]
+        save_path = output_list[output_index]
+
+    if not(("natural_language_processing" == model["category"]) or ("audio_processing" == model["category"])):
+        if ".png" in str(video_name) or ".jpg" in str(video_name):
+            if "video" in args_dict:
+                del args_dict["video"]
+            args_dict["input"]=video_name
+            args_dict["savepath"]=save_name
+        else:
+            args_dict["video"]=video_name
+            if not ("Display:" in output_list[output_index]):
+                args_dict["savepath"]=save_name
+
+    args_dict["env_id"]=env_index
+
+    options = []
+    for key in args_dict:
+        if key=="ftype":
+            continue
+        if args_dict[key] is not None:
+            if args_dict[key] is True:
+                options.append("--"+key)
+            elif args_dict[key] is False:
+                continue
+            else:
+                options.append("--"+key)
+                options.append(str(args_dict[key]))
+    
+    cmd = [cmd, model["model"]+".py"] + options
+    print(" ".join(cmd))
+
+    if not ("video" in args_dict):
+        subprocess.check_call(cmd, cwd=dir, shell=False)
+        load_image(save_path)
+    else:
+        proc = subprocess.Popen(cmd, cwd=dir)
+        try:
+            outs, errs = proc.communicate(timeout=1)
+        except subprocess.TimeoutExpired:
+            pass
+
+def train_button_clicked():
+    print("begin training")
+
+def test_button_clicked():
+    print("begin test")
+
+
+# ======================
+# Select file
+# ======================
+
+def train_file_dialog():
+    global listsInput, ListboxInput, input_index
+    fTyp = [("Image File or Video File", "*")]
+    iDir = os.path.abspath(os.path.dirname(__file__))
+    file_name = tk.filedialog.askopenfilename(filetypes=fTyp, initialdir=iDir)
+    if len(file_name) != 0:
+        input_list.append(file_name)
+        listsInput.set(input_list)
+        ListboxInput.select_clear(input_index)
+        input_index = len(input_list)-1
+        ListboxInput.select_set(input_index)
+
+def test_file_dialog():
+    global listsOutput, ListboxOutput, output_index
+    fTyp = [("Image File or Video File", "*")]
+    iDir = os.path.abspath(os.path.dirname(__file__))
+    file_name = tk.filedialog.asksaveasfilename(filetypes=fTyp, initialdir=iDir)
+    if len(file_name) != 0:
+        output_list.append(file_name)
+        listsOutput.set(output_list)
+        ListboxOutput.select_clear(output_index)
+        output_index = len(output_list)-1
+        ListboxOutput.select_set(output_index)
+
+def get_training_file_list():
+    base_path = "train/"
+    files = glob.glob(base_path+"*.jpg")
+    files.extend(glob.glob(base_path+"*.png"))
+    image_list = []
+    for image_path in files:
+        image_list.append(image_path)
+    return image_list
+
+def get_test_file_list():
+    return ["bottle_000.png"]
+
+# ======================
+# GUI
+# ======================
+
+canvas_item = None
+
+def main():
+    global train_list, test_list
+    #global ListboxModel, textModelDetail
+    #global model_list, model_request, model_loading_cnt, invalidate_quit_cnt
+    global canvas
+    #global inputFile, listsInput, input_list, ListboxInput
+    #global outputFile, listsOutput, output_list, ListboxOutput
+
+    #model_list, model_name_list, category_cnt = get_model_list()
+
+    # rootメインウィンドウの設定
+    root = tk.Tk()
+    root.title("PaDiM GUI")
+    root.geometry("1200x600")
+
+    # メインフレームの作成と設置
+    frame = ttk.Frame(root)
+    frame.pack(padx=20,pady=10)
+
+    # Listboxの選択肢
+    train_list = get_training_file_list()
+    test_list = get_test_file_list()
+
+    listsInput = tk.StringVar(value=train_list)
+    listsOutput = tk.StringVar(value=test_list)
+
+    # 各種ウィジェットの作成
+    ListboxInput = tk.Listbox(frame, listvariable=listsInput, width=40, height=12, selectmode="single", exportselection=False)
+    ListboxOutput = tk.Listbox(frame, listvariable=listsOutput, width=40, height=12, selectmode="single", exportselection=False)
+
+    ListboxInput.bind("<<ListboxSelect>>", input_changed)
+    ListboxOutput.bind("<<ListboxSelect>>", output_changed)
+
+    ListboxInput.select_set(input_index)
+    ListboxOutput.select_set(output_index)
+
+    # スクロールバーの作成
+    scrollbar = tk.Scrollbar(frame, orient=tk.VERTICAL, command=ListboxInput.yview)
+
+    # スクロールバーをListboxに反映
+    ListboxInput["yscrollcommand"] = scrollbar.set
+
+    # StringVarのインスタンスを格納する変数textの設定
+    textRun = tk.StringVar(frame)
+    textRun.set("Train")
+
+    textStop = tk.StringVar(frame)
+    textStop.set("Test")
+
+    textInputFile = tk.StringVar(frame)
+    textInputFile.set("Select train folder")
+
+    textOutputFile = tk.StringVar(frame)
+    textOutputFile.set("Add test image")
+
+    textInput = tk.StringVar(frame)
+    textInput.set("Train images")
+
+    textOutput = tk.StringVar(frame)
+    textOutput.set("Test images")
+
+    textModelDetail = tk.StringVar(frame)
+    textModelDetail.set("Preview")
+
+    # 各種ウィジェットの作成
+    #labelModel = tk.Label(frame, textvariable=textModel)
+    labelInput = tk.Label(frame, textvariable=textInput)
+    labelOutput = tk.Label(frame, textvariable=textOutput)
+    labelModelDetail = tk.Label(frame, textvariable=textModelDetail)
+
+    buttonRun = tk.Button(frame, textvariable=textRun, command=train_button_clicked, width=14)
+    buttonStop = tk.Button(frame, textvariable=textStop, command=test_button_clicked, width=14)
+    buttonInputFile = tk.Button(frame, textvariable=textInputFile, command=train_file_dialog, width=14)
+    buttonOutputFile = tk.Button(frame, textvariable=textOutputFile, command=test_file_dialog, width=14)
+
+    canvas = tk.Canvas(frame, bg="black", width=320, height=240)
+    canvas.place(x=0, y=0)
+    #load_detail(model_index)
+
+    logo = tk.Canvas(frame, bg="black", width=320, height=124)
+    logo.place(x=0, y=0)
+    global logo_img
+    logo_img = create_photo_image("../../ailia-models.png",320,124)
+    logo_item = logo.create_image(0, 0, image=logo_img, anchor=tk.NW)
+
+    # 各種ウィジェットの設置
+    labelInput.grid(row=0, column=0, sticky=tk.NW, rowspan=1)
+    ListboxInput.grid(row=1, column=0, sticky=tk.NW, rowspan=12)
+    buttonInputFile.grid(row=13, column=0, sticky=tk.NW)
+    buttonRun.grid(row=14, column=0, sticky=tk.NW)
+    scrollbar.grid(row=1, column=1, sticky=(tk.N, tk.S), rowspan=12)
+
+    labelOutput.grid(row=0, column=2, sticky=tk.NW)
+    ListboxOutput.grid(row=1, column=2, sticky=tk.NW, rowspan=12)
+    buttonOutputFile.grid(row=13, column=2, sticky=tk.NW)
+    buttonStop.grid(row=14, column=2, sticky=tk.NW)
+
+    labelModelDetail.grid(row=0, column=3, sticky=tk.NW)
+    canvas.grid(row=1, column=3, sticky=tk.NW, rowspan=4)
+
+    logo.grid(row=6, column=3, sticky=tk.NW, columnspan=2, rowspan=1)
+
+    # メインフレームの作成と設置
+    frame = ttk.Frame(root)
+    frame.pack(padx=20, pady=10)
+
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
+
+
+
