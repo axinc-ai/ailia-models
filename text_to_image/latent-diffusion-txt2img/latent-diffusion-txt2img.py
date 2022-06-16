40f367da0a62ae378a3c8e508938fc04fb042177
satoshi.ooe <ooe1123@gmail.com>
2022-06-16 23:07:18 +0900

    Implement options

--- text_to_image/latent-diffusion-txt2img/latent-diffusion-txt2img.py
+++ text_to_image/latent-diffusion-txt2img/latent-diffusion-txt2img.py
@@ -1,9 +1,9 @@
+import os
 import sys
 import time
 
 import numpy as np
 import cv2
-from PIL import Image
 
 from transformers import BertTokenizerFast
 
@@ -13,9 +13,6 @@ import ailia
 sys.path.append('../../util')
 from utils import get_base_parser, update_parser, get_savepath  # noqa
 from model_utils import check_and_download_models  # noqa
-from detector_utils import load_image  # noqa
-# from image_utils import load_image, normalize_image  # noqa
-from webcamera_utils import get_capture, get_writer  # noqa
 # logger
 from logging import getLogger  # noqa
 
@@ -41,19 +38,58 @@ WEIGHT_AUTO_ENC_PATH = 'autoencoder.onnx'
 MODEL_AUTO_ENC_PATH = 'autoencoder.onnx.prototxt'
 REMOTE_PATH = 'https://storage.googleapis.com/ailia-models/latent-diffusion-txt2img/'
 
+SAVE_IMAGE_PATH = 'output.png'
+
 # ======================
 # Arguemnt Parser Config
 # ======================
 
 parser = get_base_parser(
-    'Latent Diffusion', None, None
+    'Latent Diffusion', None, SAVE_IMAGE_PATH
+)
+parser.add_argument(
+    "-i", "--input", metavar="TEXT", type=str,
+    default="a painting of a virus monster playing guitar",
+    help="the prompt to render"
+)
+parser.add_argument(
+    "--n_iter", type=int, default=1,
+    help="sample this often",
+)
+parser.add_argument(
+    "--n_samples", type=int, default=4,
+    help="how many samples to produce for the given prompt",
+)
+parser.add_argument(
+    "--ddim_steps", type=int, default=50,
+    help="number of ddim sampling steps",
+)
+parser.add_argument(
+    "--ddim_eta", type=float, default=0.0,
+    help="ddim eta (eta=0.0 corresponds to deterministic sampling)",
+)
+parser.add_argument(
+    "--H", metavar="height", type=int, default=256,
+    help="image height, in pixel space",
+)
+parser.add_argument(
+    "--W", metavar="width", type=int, default=256,
+    help="image width, in pixel space",
+)
+parser.add_argument(
+    "--scale", type=float, default=5.0,
+    help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
+)
+parser.add_argument(
+    "--seed", type=int, default=None,
+    help="random seed",
 )
 parser.add_argument(
     '--onnx',
     action='store_true',
     help='execute onnxruntime version.'
 )
-args = update_parser(parser)
+args = update_parser(parser, check_input_type=False)
 
 
 # ======================
@@ -88,7 +124,7 @@ def make_ddim_sampling_parameters(alphacums, ddim_timesteps, eta):
 """
 ddim_timesteps
 """
-ddim_num_steps = 50
+ddim_num_steps = args.ddim_steps
 ddpm_num_timesteps = 1000
 ddim_timesteps = make_ddim_timesteps(
     ddim_num_steps, ddpm_num_timesteps)
@@ -96,7 +132,7 @@ ddim_timesteps = make_ddim_timesteps(
 """
 ddim sampling parameters
 """
-ddim_eta = 0.0
+ddim_eta = args.ddim_eta
 ddim_sigmas, ddim_alphas, ddim_alphas_prev = \
     make_ddim_sampling_parameters(
         alphacums=alphas_cumprod,
@@ -145,7 +181,7 @@ def ddim_sampling(
         unconditional_guidance_scale=1.0,
         unconditional_conditioning=None):
     img = np.random.randn(shape[0] * shape[1] * shape[2] * shape[3]).reshape(shape)
-    img = np.load("img.npy")
+    img = img.astype(np.float32)
 
     timesteps = ddim_timesteps
     time_range = np.flip(timesteps)
@@ -153,8 +189,16 @@ def ddim_sampling(
 
     logger.info(f"Running DDIM Sampling with {total_steps} timesteps")
 
-    from tqdm import tqdm
-    iterator = tqdm(time_range, desc='DDIM Sampler', total=total_steps)
+    try:
+        from tqdm import tqdm
+        iterator = tqdm(time_range, desc='DDIM Sampler', total=total_steps)
+    except ModuleNotFoundError:
+        def iter_func(a):
+            for i, x in enumerate(a):
+                print("DDIM Sampler: %s/%s" % (i + 1, len(a)))
+                yield x
+
+        iterator = iter_func(time_range)
 
     for i, step in enumerate(iterator):
         index = total_steps - i - 1
@@ -261,49 +305,105 @@ def decode_first_stage(models, z):
     return dec
 
 
+def predict(
+        models, cond_stage_model,
+        prompt, uc):
+    n_samples = args.n_samples
+    scale = args.scale
+    H = args.H
+    W = args.W
+
+    c = cond_stage_model.encode([prompt] * n_samples)
+    shape = [n_samples, 4, H // 8, W // 8]
+
+    samples = ddim_sampling(
+        models, c, shape,
+        unconditional_guidance_scale=scale,
+        unconditional_conditioning=uc)
+
+    x_samples_ddim = decode_first_stage(models, samples)
+    x_samples_ddim = np.clip((x_samples_ddim + 1.0) / 2.0, a_min=0.0, a_max=1.0)
+
+    x_samples = []
+    for x_sample in x_samples_ddim:
+        x_sample = x_sample.transpose(1, 2, 0)  # CHW -> HWC
+        x_sample = x_sample * 255
+        img = x_sample.astype(np.uint8)
+        img = img[:, :, ::-1]  # RGB -> BGR
+        x_samples.append(img)
+
+    return x_samples
+
+
 def recognize_from_text(models):
-    n_samples = 4
-    n_iter = 1
-    scale = 5.0
-    H = W = 256
+    n_iter = 1 if args.benchmark else args.n_iter
+    n_samples = args.n_samples
+    scale = args.scale
 
     transformer_emb = models['transformer_emb']
     transformer_attn = models['transformer_attn']
     cond_stage_model = BERTEmbedder(transformer_emb, transformer_attn)
 
+    prompt = args.input if isinstance(args.input, str) else args.input[0]
+    logger.info("prompt: %s" % prompt)
+
+    sample_path = os.path.join('outputs', prompt.replace(" ", "-"))
+    os.makedirs(sample_path, exist_ok=True)
+    base_count = len(os.listdir(sample_path))
+
+    logger.info('Start inference...')
     uc = None
     if scale != 1.0:
         uc = cond_stage_model.encode([""] * n_samples)
 
-    prompt = "korean girl"
-    for _ in range(n_iter):
-        c = cond_stage_model.encode([prompt] * n_samples)
-        shape = [n_samples, 4, H // 8, W // 8]
+    all_samples = []
+    for i in range(n_iter):
+        logger.info("iteration: %s" % (i + 1))
 
-        samples = ddim_sampling(
-            models, c, shape,
-            unconditional_guidance_scale=scale,
-            unconditional_conditioning=uc)
-        # samples = np.load("samples.npy")
-        # print("samples_ddim---", samples_ddim)
-        # print("samples_ddim---", samples_ddim.shape)
+        if args.benchmark:
+            logger.info('BENCHMARK mode')
+            total_time_estimation = 0
+            for i in range(args.benchmark_count):
+                start = int(round(time.time() * 1000))
+                x_samples = predict(models, cond_stage_model, prompt, uc)
+                end = int(round(time.time() * 1000))
+                estimation_time = (end - start)
 
-        x_samples_ddim = decode_first_stage(models, samples)
-        x_samples_ddim = np.clip((x_samples_ddim + 1.0) / 2.0, a_min=0.0, a_max=1.0)
+                # Logging
+                logger.info(f'\tailia processing estimation time {estimation_time} ms')
+                if i != 0:
+                    total_time_estimation = total_time_estimation + estimation_time
 
-        base_count = 0
-        for x_sample in x_samples_ddim:
-            x_sample = x_sample.transpose(1, 2, 0)  # CHW -> HWC
-            x_sample = x_sample * 255
-            img = x_sample.astype(np.uint8)
+            logger.info(f'\taverage time estimation {total_time_estimation / (args.benchmark_count - 1)} ms')
+        else:
+            x_samples = predict(models, cond_stage_model, prompt, uc)
 
-            Image.fromarray(img).save(f"{base_count:04}.png")
+        for img in x_samples:
+            sample_file = os.path.join(sample_path, f"{base_count:04}.png")
+            cv2.imwrite(sample_file, img)
             base_count += 1
+
+        x_samples = np.concatenate(x_samples, axis=1)
+        all_samples.append(x_samples)
+
+    grid_img = np.concatenate(all_samples, axis=0)
+
+    # plot result
+    savepath = get_savepath(args.savepath, "", ext='.png')
+    logger.info(f'saved at : {savepath}')
+    cv2.imwrite(savepath, grid_img)
 
     logger.info('Script finished successfully.')
 
 
 def main():
+    check_and_download_models(WEIGHT_TRANS_EMB_PATH, MODEL_TRANS_EMB_PATH, REMOTE_PATH)
+    check_and_download_models(WEIGHT_TRANS_ATTN_PATH, MODEL_TRANS_ATTN_PATH, REMOTE_PATH)
+    check_and_download_models(WEIGHT_DFSN_EMB_PATH, MODEL_DFSN_EMB_PATH, REMOTE_PATH)
+    check_and_download_models(WEIGHT_DFSN_MID_PATH, MODEL_DFSN_MID_PATH, REMOTE_PATH)
+    check_and_download_models(WEIGHT_DFSN_OUT_PATH, MODEL_DFSN_OUT_PATH, REMOTE_PATH)
+    check_and_download_models(WEIGHT_AUTO_ENC_PATH, MODEL_AUTO_ENC_PATH, REMOTE_PATH)
+
     env_id = args.env_id
 
     # initialize
@@ -332,6 +432,10 @@ def main():
         diffusion_mid = onnxruntime.InferenceSession(WEIGHT_DFSN_MID_PATH)
         diffusion_out = onnxruntime.InferenceSession(WEIGHT_DFSN_OUT_PATH)
         autoencoder = onnxruntime.InferenceSession(WEIGHT_AUTO_ENC_PATH)
+
+    seed = args.seed
+    if seed is not None:
+        np.random.seed(seed)
 
     models = dict(
         transformer_emb=transformer_emb,
