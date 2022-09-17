84c5be67a709e419c31e136f2f1f778901f0224d
ooe1123 <ooe1123@gmail.com>
2022-09-17 22:24:00 +0900

    Implementing GFM

---
+++ background_removal/gfm/gfm.py
@@ -0,0 +1,254 @@
+import sys
+import time
+
+import numpy as np
+import cv2
+from skimage.transform import resize
+
+import ailia
+
+# import original modules
+sys.path.append('../../util')
+from utils import get_base_parser, update_parser, get_savepath  # noqa
+from model_utils import check_and_download_models  # noqa
+from image_utils import normalize_image  # noqa
+from detector_utils import load_image  # noqa
+from webcamera_utils import get_capture, get_writer  # noqa
+# logger
+from logging import getLogger  # noqa
+
+logger = getLogger(__name__)
+
+# ======================
+# Parameters
+# ======================
+
+WEIGHT_PATH = 'xxx.onnx'
+MODEL_PATH = 'xxx.onnx.prototxt'
+REMOTE_PATH = 'https://storage.googleapis.com/ailia-models/gfm/'
+
+IMAGE_PATH = 'demo.jpg'
+SAVE_IMAGE_PATH = 'output.png'
+
+IMAGE_SIZE = 480
+
+# ======================
+# Arguemnt Parser Config
+# ======================
+
+parser = get_base_parser(
+    'GFM', IMAGE_PATH, SAVE_IMAGE_PATH
+)
+parser.add_argument(
+    '-m', '--model_type', default='xxx', choices=('xxx', 'XXX'),
+    help='model type'
+)
+parser.add_argument(
+    '--onnx',
+    action='store_true',
+    help='execute onnxruntime version.'
+)
+args = update_parser(parser)
+
+
+# ======================
+# Secondaty Functions
+# ======================
+
+# def draw_bbox(img, bboxes):
+#     return img
+
+
+# ======================
+# Main functions
+# ======================
+
+def preprocess(img):
+    im_h, im_w, _ = img.shape
+
+    img = img[:, :, ::-1]
+    return img
+
+    # adaptive_resize
+    scale = IMAGE_SIZE / max(im_h / 2, im_w / 2)
+    if scale < 1:
+        ow, oh = int(im_w * scale), int(im_h * scale)
+        img = cv2.resize(img, (ow, oh), interpolation=cv2.INTER_LINEAR)
+    else:
+        ow, oh = im_w, im_h
+        scale = 1
+
+    # img = img / 255
+    # img = img.transpose(2, 0, 1)  # HWC -> CHW
+    # img = np.expand_dims(img, axis=0)
+    # img = img.astype(np.float32)
+
+    return img
+
+
+def resize_pad(img, ratio):
+    h, w, _ = img.shape
+
+    resize_h = int(h * ratio)
+    resize_w = int(w * ratio)
+    resize_h = min(1080, resize_h - (resize_h % 32))
+    resize_w = min(1080, resize_w - (resize_w % 32))
+
+    scale_img = resize(img, (resize_h, resize_w)) * 255.0
+
+    if resize_w != IMAGE_SIZE or resize_h != IMAGE_SIZE:
+        pad_img = np.ones((IMAGE_SIZE, IMAGE_SIZE, 3)) * 255
+        pad_img[:resize_h, :resize_w, ...] = scale_img
+        scale_img = pad_img
+
+    return scale_img
+
+
+def post_processing(output):
+    return None
+
+
+def predict(net, img):
+    img = preprocess(img)
+
+    global_ratio = 1 / 3
+    local_ratio = 1 / 2
+
+    print(img)
+    print(img.shape)
+    print("-------")
+    scale_img = resize_pad(img, global_ratio)
+    1 / 0
+
+    # feedforward
+    if not args.onnx:
+        output = net.predict([scale_img])
+    else:
+        output = net.run(None, {'img': scale_img})
+
+    scale_img = resize_pad(img, local_ratio)
+
+    # feedforward
+    if not args.onnx:
+        output = net.predict([scale_img])
+    else:
+        output = net.run(None, {'img': scale_img})
+
+    pred = post_processing(output)
+
+    return pred
+
+
+def recognize_from_image(net):
+    # input image loop
+    for image_path in args.input:
+        logger.info(image_path)
+
+        # prepare input data
+        img = load_image(image_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+
+        # inference
+        logger.info('Start inference...')
+        if args.benchmark:
+            logger.info('BENCHMARK mode')
+            total_time_estimation = 0
+            for i in range(args.benchmark_count):
+                start = int(round(time.time() * 1000))
+                out = predict(net, img)
+                end = int(round(time.time() * 1000))
+                estimation_time = (end - start)
+
+                # Logging
+                logger.info(f'\tailia processing estimation time {estimation_time} ms')
+                if i != 0:
+                    total_time_estimation = total_time_estimation + estimation_time
+
+            logger.info(f'\taverage time estimation {total_time_estimation / (args.benchmark_count - 1)} ms')
+        else:
+            out = predict(net, img)
+
+        res_img = draw_bbox(out)
+
+        # plot result
+        savepath = get_savepath(args.savepath, image_path, ext='.png')
+        logger.info(f'saved at : {savepath}')
+        cv2.imwrite(savepath, res_img)
+
+    logger.info('Script finished successfully.')
+
+
+def recognize_from_video(net):
+    video_file = args.video if args.video else args.input[0]
+    capture = get_capture(video_file)
+    assert capture.isOpened(), 'Cannot capture source'
+
+    # create video writer if savepath is specified as video format
+    if args.savepath != SAVE_IMAGE_PATH:
+        f_h = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        f_w = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        writer = get_writer(args.savepath, f_h, f_w)
+    else:
+        writer = None
+
+    frame_shown = False
+    while True:
+        ret, frame = capture.read()
+        if (cv2.waitKey(1) & 0xFF == ord('q')) or not ret:
+            break
+        if frame_shown and cv2.getWindowProperty('frame', cv2.WND_PROP_VISIBLE) == 0:
+            break
+
+        # inference
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        out = predict(net, img)
+
+        # plot result
+        res_img = draw_bbox(frame, out)
+
+        # show
+        cv2.imshow('frame', res_img)
+        frame_shown = True
+
+        # save results
+        if writer is not None:
+            res_img = res_img.astype(np.uint8)
+            writer.write(res_img)
+
+    capture.release()
+    cv2.destroyAllWindows()
+    if writer is not None:
+        writer.release()
+
+    logger.info('Script finished successfully.')
+
+
+def main():
+    # dic_model = {
+    #     'xxx': (WEIGHT_XXX_PATH, MODEL_XXX_PATH),
+    #     'XXX': (WEIGHT_XXX_PATH, MODEL_XXX_PATH),
+    # }
+    # WEIGHT_PATH, MODEL_PATH = dic_model[args.model_type]
+    WEIGHT_PATH = "gfm_r34_2b_tt.onnx"
+    MODEL_PATH = "gfm_r34_2b_tt.onnx.prototxt"
+
+    # model files check and download
+    check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
+
+    env_id = args.env_id
+
+    # initialize
+    if not args.onnx:
+        net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=env_id)
+    else:
+        import onnxruntime
+        net = onnxruntime.InferenceSession(WEIGHT_PATH)
+
+    if args.video is not None:
+        recognize_from_video(net)
+    else:
+        recognize_from_image(net)
+
+
+if __name__ == '__main__':
+    main()
