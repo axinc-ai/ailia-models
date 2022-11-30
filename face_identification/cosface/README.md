0ee37e17694a2cf6201088610fd1c85d6fcc5f17
masayashimomatsu <masa.shimomatsu@gmail.com>
2022-11-30 23:28:32 +0100

    Updated as reviewed

--- face_identification/cosface/README.md
+++ face_identification/cosface/README.md
@@ -1,5 +1,34 @@
 # CosFace
 
+### Input
+| Image filename   | Image                       |
+|------------------|-----------------------------|
+ | image_id.jpg     | ![Input](image_id.jpg)      |
+ | image_target.jpg | ![Input](image_target.jpg)] |
+(Image from https://github.com/ronghuaiyang/arcface-pytorch/issues/63)
+ - Ailia input shape : (1, 3, 96, 112)
+ - Range: [-1.0,1.0]
+
+### Output
+ A similarity of a pair of images.
+
+### Usage
+Automatically downloads the onnx and onnx file on the first run.  
+It is necessary to be connected to the Internet while downloading.
+
+By default, the following two images are loaded: `image_id.jpg`, `image_target.jpg` 
+``` bash
+$ python3 acosface.py
+...
+Similarity of (image_id.jpg, image_target.jpg) : 0.209
+Script finished successfully.
+```
+
+If you want to specify images, specify the paths of the two images after the `--inputs` option.
+``` bash
+$ python3 cosface.py --inputs IMAGE_PATH1 IMAGE_PATH2
+```
+
 ### Paper
  - CosFace: Large Margin Cosine Loss for Deep Face Recognition
    - https://arxiv.org/abs/1801.09414
@@ -7,21 +36,8 @@
 ### Reference
  - https://github.com/MuggleWang/CosFace_pytorch
 
-### Sample Code
-  - cosface.py  # Output similarity from two face images
-  - requirements.txt # Dependencies
-#### Usages
- - Install dependencies
-   - `pip install -r requirements.txt`
- - Show Help 
-   - `python cosface.py --help`
- - Get similarity from 2 face images
-   - `python cosface.py --target_image=image_target.jpg --id_image=image_id.jpg --onnx=cosface_sphere20.onnx`
+### Dependencies
+  - requirements.txt
 
-#### Input
-- Ailia input shape : (1, 3, 96, 112)
-
-| Sample input file | Image                       |
-|-------------------|-----------------------------|
- | image_target.jpg  | ![Input](image_target.jpg)] |
- | image_id.jpg      | ![Input](image_id.jpg)      |
+### Model Format
+  - ONNX opset = 10
--- face_identification/cosface/cosface.py
+++ face_identification/cosface/cosface.py
@@ -1,20 +1,54 @@
-import argparse
+import sys
+
+sys.path.append('../../util')
+from utils import get_base_parser, update_parser
+from model_utils import check_and_download_models
 
 import numpy as np
 import ailia
 from PIL import Image
-
 from torchvision.transforms import functional as F
 import torchvision.transforms as transforms
 
-parser = argparse.ArgumentParser(description='ailia CosFace')
-parser.add_argument('--target_image', type=str, default='image_target.jpg',
-                    help='Test Target image path for prediction')
-parser.add_argument('--id_image', type=str, default='image_id.jpg',
-                    help='ID image path for prediction')
-parser.add_argument('--onnx', type=str, default='cosface_sphere20.onnx',
-                    help='Onnx model file path')
-args = parser.parse_args()
+# logger
+from logging import getLogger   # noqa: E402
+logger = getLogger(__name__)
+
+# ======================
+# PARAMETERS
+# ======================
+MODEL_LISTS = [
+    'cosface_sphere20_opset10'
+]
+
+REMOTE_PATH = "https://storage.googleapis.com/ailia-models/cosface/"
+IMG_PATH_1 = 'image_id.jpg'
+IMG_PATH_2 = 'image_target.jpg'
+
+# ======================
+# Arguemnt Parser Config
+# ======================
+parser = get_base_parser(
+    'Determine if the person is the same from two facial images by CosFace',
+    None,
+    None,
+)
+
+# Overwrite default config
+# NOTE: cosface has different usage for `--input` with other models
+parser.add_argument(
+    '-i', '--inputs', metavar='IMAGE', nargs=2, default='',
+    help='Two image paths for calculating the face match.'
+)
+parser.add_argument(
+    '-a', '--arch', metavar='ARCH',
+    default='CosFace_sphere20_opset10', choices=MODEL_LISTS,
+    help='Supported model: ' + ' | '.join(MODEL_LISTS)
+)
+
+args = update_parser(parser)
+
+WEIGHT_PATH = args.arch + '.onnx'
 
 
 def preprocessing(img_path):
@@ -38,23 +72,37 @@ def extractDeepFeature(images, model):
     return ft
 
 
-def cosFace(query_img_path, id_img_path, model_path):
-    # pre-processing
-    img1 = preprocessing(query_img_path)
-    img2 = preprocessing(id_img_path)
+def cosFace(id_img_path, query_img_path):
+    # Pre-processing
+    img1 = preprocessing(id_img_path)
+    img2 = preprocessing(query_img_path)
 
-    # inference ( extract feature )
-    model = ailia.Net(weight=model_path)
+    # Inference ( extract feature )
+    model = ailia.Net(weight=WEIGHT_PATH, env_id=args.env_id)
     f1 = extractDeepFeature(img1, model)
     f2 = extractDeepFeature(img2, model)
 
-    # post-processing ( cosine distance )
+    # Post-processing ( cosine distance )
     cosine_distance = f1.dot(f2) / (np.linalg.norm(f1) * np.linalg.norm(f2) + 1e-5)
     return cosine_distance
 
 
+def main():
+    # Check and download CosFace model
+    check_and_download_models(WEIGHT_PATH, None, REMOTE_PATH)
+
+    # Specify default images when there is no inputs
+    if len(args.inputs) == 0:
+        args.inputs = [IMG_PATH_1, IMG_PATH_2]
+
+    # Comparing two images
+    distance = cosFace(args.inputs[0],
+                       args.inputs[1])
+    logger.info(
+        f'Similarity of ({args.inputs[0]}, {args.inputs[1]}) : {distance:.3f}'
+    )
+    logger.info('Script finished successfully.')
+
+
 if __name__ == '__main__':
-    distance = cosFace(args.target_image,
-                       args.id_image,
-                       args.onnx)
-    print(distance)
+    main()
