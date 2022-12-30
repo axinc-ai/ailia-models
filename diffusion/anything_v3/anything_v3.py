3ea449f0485db28a065e399d0e95ec7ebd9776cc
宮本蒼太 <miyamotost1016@yahoo.co.jp>
2022-12-30 23:19:44 +0900

    Add anything_v3

---
+++ diffusion/anything_v3/anything_v3.py
@@ -0,0 +1,155 @@
+import os
+import sys
+import time
+
+import numpy as np
+import cv2
+
+import ailia
+
+import random
+
+# import original modules
+sys.path.append('../../util')
+from utils import get_base_parser, update_parser, get_savepath  # noqa
+from model_utils import check_and_download_models  # noqa
+# logger
+from logging import getLogger  # noqa
+
+logger = getLogger(__name__)
+
+
+# ======================
+# Parameters
+# ======================
+
+WEIGHT_UNET_PATH = 'unet.onnx'
+WEIGHT_PB_UNET_PATH = 'weights.pb'
+MODEL_UNET_PATH = 'unet.onnx.prototxt'
+WEIGHT_SAFETY_CHECKER_PATH = 'safety_checker.onnx'
+MODEL_SAFETY_CHECKER_PATH = 'safety_checker.onnx.prototxt'
+WEIGHT_TEXT_ENCODER_PATH = 'text_encoder.onnx'
+MODEL_TEXT_ENCODER_PATH = 'text_encoder.onnx.prototxt'
+WEIGHT_VAE_ENCODER_PATH = 'vae_encoder.onnx'
+MODEL_VAE_ENCODER_PATH = 'vae_encoder.onnx.prototxt'
+WEIGHT_VAE_DECODER_PATH = 'vae_decoder.onnx'
+MODEL_VAE_DECODER_PATH = 'vae_decoder.onnx.prototxt'
+
+SAVE_IMAGE_PATH = 'output.png'
+
+
+# ======================
+# Arguemnt Parser Config
+# ======================
+parser = get_base_parser(
+    'Anything V3', None, SAVE_IMAGE_PATH
+)
+parser.add_argument(
+    "-i", "--input", metavar="TEXT", type=str,
+    default="pikachu",
+    help="the prompt to render"
+)
+parser.add_argument(
+    '--onnx',
+    action='store_true',
+    help='execute onnxruntime version.'
+)
+args = update_parser(parser, check_input_type=False)
+
+
+# ======================
+# Main functions
+# ======================
+def recognize_from_text(pipe):
+    prompt = args.input if isinstance(args.input, str) else args.input[0]
+    logger.info("prompt: %s" % prompt)
+
+    output_dir = 'outputs'
+    os.makedirs(output_dir, exist_ok=True)
+    output_name = '{}_{}.png'.format(prompt.replace(" ", "-"), random.randint(10000, 99999))
+    output_path = os.path.join(output_dir, output_name)
+    logger.info(output_path)
+
+    logger.info('Start inference...')
+
+    image = pipe(prompt).images[0]
+    image.save(output_path)
+
+    logger.info('Script finished successfully.')
+
+
+def main():
+    #check_and_download_models()
+
+    env_id = args.env_id
+
+    # initialize
+    if not args.onnx:
+        logger.info("Ailia mode is not implemented.")
+        exit()
+    else:
+        import transformers
+        import df
+        from df import OnnxRuntimeModel
+
+        unet = OnnxRuntimeModel.from_pretrained(
+            "./", "unet.onnx",
+            {'provider': 'CPUExecutionProvider', 'sess_options': None}
+        )
+        safety_checker = OnnxRuntimeModel.from_pretrained(
+            "./", "safety_checker.onnx",
+            {'provider': 'CPUExecutionProvider', 'sess_options': None}
+        )
+        vae_decoder = OnnxRuntimeModel.from_pretrained(
+            "./", "vae_decoder.onnx",
+            {'provider': 'CPUExecutionProvider', 'sess_options': None}
+        )
+        pndm_scheduler = df.schedulers.scheduling_pndm.PNDMScheduler.from_pretrained(
+            "./scheduler"
+        )
+        feature_extractor = transformers.CLIPImageProcessor.from_pretrained(
+            "./feature_extractor"
+        )
+        text_encoder = OnnxRuntimeModel.from_pretrained(
+            "./", "text_encoder.onnx",
+            {'provider': 'CPUExecutionProvider', 'sess_options': None}
+        )
+        vae_encoder = OnnxRuntimeModel.from_pretrained(
+            "./", "vae_encoder.onnx",
+            {'provider': 'CPUExecutionProvider', 'sess_options': None}
+        )
+        tokenizer = transformers.CLIPTokenizer.from_pretrained(
+            "./tokenizer"
+        )
+
+    # set pipeline
+    use_transformers = True
+    use_diffusers = False
+
+    if use_diffusers and use_transformers:
+        import diffusers
+        pipeline_cls = diffusers.OnnxStableDiffusionPipeline
+    elif not use_diffusers and use_transformers:
+        pipeline_cls = df.OnnxStableDiffusionPipeline
+    else:
+        print('not implemented.')
+        exit()
+    
+    pipe = pipeline_cls(
+        vae_encoder = vae_encoder,
+        vae_decoder = vae_decoder,
+        text_encoder = text_encoder,
+        tokenizer = tokenizer,
+        unet = unet,
+        scheduler = pndm_scheduler,
+        safety_checker = safety_checker,
+        feature_extractor = feature_extractor,
+        requires_safety_checker = True       
+    )
+
+    # generate
+    recognize_from_text(pipe)
+
+
+if __name__ == '__main__':
+    main()
