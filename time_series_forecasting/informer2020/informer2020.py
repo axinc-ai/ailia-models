13c943cf8f7c9e1570e857f8e80378570a52a1c7
宮本蒼太 <miyamotost1016@yahoo.co.jp>
2023-02-10 01:37:02 +0900

    Implementation of Informer2020

---
+++ time_series_forecasting/informer2020/informer2020.py
@@ -0,0 +1,284 @@
+import os
+import sys
+import ailia
+import numpy as np
+import time
+
+# import original modules
+sys.path.append('../../util')
+from image_utils import load_image, get_image_shape  # noqa: E402
+from utils import get_base_parser, update_parser, get_savepath  # noqa: E402
+import webcamera_utils  # noqa: E402
+from model_utils import check_and_download_models  # noqa: E402
+
+# logger
+from logging import getLogger   # noqa: E402
+logger = getLogger(__name__)
+
+from informer2020_utils import Dataset_Pred
+from torch.utils.data import DataLoader
+import torch
+import matplotlib.pyplot as plt
+import seaborn as sns
+import onnxruntime
+
+
+# ======================
+# Parameters
+# ======================
+
+DATA_PATH = 'input.csv'
+SAVE_DATA_PATH = 'output.csv'
+
+INFORMER_ETTH1_WEIGHT_PATH = 'informer_ETTh1.onnx'
+INFORMER_ETTH1_MODEL_PATH  = 'informer_ETTh1.onnx.prototxt'
+INFORMER_ETTM1_WEIGHT_PATH = 'informer_ETTm1.onnx'
+INFORMER_ETTM1_MODEL_PATH  = 'informer_ETTm1.onnx.prototxt'
+
+INFORMERSTACK_ETTH1_WEIGHT_PATH = 'informerstack_ETTh1.onnx'
+INFORMERSTACK_ETTH1_MODEL_PATH  = 'informerstack_ETTh1.onnx.prototxt'
+INFORMERSTACK_ETTM1_WEIGHT_PATH = 'informerstack_ETTm1.onnx'
+INFORMERSTACK_ETTM1_MODEL_PATH  = 'informerstack_ETTm1.onnx.prototxt'
+
+REMOTE_PATH = 'https://storage.googleapis.com/ailia-models/informer2020/'
+
+
+# ======================
+# Arguemnt Parser Config
+# ======================
+
+parser = get_base_parser(
+    'Informer: Beyond Efficient Transformer for Long Sequence Time-Series Forecasting', 
+    DATA_PATH, 
+    SAVE_DATA_PATH
+)
+parser.add_argument(
+    '-m', '--model', metavar='MODEL', default="informer", 
+    choices=['informer', 'informerstack']
+)
+parser.add_argument(
+    '-d', '--data', default="ETTh1", 
+    choices=['ETTh1', 'ETTm1']
+)
+parser.add_argument(
+    '--onnx', action='store_true',
+    help='By default, the ailia SDK is used, but with this option, you can switch to using ONNX Runtime'
+)
+args = update_parser(parser)
+
+
+# ======================
+# Main functions
+# ======================
+
+def _get_data():
+    timeenc = 0 if args.embed!='timeF' else 1
+
+    shuffle_flag = False; 
+    drop_last = False; 
+    batch_size = 1; 
+    freq=args.detail_freq
+
+    data_set = Dataset_Pred(
+        root_path='./',
+        data_path=args.data_path,
+        flag='pred',
+        size=[args.seq_len, args.label_len, args.pred_len],
+        features=args.features,
+        target=args.target,
+        inverse=args.inverse,
+        timeenc=timeenc,
+        freq=freq,
+        cols=args.cols
+    )
+
+    data_loader = DataLoader(
+        data_set,
+        batch_size=batch_size,
+        shuffle=shuffle_flag,
+        num_workers=args.num_workers,
+        drop_last=drop_last)
+
+    return data_set, data_loader
+
+
+def _process_one_batch(net, dataset_object, batch_x, batch_y, batch_x_mark, batch_y_mark):
+    batch_x = batch_x.float()
+    batch_y = batch_y.float()
+
+    batch_x_mark = batch_x_mark.float()
+    batch_y_mark = batch_y_mark.float()
+
+    # decoder input
+    if args.padding==0:
+        dec_inp = torch.zeros([batch_y.shape[0], args.pred_len, batch_y.shape[-1]]).float()
+    elif args.padding==1:
+        dec_inp = torch.ones([batch_y.shape[0], args.pred_len, batch_y.shape[-1]]).float()
+    dec_inp = torch.cat([batch_y[:,:args.label_len,:], dec_inp], dim=1).float()
+    
+    # encoder - decoder
+    """
+    if self.args.use_amp:
+        with torch.cuda.amp.autocast():
+            if self.args.output_attention:
+                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+            else:
+                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+    else:
+        if self.args.output_attention:
+            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+        else:
+            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+    """
+
+    batch_x = batch_x.to('cpu').detach().numpy().copy()
+    batch_x_mark = batch_x_mark.to('cpu').detach().numpy().copy()
+    dec_inp = dec_inp.to('cpu').detach().numpy().copy()
+    batch_y_mark = batch_y_mark.to('cpu').detach().numpy().copy()
+
+    # dummy concatenation for batched input
+    batch_x = np.concatenate([batch_x, batch_x], axis=0)
+    batch_x_mark = np.concatenate([batch_x_mark, batch_x_mark], axis=0)
+    dec_inp = np.concatenate([dec_inp, dec_inp], axis=0)
+    batch_y_mark = np.concatenate([batch_y_mark, batch_y_mark], axis=0)
+
+    if not args.onnx:
+        outputs = net.predict({
+            'batch_x': batch_x, 
+            'batch_x_mark': batch_x_mark,
+            'dec_inp': dec_inp,
+            'batch_y_mark': batch_y_mark
+        })[0]
+    else:
+        outputs = net.run(
+            [
+                net.get_outputs()[0].name
+            ], 
+            {
+                net.get_inputs()[0].name: batch_x, 
+                net.get_inputs()[1].name: batch_x_mark,
+                net.get_inputs()[2].name: dec_inp,
+                net.get_inputs()[3].name: batch_y_mark
+            }
+        )[0]
+
+    outputs = torch.from_numpy(outputs[0, :, :])
+
+    if args.inverse:
+        outputs = dataset_object.inverse_transform(outputs)
+    f_dim = -1 if args.features=='MS' else 0
+    batch_y = batch_y[:,-args.pred_len:,f_dim:]
+
+    return outputs, batch_y
+
+
+def time_series_forecasting(net):
+    ### prepare dataset ###
+    pred_data, pred_loader = _get_data()
+
+    ### predict ###  
+    preds = []
+    trues = []
+    
+    for i, (batch_x,batch_y,batch_x_mark,batch_y_mark) in enumerate(pred_loader):
+        """
+            batch_x.shape = data_x[index : index+seq_len]
+            batch_y.shape = data_y[index+seq_len-label_len : index+seq_len+pred_len]
+            true.shape    = data_y[index+seq_len : index+seq_len+pred_len]
+            batch_x[-args.label_len:] is equal to batch_y[:args.label_len]
+        """
+        pred, true = _process_one_batch(
+            net, 
+            pred_data, 
+            batch_x, 
+            batch_y, 
+            batch_x_mark, 
+            batch_y_mark
+        )
+        preds.append(pred.detach().cpu().numpy())
+        trues.append(true.detach().cpu().numpy())
+
+    preds = np.array(preds)
+    trues = np.array(trues)
+
+    preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
+    trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
+    
+    ### save result ###
+    np.save('real_prediction.npy', preds)
+    
+    ### visualize ###
+    # draw OT prediction
+    plt.figure()
+    plt.plot(trues[0,:,-1], label='GroundTruth')
+    plt.plot(preds[0,:,-1], label='Prediction')
+    plt.legend()
+    plt.savefig("vis_{}_{}_OT.png".format(args.model, args.data))
+    # draw HUFL prediction
+    plt.figure()
+    plt.plot(trues[0,:,0], label='GroundTruth')
+    plt.plot(preds[0,:,0], label='Prediction')
+    plt.legend()
+    plt.savefig("vis_{}_{}_HUFL.png".format(args.model, args.data))
+
+    logger.info('Script finished successfully.')
+
+
+def main():
+    # update parameters
+    args.embed = 'timeF'
+    args.features = 'M'
+    args.target = 'OT'
+    args.inverse = False
+    args.cols = None
+    args.num_workers = 0
+    args.padding = 0
+    args.use_amp = False
+    args.output_attention = False
+    args.seq_len = 96
+    args.label_len = 48
+    args.pred_len = 24
+    if args.data == 'ETTm1':
+        args.data_path='ETTm1.csv'
+        args.freq='t'
+        args.detail_freq = '15t'
+    elif args.data == 'ETTh1':
+        args.data_path = 'ETTh1.csv'
+        args.freq = 'h'
+        args.detail_freq = 'h'
+
+    # choose model
+    weight_path, model_path = None, None
+    if args.model == 'informer':
+        if args.data == 'ETTh1':
+            weight_path = INFORMER_ETTH1_WEIGHT_PATH
+            model_path  = INFORMER_ETTH1_MODEL_PATH           
+        elif args.data == 'ETTm1':
+            weight_path = INFORMER_ETTM1_WEIGHT_PATH
+            model_path  = INFORMER_ETTM1_MODEL_PATH
+    elif args.model == 'informerstack':
+        if args.data == 'ETTh1':
+            weight_path = INFORMERSTACK_ETTH1_WEIGHT_PATH
+            model_path  = INFORMERSTACK_ETTH1_MODEL_PATH
+        elif args.data == 'ETTm1':
+            weight_path = INFORMERSTACK_ETTM1_WEIGHT_PATH
+            model_path  = INFORMERSTACK_ETTM1_MODEL_PATH
+    if weight_path == None or model_path == None:
+        logger.info('Invalid model or data.')
+        exit()
+
+    # model files check and download
+    check_and_download_models(weight_path, model_path, REMOTE_PATH)
+
+    # net initialize
+    if not args.onnx:
+        net = ailia.Net(model_path, weight_path, env_id=-1)
+    else:
+        net = onnxruntime.InferenceSession(weight_path)
+    
+    # forecasting
+    time_series_forecasting(net)
+
+
+if __name__ == '__main__':
+    main()
---
+++ time_series_forecasting/informer2020/informer2020_utils.py
@@ -0,0 +1,284 @@
+import os
+import numpy as np
+import pandas as pd
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+# from sklearn.preprocessing import StandardScaler
+
+from typing import List
+
+from pandas.tseries import offsets
+from pandas.tseries.frequencies import to_offset
+
+class StandardScaler():
+    def __init__(self):
+        self.mean = 0.
+        self.std = 1.
+    
+    def fit(self, data):
+        self.mean = data.mean(0)
+        self.std = data.std(0)
+
+    def transform(self, data):
+        mean = torch.from_numpy(self.mean).type_as(data).to(data.device) if torch.is_tensor(data) else self.mean
+        std = torch.from_numpy(self.std).type_as(data).to(data.device) if torch.is_tensor(data) else self.std
+        return (data - mean) / std
+
+    def inverse_transform(self, data):
+        mean = torch.from_numpy(self.mean).type_as(data).to(data.device) if torch.is_tensor(data) else self.mean
+        std = torch.from_numpy(self.std).type_as(data).to(data.device) if torch.is_tensor(data) else self.std
+        if data.size(-1) != mean.size(-1):
+            mean = mean[-1:]
+            std = std[-1:]
+        return (data * std) + mean
+
+class Dataset_Pred(Dataset):
+    def __init__(self, root_path, flag='pred', size=None, 
+                 features='S', data_path='ETTh1.csv', 
+                 target='OT', scale=True, inverse=False, timeenc=0, freq='15min', cols=None):
+        # size [seq_len, label_len, pred_len]
+        # info
+        if size == None:
+            self.seq_len = 24*4*4
+            self.label_len = 24*4
+            self.pred_len = 24*4
+        else:
+            self.seq_len = size[0]
+            self.label_len = size[1]
+            self.pred_len = size[2]
+        # init
+        assert flag in ['pred']
+        
+        self.features = features
+        self.target = target
+        self.scale = scale
+        self.inverse = inverse
+        self.timeenc = timeenc
+        self.freq = freq
+        self.cols=cols
+        self.root_path = root_path
+        self.data_path = data_path
+        self.__read_data__()
+
+    def __read_data__(self):
+        self.scaler = StandardScaler()
+        df_raw = pd.read_csv(os.path.join(self.root_path,
+                                          self.data_path))
+        '''
+        df_raw.columns: ['date', ...(other features), target feature]
+        '''
+        if self.cols:
+            cols=self.cols.copy()
+            cols.remove(self.target)
+        else:
+            cols = list(df_raw.columns); cols.remove(self.target); cols.remove('date')
+        df_raw = df_raw[['date']+cols+[self.target]]
+        
+        # 通常
+        border1 = len(df_raw)-self.seq_len
+        border2 = len(df_raw)
+
+        # 検証用(1)
+        #print('TODO: 検証用')
+        #border1 = 0
+        #border2 = self.seq_len+self.label_len
+        
+        if self.features=='M' or self.features=='MS':
+            cols_data = df_raw.columns[1:]
+            df_data = df_raw[cols_data]
+        elif self.features=='S':
+            df_data = df_raw[[self.target]]
+
+        if self.scale:
+            self.scaler.fit(df_data.values)
+            data = self.scaler.transform(df_data.values)
+        else:
+            data = df_data.values
+            
+        tmp_stamp = df_raw[['date']][border1:border2]
+        tmp_stamp['date'] = pd.to_datetime(tmp_stamp.date)
+        pred_dates = pd.date_range(tmp_stamp.date.values[-1], periods=self.pred_len+1, freq=self.freq)
+        
+        df_stamp = pd.DataFrame(columns = ['date'])
+        df_stamp.date = list(tmp_stamp.date.values) + list(pred_dates[1:])
+        data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq[-1:])
+
+        self.data_x = data[border1:border2]
+        if self.inverse:
+            self.data_y = df_data.values[border1:border2]
+        else:
+            self.data_y = data[border1:border2]
+        self.data_stamp = data_stamp
+    
+    def __getitem__(self, index):
+        s_begin = index
+        s_end = s_begin + self.seq_len
+        r_begin = s_end - self.label_len
+        r_end = r_begin + self.label_len + self.pred_len
+
+        seq_x = self.data_x[s_begin:s_end]
+        if self.inverse:
+            seq_y = self.data_x[r_begin:r_begin+self.label_len]
+        else:
+            # TODO:
+            #seq_y = self.data_y[r_begin:r_end]
+            seq_y = self.data_y[r_begin:r_begin+self.label_len]
+        seq_x_mark = self.data_stamp[s_begin:s_end]
+        seq_y_mark = self.data_stamp[r_begin:r_end]
+
+        #print(seq_y.shape)
+
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
+    
+    def __len__(self):
+        return len(self.data_x) - self.seq_len + 1
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
+
+class TimeFeature:
+    def __init__(self):
+        pass
+
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        pass
+
+    def __repr__(self):
+        return self.__class__.__name__ + "()"
+
+class SecondOfMinute(TimeFeature):
+    """Minute of hour encoded as value between [-0.5, 0.5]"""
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return index.second / 59.0 - 0.5
+
+class MinuteOfHour(TimeFeature):
+    """Minute of hour encoded as value between [-0.5, 0.5]"""
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return index.minute / 59.0 - 0.5
+
+class HourOfDay(TimeFeature):
+    """Hour of day encoded as value between [-0.5, 0.5]"""
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return index.hour / 23.0 - 0.5
+
+class DayOfWeek(TimeFeature):
+    """Hour of day encoded as value between [-0.5, 0.5]"""
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return index.dayofweek / 6.0 - 0.5
+
+class DayOfMonth(TimeFeature):
+    """Day of month encoded as value between [-0.5, 0.5]"""
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return (index.day - 1) / 30.0 - 0.5
+
+class DayOfYear(TimeFeature):
+    """Day of year encoded as value between [-0.5, 0.5]"""
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return (index.dayofyear - 1) / 365.0 - 0.5
+
+class MonthOfYear(TimeFeature):
+    """Month of year encoded as value between [-0.5, 0.5]"""
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return (index.month - 1) / 11.0 - 0.5
+
+class WeekOfYear(TimeFeature):
+    """Week of year encoded as value between [-0.5, 0.5]"""
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return (index.week - 1) / 52.0 - 0.5
+
+def time_features_from_frequency_str(freq_str: str) -> List[TimeFeature]:
+    """
+    Returns a list of time features that will be appropriate for the given frequency string.
+    Parameters
+    ----------
+    freq_str
+        Frequency string of the form [multiple][granularity] such as "12H", "5min", "1D" etc.
+    """
+
+    features_by_offsets = {
+        offsets.YearEnd: [],
+        offsets.QuarterEnd: [MonthOfYear],
+        offsets.MonthEnd: [MonthOfYear],
+        offsets.Week: [DayOfMonth, WeekOfYear],
+        offsets.Day: [DayOfWeek, DayOfMonth, DayOfYear],
+        offsets.BusinessDay: [DayOfWeek, DayOfMonth, DayOfYear],
+        offsets.Hour: [HourOfDay, DayOfWeek, DayOfMonth, DayOfYear],
+        offsets.Minute: [
+            MinuteOfHour,
+            HourOfDay,
+            DayOfWeek,
+            DayOfMonth,
+            DayOfYear,
+        ],
+        offsets.Second: [
+            SecondOfMinute,
+            MinuteOfHour,
+            HourOfDay,
+            DayOfWeek,
+            DayOfMonth,
+            DayOfYear,
+        ],
+    }
+
+    offset = to_offset(freq_str)
+
+    for offset_type, feature_classes in features_by_offsets.items():
+        if isinstance(offset, offset_type):
+            return [cls() for cls in feature_classes]
+
+    supported_freq_msg = f"""
+    Unsupported frequency {freq_str}
+    The following frequencies are supported:
+        Y   - yearly
+            alias: A
+        M   - monthly
+        W   - weekly
+        D   - daily
+        B   - business days
+        H   - hourly
+        T   - minutely
+            alias: min
+        S   - secondly
+    """
+    raise RuntimeError(supported_freq_msg)
+
+def time_features(dates, timeenc=1, freq='h'):
+    """
+    > `time_features` takes in a `dates` dataframe with a 'dates' column and extracts the date down to `freq` where freq can be any of the following if `timeenc` is 0: 
+    > * m - [month]
+    > * w - [month]
+    > * d - [month, day, weekday]
+    > * b - [month, day, weekday]
+    > * h - [month, day, weekday, hour]
+    > * t - [month, day, weekday, hour, *minute]
+    > 
+    > If `timeenc` is 1, a similar, but different list of `freq` values are supported (all encoded between [-0.5 and 0.5]): 
+    > * Q - [month]
+    > * M - [month]
+    > * W - [Day of month, week of year]
+    > * D - [Day of week, day of month, day of year]
+    > * B - [Day of week, day of month, day of year]
+    > * H - [Hour of day, day of week, day of month, day of year]
+    > * T - [Minute of hour*, hour of day, day of week, day of month, day of year]
+    > * S - [Second of minute, minute of hour, hour of day, day of week, day of month, day of year]
+
+    *minute returns a number from 0-3 corresponding to the 15 minute period it falls into.
+    """
+    if timeenc==0:
+        dates['month'] = dates.date.apply(lambda row:row.month,1)
+        dates['day'] = dates.date.apply(lambda row:row.day,1)
+        dates['weekday'] = dates.date.apply(lambda row:row.weekday(),1)
+        dates['hour'] = dates.date.apply(lambda row:row.hour,1)
+        dates['minute'] = dates.date.apply(lambda row:row.minute,1)
+        dates['minute'] = dates.minute.map(lambda x:x//15)
+        freq_map = {
+            'y':[],'m':['month'],'w':['month'],'d':['month','day','weekday'],
+            'b':['month','day','weekday'],'h':['month','day','weekday','hour'],
+            't':['month','day','weekday','hour','minute'],
+        }
+        return dates[freq_map[freq.lower()]].values
+    if timeenc==1:
+        dates = pd.to_datetime(dates.date.values)
+        return np.vstack([feat(dates) for feat in time_features_from_frequency_str(freq)]).transpose(1,0)
+
