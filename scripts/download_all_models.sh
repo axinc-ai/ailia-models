2dbcb6a6ef8d579fa8759363eac558de29756ba4
yuki <a@a.a>
2023-03-20 23:24:39 +0900

    fix script and README

--- scripts/download_all_models.sh
+++ scripts/download_all_models.sh
@@ -258,6 +258,7 @@ cd ../../super_resolution/edsr; python3 edsr.py ${OPTION}
 cd ../../super_resolution/real-esrgan; python3 real-esrgan.py ${OPTION}
 cd ../../super_resolution/rcan-it; python3 rcan-it.py ${OPTION}
 cd ../../super_resolution/swinir; python3 swinir.py ${OPTION}
+cd ../../super_resolution/CodeFormer; python3 codeformer.py ${OPTION}
 cd ../../text_detection/craft_pytorch; python3 craft_pytorch.py ${OPTION}
 cd ../../text_detection/pixel_link; python3 pixel_link.py ${OPTION}
 cd ../../text_detection/east; python3 east.py ${OPTION}
