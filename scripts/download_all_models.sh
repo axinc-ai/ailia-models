2126776e9986797770bcf721357cf56e7f9689d5
ooe1123 <ooe1123@gmail.com>
2023-03-29 23:08:30 +0900

    Add model to list

--- scripts/download_all_models.sh
+++ scripts/download_all_models.sh
@@ -17,6 +17,7 @@ cd ../../audio_processing/auto_speech/; python3 auto_speech.py ${OPTION}
 cd ../../audio_processing/voicefilter/; python3 voicefilter.py ${OPTION}
 cd ../../audio_processing/whisper/; python3 whisper.py ${OPTION}
 cd ../../audio_processing/clap/; python3 clap.py ${OPTION}
+cd ../../audio_processing/wespeaker/; python3 wespeaker.py ${OPTION}
 cd ../../background_ramoval/deep-image-matting; python3 deep-image-matting.py ${OPTION}
 cd ../../background_ramoval/u2net; python3 u2net.py ${OPTION}
 cd ../../background_ramoval/u2net; python3 u2net.py -a small ${OPTION}
