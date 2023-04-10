f2a109e428c7590468459823eb191a4b8e2644cc
Kazuki Kyakuno <kyakuno@axell.co.jp>
2023-04-10 15:23:58 +0900

    Fix inference error

--- object_detection/yolov1-tiny/yolov1-tiny.py
+++ object_detection/yolov1-tiny/yolov1-tiny.py
@@ -144,7 +144,7 @@ def recognize_from_video():
 
         img = cv2.cvtColor(frame, cv2.COLOR_RGB2BGRA)
         detector.compute(img, THRESHOLD, IOU)
-        res_img = plot_results(detector, frame, VOC_CATEGORY, False)
+        res_img = plot_results(detector, frame, VOC_CATEGORY, logging = False)
         cv2.imshow('frame', res_img)
         frame_shown = True
 
