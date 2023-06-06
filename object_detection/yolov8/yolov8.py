36b5a96f403f0abc24bab4937a05792785b84d71
Kazuki Kyakuno <kyakuno@axell.co.jp>
2023-06-06 10:22:28 +0900

    Rename to argutil

--- object_detection/yolov8/yolov8.py
+++ object_detection/yolov8/yolov8.py
@@ -8,7 +8,7 @@ import ailia
 
 # import original modules
 sys.path.append('../../util')
-from utils import get_base_parser, update_parser, get_savepath  # noqa
+from arg_utils import get_base_parser, update_parser, get_savepath  # noqa
 from model_utils import check_and_download_models  # noqa
 from image_utils import normalize_image  # noqa
 from detector_utils import plot_results, write_predictions, load_image  # noqa
