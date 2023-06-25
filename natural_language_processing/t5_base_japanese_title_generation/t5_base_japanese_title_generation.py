474c8b51dff2b004b79debc33e4925200c0e242d
takumi2786 <revolver12345678@gmail.conm>
2023-06-25 13:43:03 +0900

    fix: not use torch but use numpy

--- natural_language_processing/t5_base_japanese_title_generation/t5_base_japanese_title_generation.py
+++ natural_language_processing/t5_base_japanese_title_generation/t5_base_japanese_title_generation.py
@@ -4,8 +4,7 @@ import sys
 import unicodedata
 
 from transformers import T5Tokenizer
-import torch
-import torch.nn.functional as F
+import numpy as np
 from tqdm import trange
 sys.path.append('../../util')
 from arg_utils import get_base_parser, update_parser, get_savepath  # noqa: E402
@@ -29,37 +28,42 @@ DEFAULT_INPUT_PATH = "./input.txt"
 DEFAULT_OUTPUT_PATH = "./output.txt"
 
 
+def softmax_np(x: np.ndarray):
+    e_x = np.exp(x - np.max(x))  # subtract max to stabilize
+    return e_x / e_x.sum(axis=0)
+
 """
 top k top p filtering algorithm
 """
-def top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-float("Inf")):
-    """ Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
-        Function created by Thomas Wolf of the huggingface team
-        Args:
-            logits: logits distribution shape (vocabulary size)
-            top_k > 0: keep only top k tokens with highest probability (top-k filtering).
-            top_p > 0.0: keep the top tokens with cumulative probability >= top_p (nucleus filtering).
-                Nucleus filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751)
-        From: https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
+def top_k_top_p_filtering(logits: np.ndarray, top_k:int=0, top_p:float=0.0, filter_value:float=-float("Inf")):
+    """
+    Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
+    Args:
+        logits: logits distribution shape (vocabulary size)
+        top_k > 0: keep only top k tokens with highest probability (top-k filtering).
+        top_p > 0.0: keep the top tokens with cumulative probability >= top_p (nucleus filtering).
+            Nucleus filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751)
+    From: https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
     """
     assert (
-        logits.dim() == 1
+        logits.ndim == 1
     )  # batch size 1 for now - could be updated for more but the code would be less clear
-    top_k = min(top_k, logits.size(-1))  # Safety check
+    top_k = min(top_k, logits.shape[-1])  # Safety check
     if top_k > 0:
         # Remove all tokens with a probability less than the last token of the top-k
-        indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1, None]
+        indices_to_remove = logits < np.partition(logits, -top_k)[-top_k]
         logits[indices_to_remove] = filter_value
 
     if top_p > 0.0:
-        sorted_logits, sorted_indices = torch.sort(logits, descending=True)
-        cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+        sorted_indices = np.argsort(logits)[::-1]
+        sorted_logits = logits[sorted_indices]
+        cumulative_probs = np.cumsum(np.exp(sorted_logits) / np.sum(np.exp(sorted_logits)))
 
         # Remove tokens with cumulative probability above the threshold
         sorted_indices_to_remove = cumulative_probs > top_p
         # Shift the indices to the right to keep also the first token above the threshold
-        sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
-        sorted_indices_to_remove[..., 0] = 0
+        sorted_indices_to_remove[1:] = sorted_indices_to_remove[:-1]
+        sorted_indices_to_remove[0] = 0
 
         indices_to_remove = sorted_indices[sorted_indices_to_remove]
         logits[indices_to_remove] = filter_value
@@ -68,7 +72,7 @@ def top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-float("Inf")
 """
 model wrapper
 """
-class T5Model(torch.nn.Module):
+class T5Model:
     """
     This class is based on `GenerativeT5` from `models` in `onnxt5`.
     Modified by Takumi Ibayashi.
@@ -79,7 +83,7 @@ class T5Model(torch.nn.Module):
         self.decoder_with_lm_head = decoder_with_lm_head
         self.tokenizer = tokenizer
 
-    def forward(
+    def estimate(
         self, prompt: str, max_length: int, temperature:float=1.0, repetition_penalty:float=1.0, top_k:int=50, top_p:int=0, max_context_length: int=512, 
     ):
         """
@@ -101,58 +105,57 @@ class T5Model(torch.nn.Module):
             max_context_length (int, optional): The maximum length of the context to be 
                 used in generation. Defaults to 512.
         """
-        with torch.no_grad():
-            new_tokens = torch.tensor(())
-            new_logits = []
+        new_tokens = np.array([], dtype=np.float32)
+        new_logits = []
 
-            # generate tokens with tokenizer
-            token = torch.tensor(self.tokenizer(prompt)['input_ids'])[:max_context_length - 1].unsqueeze(0)
+        # generate tokens with tokenizer
+        token = np.expand_dims(np.array(self.tokenizer(prompt)['input_ids'])[:max_context_length - 1], axis=0)
 
-            # encode tokens
+        # encode tokens
+        if args.onnx:
+            encoder_outputs_prompt = self.encoder.run(None, {"input_ids": token})[0]
+        else:
+            encoder_outputs_prompt = self.encoder.run({"input_ids": token})[0]
+
+        # reset token
+        token = np.zeros((1,1), dtype=np.int64)
+        for _ in trange(max_length):
+            # decode tokens
             if args.onnx:
-                encoder_outputs_prompt = self.encoder.run(None, {"input_ids": token.cpu().numpy()})[0]
+                outputs = np.array(
+                    self.decoder_with_lm_head.run(
+                        None,
+                        {"input_ids": token,"encoder_hidden_states": encoder_outputs_prompt},
+                    )[0][0]
+                )
             else:
-                encoder_outputs_prompt = self.encoder.run({"input_ids": token.cpu().numpy()})[0]
+                outputs = np.array(
+                    self.decoder_with_lm_head.run(
+                        {"input_ids": token,"encoder_hidden_states": encoder_outputs_prompt},
+                    )[0][0]
+                )
+            next_token_logits = outputs[-1, :] / (temperature if temperature > 0 else 1.0)
 
-            # reset token
-            token = torch.zeros((1,1), dtype=torch.long)
-            for _ in trange(max_length):
-                # decode tokens
-                if args.onnx:
-                    outputs = torch.tensor(
-                        self.decoder_with_lm_head.run(
-                            None,
-                            {"input_ids": token.cpu().numpy(),"encoder_hidden_states": encoder_outputs_prompt},
-                        )[0][0]
-                    )
-                else:
-                    outputs = torch.tensor(
-                        self.decoder_with_lm_head.run(
-                            {"input_ids": token.cpu().numpy(),"encoder_hidden_states": encoder_outputs_prompt},
-                        )[0][0]
-                    )
-                next_token_logits = outputs[-1, :] / (temperature if temperature > 0 else 1.0)
+            # `1` means end of sentence. (EOS token)
+            if int(next_token_logits.argmax()) == 1:
+                break
 
-                # `1` means end of sentence. (EOS token)
-                if int(next_token_logits.argmax()) == 1:
-                    break
+            new_logits.append(next_token_logits)
+            for _ in set(token.reshape(-1).tolist()):
+                next_token_logits[_] /= repetition_penalty
 
-                new_logits.append(next_token_logits)
-                for _ in set(token.view(-1).tolist()):
-                    next_token_logits[_] /= repetition_penalty
-
-                # select next token
-                if temperature == 0:
-                    # greedy sampling: this methods always choose the most probable token.
-                    next_token = torch.argmax(next_token_logits).unsqueeze(0)
-                else:
-                    # Top-k and top-p filtering: this methods enhance text diversity and creativity.
-                    filtered_logits = top_k_top_p_filtering(next_token_logits, top_k=top_k, top_p=top_p)
-                    next_token = torch.multinomial(F.softmax(filtered_logits, dim=-1), num_samples=1)
-
-                token = torch.cat((token, next_token.unsqueeze(0)), dim=1)
-                new_tokens = torch.cat((new_tokens, next_token), 0)
-            return self.tokenizer.decode(new_tokens), new_logits
+            # select next token
+            if temperature == 0:
+                # greedy sampling: this methods always choose the most probable token.
+                next_token = np.expand_dims(np.argmax(next_token_logits), axis=0)
+            else:
+                # Top-k and top-p filtering: this methods enhance text diversity and creativity.
+                filtered_logits = top_k_top_p_filtering(next_token_logits, top_k=top_k, top_p=top_p)
+                probs = softmax_np(filtered_logits)
+                next_token = np.expand_dims(np.random.choice(np.arange(len(probs)), p=probs), axis=0)
+            token = np.concatenate((token, np.expand_dims(next_token, axis=0)), axis=1)
+            new_tokens = np.concatenate((new_tokens, next_token), axis=0)
+        return self.tokenizer.decode(new_tokens), new_logits
 
 """
 pre process functions
@@ -248,7 +251,6 @@ def main(args):
         model = T5Model(encoder_sess, decoder_sess, tokenizer)
     else:
         import ailia
-        import torch
         encoder_sess = ailia.Net(ENCODER_PROTOTXT_PATH, ENCODER_ONNX_PATH)
         decoder_sess = ailia.Net(DECODER_PROTOTXT_PATH, DECODER_ONNX_PATH)
         model = T5Model(encoder_sess, decoder_sess, tokenizer)
@@ -262,7 +264,7 @@ def main(args):
         body_preprocessed = preprocess_body(body)
 
         # execute prediction
-        most_plausible_title, _ = model(body_preprocessed, 21, temperature=1.0, top_k=50, top_p=0.3)
+        most_plausible_title, _ = model.estimate(body_preprocessed, 21, temperature=1.0, top_k=50, top_p=0.3)
         logger.info("title: %s", most_plausible_title)
         save_path = get_savepath(args.savepath, input_path)
         with open(save_path, "a") as fo:
