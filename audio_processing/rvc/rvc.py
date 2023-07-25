aa239f89c5b1a53857cdd7d1253ea158b0149ade
ooe1123 <ooe1123@gmail.com>
2023-07-26 01:06:03 +0900

    Implement RVC

--- audio_processing/rvc/rvc.py
+++ audio_processing/rvc/rvc.py
@@ -4,9 +4,11 @@ from logging import getLogger
 
 import numpy as np
 import scipy.signal as signal
-import ffmpeg
 import torch
 import torch.nn.functional as F
+import ffmpeg
+import librosa
+import soundfile as sf
 
 import ailia
 
@@ -38,6 +40,22 @@ SAVE_TEXT_PATH = 'output.txt'
 
 parser = get_base_parser(
     'Retrieval-based-Voice-Conversion', WAV_PATH, SAVE_TEXT_PATH, input_ftype='audio'
+)
+parser.add_argument(
+    '--sid', type=int, default=0,
+    help='Select Speaker/Singer ID',
+)
+parser.add_argument(
+    '--index_rate', type=float, default=0.75,
+    help='Search feature ratio. (controls accent strength, too high has artifacting)',
+)
+parser.add_argument(
+    '--resample_sr', type=int, default=0,
+    help='Resample the output audio. Set to 0 for no resampling.',
+)
+parser.add_argument(
+    '--rms_mix_rate', type=float, default=0.25,
+    help='Adjust the volume envelope scaling.',
 )
 parser.add_argument(
     '-V', action='store_true',
@@ -84,6 +102,28 @@ def load_audio(file, sr):
     return np.frombuffer(out, np.float32).flatten()
 
 
+def change_rms(data1, sr1, data2, sr2, rate):  # 1是输入音频，2是输出音频,rate是2的占比
+    rms1 = librosa.feature.rms(
+        y=data1, frame_length=sr1 // 2 * 2, hop_length=sr1 // 2
+    )  # 每半秒一个点
+    rms2 = librosa.feature.rms(y=data2, frame_length=sr2 // 2 * 2, hop_length=sr2 // 2)
+    rms1 = torch.from_numpy(rms1)
+    rms1 = F.interpolate(
+        rms1.unsqueeze(0), size=data2.shape[0], mode="linear"
+    ).squeeze()
+    rms2 = torch.from_numpy(rms2)
+    rms2 = F.interpolate(
+        rms2.unsqueeze(0), size=data2.shape[0], mode="linear"
+    ).squeeze()
+    rms2 = torch.max(rms2, torch.zeros_like(rms2) + 1e-6)
+    data2 *= (
+            torch.pow(rms1, torch.tensor(1 - rate))
+            * torch.pow(rms2, torch.tensor(rate - 1))
+    ).numpy()
+
+    return data2
+
+
 # ======================
 # Main functions
 # ======================
@@ -124,7 +164,7 @@ def vc(
 
     feats = torch.from_numpy(feats)
     feats = F.interpolate(feats.permute(0, 2, 1), scale_factor=2).permute(0, 2, 1)
-    feats = feats.numpy().astype(np.float16)
+    feats = feats.numpy()
 
     p_len = audio0.shape[0] // vc_param.window
     if feats.shape[1] < p_len:
@@ -132,11 +172,12 @@ def vc(
     p_len = np.array([p_len], dtype=int)
 
     # feedforward
+    rnd = np.random.randn(1, 192, p_len[0]).astype(np.float32) * 0.66666  # 噪声（加入随机因子）
     if not args.onnx:
-        output = net_g.predict([feats, p_len, sid])
+        output = net_g.predict([feats, p_len, sid, rnd])
     else:
         output = net_g.run(None, {
-            'phone': feats, 'phone_lengths': p_len, 'sid': sid
+            'phone': feats, 'phone_lengths': p_len, 'ds': sid, 'rnd': rnd
         })
     audio1 = output[0][0, 0]
 
@@ -146,14 +187,16 @@ def vc(
 bh, ah = signal.butter(N=5, Wn=48, btype="high", fs=16000)
 
 
-def predict(audio, models, pitch=None, pitchf=None):
+def predict(audio, models, tgt_sr):
     audio_max = np.abs(audio).max() / 0.95
     if audio_max > 1:
         audio /= audio_max
 
-    # depend on voice model
-    if_f0 = 0
-    tgt_sr = 40000
+    sid = args.sid
+    index_rate = args.index_rate
+    resample_sr = args.resample_sr
+    rms_mix_rate = args.rms_mix_rate
+
     vc_param = VCParam(tgt_sr)
 
     index = big_npy = None
@@ -178,11 +221,6 @@ def predict(audio, models, pitch=None, pitchf=None):
     audio_opt = []
     t = None
     audio_pad = np.pad(audio, (vc_param.t_pad, vc_param.t_pad), mode="reflect")
-    p_len = audio_pad.shape[0] // vc_param.window
-    inp_f0 = None
-
-    sid = 0
-    index_rate = 0.75
 
     sid = np.array([sid], dtype=int)
     for t in opt_ts:
@@ -210,17 +248,16 @@ def predict(audio, models, pitch=None, pitchf=None):
         index_rate,
     )
     audio_opt.append(audio1[vc_param.t_pad_tgt: -vc_param.t_pad_tgt])
-
     audio_opt = np.concatenate(audio_opt)
+    audio_opt = audio_opt.astype(np.float32)
 
-    rms_mix_rate = 0.25
-    resample_sr = 0
-    if rms_mix_rate != 1:
+    if rms_mix_rate < 1:
         audio_opt = change_rms(audio, 16000, audio_opt, tgt_sr, rms_mix_rate)
-    if resample_sr >= 16000 and tgt_sr != resample_sr:
+    if 16000 <= resample_sr != tgt_sr:
         audio_opt = librosa.resample(
             audio_opt, orig_sr=tgt_sr, target_sr=resample_sr
         )
+        tgt_sr = resample_sr
 
     audio_max = np.abs(audio_opt).max() / 0.99
     max_int16 = 32768
@@ -228,14 +265,13 @@ def predict(audio, models, pitch=None, pitchf=None):
         max_int16 /= audio_max
     audio_opt = (audio_opt * max_int16).astype(np.int16)
 
-    del pitch, pitchf, sid
-    if torch.cuda.is_available():
-        torch.cuda.empty_cache()
-
-    return audio_opt
+    return audio_opt, tgt_sr
 
 
 def recognize_from_audio(models):
+    # Depend on voice model
+    tgt_sr = 40000
+
     # input audio loop
     for audio_path in args.input:
         logger.info(audio_path)
@@ -248,12 +284,17 @@ def recognize_from_audio(models):
         if args.benchmark:
             logger.info('BENCHMARK mode')
             start = int(round(time.time() * 1000))
-            output = predict(audio, models)
+            output, sr = predict(audio, models, tgt_sr)
             end = int(round(time.time() * 1000))
             estimation_time = (end - start)
             logger.info(f'\ttotal processing time {estimation_time} ms')
         else:
-            output = predict(audio, models)
+            output, sr = predict(audio, models, tgt_sr)
+
+        # save result
+        savepath = get_savepath(args.savepath, audio_path, ext='.wav')
+        logger.info(f'saved at : {savepath}')
+        sf.write(savepath, output, sr)
 
     logger.info('Script finished successfully.')
 
