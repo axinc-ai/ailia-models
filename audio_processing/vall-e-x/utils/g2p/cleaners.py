a73e2e779814ae65aa43918c069b0de3f076ef83
Kazuki Kyakuno <kyakuno@axell.co.jp>
2023-09-13 21:38:45 +0900

    Refactoring

--- audio_processing/vall-e-x/utils/g2p/cleaners.py
+++ audio_processing/vall-e-x/utils/g2p/cleaners.py
@@ -1,23 +1,8 @@
 import re
-from utils.g2p.japanese import japanese_to_romaji_with_accent, japanese_to_ipa, japanese_to_ipa2, japanese_to_ipa3
+from utils.g2p.japanese import japanese_to_ipa2
 from utils.g2p.mandarin import number_to_chinese, chinese_to_bopomofo, latin_to_bopomofo, chinese_to_romaji, chinese_to_lazy_ipa, chinese_to_ipa, chinese_to_ipa2
 from utils.g2p.english import english_to_lazy_ipa, english_to_ipa2, english_to_lazy_ipa2
 patterns = [r'\[EN\](.*?)\[EN\]', r'\[ZH\](.*?)\[ZH\]', r'\[JA\](.*?)\[JA\]']
-def japanese_cleaners(text):
-    text = japanese_to_romaji_with_accent(text)
-    text = re.sub(r'([A-Za-z])$', r'\1.', text)
-    return text
-
-def japanese_cleaners2(text):
-    return japanese_cleaners(text).replace('ts', 'ʦ').replace('...', '…')
-
-def chinese_cleaners(text):
-    '''Pipeline for Chinese text'''
-    text = number_to_chinese(text)
-    text = chinese_to_bopomofo(text)
-    text = latin_to_bopomofo(text)
-    text = re.sub(r'([ˉˊˇˋ˙])$', r'\1。', text)
-    return text
 
 def cje_cleaners(text):
     matches = []
--- audio_processing/vall-e-x/utils/g2p/japanese.py
+++ audio_processing/vall-e-x/utils/g2p/japanese.py
@@ -1,8 +1,6 @@
 import re
 from unidecode import unidecode
 
-
-
 # Regular expression matching Japanese without punctuation marks:
 _japanese_characters = re.compile(
     r'[A-Za-z\d\u3005\u3040-\u30ff\u4e00-\u9fff\uff11-\uff19\uff21-\uff3a\uff41-\uff5a\uff66-\uff9d]')
@@ -14,22 +12,6 @@ _japanese_marks = re.compile(
 # List of (symbol, Japanese) pairs for marks:
 _symbols_to_japanese = [(re.compile('%s' % x[0]), x[1]) for x in [
     ('％', 'パーセント')
-]]
-
-# List of (romaji, ipa) pairs for marks:
-_romaji_to_ipa = [(re.compile('%s' % x[0]), x[1]) for x in [
-    ('ts', 'ʦ'),
-    ('u', 'ɯ'),
-    ('j', 'ʥ'),
-    ('y', 'j'),
-    ('ni', 'n^i'),
-    ('nj', 'n^'),
-    ('hi', 'çi'),
-    ('hj', 'ç'),
-    ('f', 'ɸ'),
-    ('I', 'i*'),
-    ('U', 'ɯ*'),
-    ('r', 'ɾ')
 ]]
 
 # List of (romaji, ipa2) pairs for marks:
@@ -125,30 +107,12 @@ def get_real_hatsuon(text):
     return text
 
 
-def japanese_to_ipa(text):
-    text = japanese_to_romaji_with_accent(text).replace('...', '…')
-    text = re.sub(
-        r'([aiueo])\1+', lambda x: x.group(0)[0]+'ː'*(len(x.group(0))-1), text)
-    text = get_real_sokuon(text)
-    text = get_real_hatsuon(text)
-    for regex, replacement in _romaji_to_ipa:
-        text = re.sub(regex, replacement, text)
-    return text
-
-
 def japanese_to_ipa2(text):
+    print("G2P Input", text)
     text = japanese_to_romaji_with_accent(text).replace('...', '…')
     text = get_real_sokuon(text)
     text = get_real_hatsuon(text)
     for regex, replacement in _romaji_to_ipa2:
         text = re.sub(regex, replacement, text)
-    return text
-
-
-def japanese_to_ipa3(text):
-    text = japanese_to_ipa2(text).replace('n^', 'ȵ').replace(
-        'ʃ', 'ɕ').replace('*', '\u0325').replace('#', '\u031a')
-    text = re.sub(
-        r'([aiɯeo])\1+', lambda x: x.group(0)[0]+'ː'*(len(x.group(0))-1), text)
-    text = re.sub(r'((?:^|\s)(?:ts|tɕ|[kpt]))', r'\1ʰ', text)
+    print("G2P Output", text)
     return text
