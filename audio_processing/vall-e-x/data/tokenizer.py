766056d6eea9f173e0e284b588219fe94d43ad89
Kazuki Kyakuno <kyakuno@axell.co.jp>
2023-09-15 13:22:02 +0900

    Remove torch audio

--- audio_processing/vall-e-x/data/tokenizer.py
+++ audio_processing/vall-e-x/data/tokenizer.py
@@ -17,54 +17,37 @@ from typing import Any
 
 import ailia
 import numpy as np
-import torch
-import torchaudio
-
+import librosa
 
 class AudioTokenizer:
     """EnCodec audio."""
 
     def __init__(
-        self
+        self,
+        net
     ) -> None:
         self.sample_rate = 24000
         self.channels = 1
-        self.vnet = ailia.Net(weight="onnx/encodec.onnx", env_id = 1, memory_mode = 11)
+        self.net = net
         
 
-    def encode(self, wav: torch.Tensor) -> torch.Tensor:
-        #print("Impot encodec from onnx")
-        encoded_frames = self.vnet.run([wav.numpy()])[0]
-        encoded_frames = torch.from_numpy(encoded_frames)
+    def encode(self, wav):
+        encoded_frames = self.net.run([wav])[0]
         encoded_frames = [[encoded_frames]]
-
         return encoded_frames
 
 
 
 def tokenize_audio(tokenizer: AudioTokenizer, audio):
-    # Load and pre-process the audio waveform
-    #if isinstance(audio, str):
-    #    wav, sr = torchaudio.load(audio)
-    #else:
-    #    wav, sr = audio
-    #wav = convert_audio(wav, sr, tokenizer.sample_rate, tokenizer.channels)
-    #print(wav.shape)
-    #wav = wav.unsqueeze(0)
-    #print(wav.shape)
-
-    import librosa
     wav, sr = librosa.load(audio, sr=tokenizer.sample_rate, mono=True)
-    wav = torch.tensor(wav) # range = 0 - 1
-    wav = wav.unsqueeze(0)
-    wav = wav.unsqueeze(0)
+    wav = np.array(wav)
+    wav = np.expand_dims(wav, axis = 0)
+    wav = np.expand_dims(wav, axis = 0)
 
-    if wav.size(-1) / sr > 15:
-        raise ValueError(f"Prompt too long, expect length below 15 seconds, got {wav.size(-1) / sr} seconds.")
+    if wav.shape[-1] / sr > 15:
+        raise ValueError(f"Prompt too long, expect length below 15 seconds, got {wav.shape[-1] / sr} seconds.")
 
     # Extract discrete codes from EnCodec
-    with torch.no_grad():
-        encoded_frames = tokenizer.encode(wav)
-
+    encoded_frames = tokenizer.encode(wav)
     return encoded_frames
 
--- audio_processing/vall-e-x/utils/prompt_making.py
+++ audio_processing/vall-e-x/utils/prompt_making.py
@@ -1,6 +1,5 @@
 import os
 import torch
-import torchaudio
 import logging
 import langid
 langid.set_languages(['en', 'zh', 'ja'])
@@ -18,21 +17,13 @@ from macros import *
 text_tokenizer = PhonemeBpeTokenizer(tokenizer_path="./utils/g2p/bpe_69.json")
 text_collater = get_text_token_collater()
 
-codec = AudioTokenizer()
-
 def make_prompt(name, audio_prompt_path, transcript=None, models=None):
-    #global model, text_collater, text_tokenizer, codec
-    #wav_pr, sr = torchaudio.load(audio_prompt_path)
-    # check length
-    #if wav_pr.size(-1) / sr > 15:
-    #    raise ValueError(f"Prompt too long, expect length below 15 seconds, got {wav_pr / sr} seconds.")
-    #if wav_pr.size(0) == 2:
-    #    wav_pr = wav_pr.mean(0, keepdim=True)
+    codec = AudioTokenizer(models["encodec.onnx"])
     text_pr, lang_pr = make_transcript(transcript)
 
     # tokenize audio
-    encoded_frames = tokenize_audio(codec, audio_prompt_path)#(wav_pr, sr))
-    audio_tokens = encoded_frames[0][0].transpose(2, 1).cpu().numpy()
+    encoded_frames = tokenize_audio(codec, audio_prompt_path)
+    audio_tokens = np.transpose(encoded_frames[0][0], axes = (0, 2, 1))
 
     # tokenize text
     phonemes, langs = text_tokenizer.tokenize(text=f"{text_pr}".strip())
