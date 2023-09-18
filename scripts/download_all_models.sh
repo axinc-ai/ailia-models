aadbe8513dc42c4b842e42e265712b553533b134
Kazuki Kyakuno <kyakuno@axell.co.jp>
2023-09-18 20:22:28 +0900

    Add model to list

--- scripts/download_all_models.sh
+++ scripts/download_all_models.sh
@@ -22,6 +22,7 @@ cd ../../audio_processing/tacotron2/; python3 tacotron2.py ${OPTION}
 cd ../../audio_processing/silero-vad/; python3 silero-vad.py ${OPTION}
 cd ../../audio_processing/rvc/; python3 rvc.py ${OPTION}
 cd ../../audio_processing/crepe/; python3 crepe.py ${OPTION}
+cd ../../audio_processing/vall-e-x/; python3 vall-e-x.py ${OPTION}
 cd ../../background_ramoval/deep-image-matting; python3 deep-image-matting.py ${OPTION}
 cd ../../background_ramoval/u2net; python3 u2net.py ${OPTION}
 cd ../../background_ramoval/u2net; python3 u2net.py -a small ${OPTION}
