d71abb95a59f43139824b9876d3f590d9ad2006e
Kazuki Kyakuno <kyakuno@axell.co.jp>
2023-10-29 14:23:28 +0900

    Fix util path

--- anomaly_detection/patchcore/patchcore.py
+++ anomaly_detection/patchcore/patchcore.py
@@ -20,7 +20,7 @@ from logging import getLogger  # noqa: E402
 import webcamera_utils  # noqa: E402
 from detector_utils import load_image  # noqa: E402
 from model_utils import check_and_download_models  # noqa: E402
-from utils import get_base_parser, get_savepath, update_parser  # noqa: E402
+from arg_utils import get_base_parser, get_savepath, update_parser  # noqa: E402
 
 from patchcore_utils import *
 
