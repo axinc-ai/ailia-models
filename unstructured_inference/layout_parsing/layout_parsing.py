5b542a1e86b0435e772274550ebbe98ac12f7d15
Daisuke AKAGAWA <daisuke1024akagawa@gmail.com>
2024-01-21 17:00:20 +0900

    fix how to generate result image path

--- unstructured_inference/layout_parsing/layout_parsing.py
+++ unstructured_inference/layout_parsing/layout_parsing.py
@@ -1,7 +1,9 @@
+import os
 import sys
 
 import ailia
 import cv2
+from numpy import fix
 
 sys.path.append("../../util")
 from logging import getLogger  # noqa: E402
@@ -29,7 +31,7 @@ parser = get_base_parser('Layout parsing', PDF_PATH, SAVE_IMAGE_PATH)
 parser.add_argument(
     "-i", "--input",
     type=str,
-    help="input PDF file name",
+    help="input file name",
     default=PDF_PATH,
 )
 parser.add_argument(
@@ -50,10 +52,15 @@ parser.add_argument(
     default=NMS_THR, type=float,
     help='The detection iou for yolo. (default: '+str(NMS_THR)+')'
 )
+parser.add_argument(
+    "-fi", "--from_image",
+    action="store_true",
+    help="set this option when target file is image",
+)
 args = update_parser(parser)
 
 
-def infer(detector: ailia.Detector):
+def infer_from_pdf(detector: ailia.Detector):
     image_names = pdf_to_images(args.input[0], dpi=args.dpi, output_folder=args.savepath)
 
     for image_name in image_names:
@@ -95,6 +102,49 @@ def infer(detector: ailia.Detector):
         cv2.imwrite(image_name.replace(".ppm", "_parsed.jpg"), res_img)
 
 
+def infer_from_image(detector: ailia.Detector):
+    image_filename = args.input[0]
+    img_orig = cv2.imread(image_filename)
+    img_processed, ratio = preprocess(img_orig, INPUT_SHAPE)
+
+    def compute():
+        if args.detector:
+            detector.compute(img_orig, args.threshold, args.iou)
+            return None
+        else:
+            return detector.run(img_processed)
+
+    logger.info("Start: inference...")
+    if args.benchmark:
+        logger.info("BENCHMARK mode")
+        total_time = 0
+        for i in range(args.benchmark_count):
+            start = int(rount(time.time() * 1000))
+            output = compute()
+            end = int(rount(time.time() * 1000))
+            if i != 0:
+                total_time = total_time + (end - start)
+            logger.info(f"\tailia processing time {end - start} ms")
+        
+        logger.info(f"\taverage time {total_time / (args.benchmark_count - 1)} ms")
+    else:
+        output = compute()
+    
+    if args.detector:
+        res_img = plot_results(detector, img_orig, YOLOX_LABEL_MAP)
+        detector_object = detector
+    else:
+        predictions = postprocess(output[0]. INPUT_SHAPE)[0]
+        detect_object = predictions_to_object(predictions, img_orig, ratio, args.iou, args.threshold)
+        detect_object = reverse_letterbox(detect_object, img_orig, (img_orig.shape[0], img_orig.shape[1]))
+        res_img = plot_results(detect_object, img_orig, YOLOX_LABEL_MAP)
+
+    extension = os.path.splitext(image_filename)[1]
+    output_filename = image_filename.replace(extension, "_parsed.jpg")
+    cv2.imwrite(output_filename, res_img)
+
+    
+
 if __name__ == "__main__":
     # weight_path, model_path, params = get_params()
     # check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
@@ -109,4 +159,7 @@ if __name__ == "__main__":
         algorithm=ailia.DETECTOR_ALGORITHM_YOLOX,
         env_id=env_id,
     )
-    infer(detector)
+    if args.from_image:
+        infer_from_image(detector)
+    else:
+        infer_from_pdf(detector)
