391b9d08755651bca443ba8124879619fe8f7146
ooe1123 <ooe1123@gmail.com>
2024-01-30 22:44:35 +0900

    fix

--- diffusion/marigold/marigold.py
+++ diffusion/marigold/marigold.py
@@ -18,7 +18,7 @@ import ailia
 # import original modules
 sys.path.append('../../util')
 from arg_utils import get_base_parser, update_parser, get_savepath  # noqa
-from model_utils import check_and_download_models  # noqa
+from model_utils import check_and_download_models, check_and_download_file  # noqa
 from image_utils import normalize_image  # noqa
 from detector_utils import load_image  # noqa
 from webcamera_utils import get_capture, get_writer  # noqa
