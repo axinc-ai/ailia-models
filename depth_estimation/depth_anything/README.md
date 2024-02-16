c01b401622c19163e58512c0d23e4273febc0604
kobakos <koki-kobayashi@outlook.jp>
2024-02-16 15:42:55 +0900

    Initial commit

---
+++ depth_estimation/depth_anything/README.md
@@ -0,0 +1,63 @@
+# Depth Anything: Unleashing the Power of Large-Scale Unlabeled Data
+
+## Input
+
+* **Image or Video**
+
+The script will perform a monocular depth estimation on the input media.
+
+## Output
+
+* **Depth image**
+
+Estimated relative depth, scaled to fit in the 0~255 range.
+
+Saves to ```./output.wav``` by default but it can be specified with the ```--path``` option 
+
+## Usage
+Internet connection is required when running the script for the first time,
+as it will download the necessary model files.
+
+Running this script will estimate the relative depth of the input image/video.
+The results will be shown in a separate window(when inferencing on image and video), 
+or saved as a single channel image(when inferencing on image).
+
+#### Example 1: Inference on prepared demo image.
+```bash
+$ python3 depth_anything.py
+```
+The result will be saved to ```demo_result.png``` by default.
+
+#### Example 2: Specify input path, save path, and encoder type.
+```bash
+$ python3 depth_anything.py -i custom_input.png -s result.png -ec vitl
+```
+```-i```, ```-s```, ```-ec``` options can be used to specify the
+input path, save path, and encoder type separately.
+
+#### Example 3: Inference on Video.
+```bash
+$ python3 depth_anything.py -v 0
+```
+argument after the ```-v``` option can be the device id of the webcam,
+or the path to the input video.
+
+## Reference
+
+* [DepthAnythig](https://github.com/LiheYoung/Depth-Anything)
+
+## Framework
+
+Pytorch
+
+
+
+
+
+
+## Model Format
+
+ONNX opset=11
+
+## Netron
+
---
+++ depth_estimation/depth_anything/depth_anything.py
@@ -0,0 +1,190 @@
+import sys
+import time
+
+import ailia
+import cv2
+
+import numpy as np
+
+# import original modules
+sys.path.append('../../util')
+
+from depth_anything_util.transform import Resize, NormalizeImage, PrepareForNet
+# logger
+from logging import getLogger  # noqa: E402
+
+import webcamera_utils  # noqa: E402
+from image_utils import imread  # noqa: E402
+from model_utils import check_and_download_models  # noqa: E402
+from arg_utils import get_base_parser, get_savepath, update_parser  # noqa: E402
+
+import matplotlib.pyplot as plt
+
+logger = getLogger(__name__)
+
+
+# ======================
+# Parameters
+# ======================
+
+WEIGHT_PATH_S = "depth_anything_vits14.onnx"
+MODEL_PATH_S = "depth_anything_vits14.onnx.prototxt"
+
+WEIGHT_PATH_B = "onnx_repo_safe/depth_anything_vitb14.onnx"
+MODEL_PATH_B = "depth_anything_vitb14.onnx.prototxt"
+
+WEIGHT_PATH_L = "onnx_repo_safe/depth_anything_vitl14.onnx"
+MODEL_PATH_L = "depth_anything_vitl14.onnx.prototxt"
+
+REMOTE_PATH = None
+
+DEFAULT_INPUT_PATH = 'demo1.png'
+DEFAULT_SAVE_PATH = 'demo_result.png'
+
+# ======================
+# Arguemnt Parser Config
+# ======================
+parser = get_base_parser(
+    'Demo of Depth Anything: Unleashing the Power of Large-Scale Unlabeled Data',
+    DEFAULT_INPUT_PATH, DEFAULT_SAVE_PATH
+)
+
+parser.add_argument(
+    '--encoder', '-ec', type=str, default='vits',
+    help='model type. vits, vitb, vitl'
+)
+
+args = update_parser(parser)
+
+# ======================
+# Helper functions
+# ======================
+
+class get_depth_anything_ts():
+    def __init__(self, mean = (0.485, 0.456, 0.406), std = (0.229, 0.224, 0.225)):
+        self.mean = mean
+        self.std = std
+        self.resize = Resize(
+               width=518,
+               height=518,
+               resize_target=False,
+               keep_aspect_ratio=True,
+               ensure_multiple_of=14,
+               resize_method='lower_bound',
+               image_interpolation_method=cv2.INTER_CUBIC,
+           )
+        self.normalize = NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        self.prepare = PrepareForNet()
+    def __call__(self, image):
+        image = self.resize(image)
+        image = self.normalize(image)
+        image = self.prepare(image)
+        return image
+
+def plot_image(image, depth, savepath=None):
+    plt.imshow(depth)
+    plt.show()
+    if savepath is not None:
+        logger.info(f'saving result to {savepath}')
+        cv2.imwrite(savepath, depth)
+
+def update_frame(image, depth, frame):
+    if frame is None:
+        frame = plt.imshow(depth)
+        plt.pause(.01)
+    else:
+        frame.set_data(depth)
+        plt.pause(0.1)
+    return frame
+
+def post_process(depth, h, w):
+    depth = cv2.resize(depth[0,0], dsize=(w, h), interpolation=cv2.INTER_LINEAR)
+    depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
+    depth = depth.astype(np.uint8)
+    return depth
+
+# ======================
+# Main functions
+# ======================
+
+def recognize_from_image(model):
+    da_transform = get_depth_anything_ts()
+
+    # input image loop
+    logger.info('Start inference...')
+    for image_path in args.input:
+        # prepare input data
+        org_img = cv2.cvtColor(imread(image_path), cv2.COLOR_BGR2RGB) / 255.
+
+        
+        image = da_transform({'image':org_img})['image'][None]
+        if args.benchmark and not (args.video is not None):
+            logger.info('BENCHMARK mode')
+            for i in range(5):
+                start = int(round(time.time() * 1000))
+                depth = model.predict(image)
+                end = int(round(time.time() * 1000))
+                logger.info(f'\tailia processing time {end - start} ms')
+        else:
+            depth = model.predict(image)
+        depth = post_process(depth, org_img.shape[0], org_img.shape[1])
+
+        # visualize
+        plot_image(org_img, depth, args.savepath)
+
+    logger.info('Script finished successfully.')
+
+def recognize_from_video(model):
+    da_transform = get_depth_anything_ts()
+    # net initialize
+
+    capture = webcamera_utils.get_capture(args.video)
+
+    frame_shown = None
+    while(True):
+        ret, frame = capture.read()
+        if (cv2.waitKey(1) & 0xFF == ord("q")) or not ret:
+            break
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) / 255.
+
+        # inference
+        image = da_transform({'image':frame})['image'][None]
+        depth = model.predict(image)
+        depth = post_process(depth, frame.shape[0], frame.shape[1])
+
+        # visualize
+        frame_shown = update_frame(frame, depth, frame_shown)
+        if not plt.get_fignums():
+            break
+
+    capture.release()
+    logger.info('Script finished successfully.')
+
+def main():
+    # model files check and download
+    assert args.encoder in ['vits', 'vitb', 'vitl'], 'encoder should be vits, vitb, or vitl'
+    if args.encoder == 'vits':
+        WEIGHT_PATH = WEIGHT_PATH_S
+        MODEL_PATH = MODEL_PATH_S
+    elif args.encoder == 'vitb':
+        WEIGHT_PATH = WEIGHT_PATH_B
+        MODEL_PATH = MODEL_PATH_B
+    else:
+        WEIGHT_PATH = WEIGHT_PATH_L
+        MODEL_PATH = MODEL_PATH_L
+
+    check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
+    
+    # net initialize
+    model = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id = args.env_id)
+
+    if args.video is not None:
+        # video mode
+        recognize_from_video(model)
+    else:
+        # image mode
+        recognize_from_image(model)
+
+
+if __name__ == '__main__':
+    main()
