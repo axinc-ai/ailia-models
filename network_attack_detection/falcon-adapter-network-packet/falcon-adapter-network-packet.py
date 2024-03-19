500b80f6889246d9951ecc8b399158d865a43133
ooe1123 <ooe1123@gmail.com>
2024-03-19 23:36:12 +0900

    wip

--- network_attack_detection/falcon-adapter-network-packet/falcon-adapter-network-packet.py
+++ network_attack_detection/falcon-adapter-network-packet/falcon-adapter-network-packet.py
@@ -1,11 +1,21 @@
 import sys
 import time
-import joblib
+import re
+import itertools
+import importlib
+import pickle
+from dataclasses import dataclass
 from logging import getLogger
 
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize
+from nltk.stem import WordNetLemmatizer
 from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
 from transformers import AutoTokenizer
 from scapy.all import Ether, CookedLinux
 
@@ -22,13 +32,24 @@ logger = getLogger(__name__)
 # Parameters
 # ======================
 
-WEIGHT_ENC_PATH = "reazonspeech-nemo-v2_encoder.onnx"
-MODEL_ENC_PATH = "reazonspeech-nemo-v2_encoder.onnx.prototxt"
+WEIGHT_ENC_PATH = "xxx.onnx"
+MODEL_ENC_PATH = "xxx.onnx.prototxt"
 REMOTE_PATH = (
     "https://storage.googleapis.com/ailia-models/falcon-adapter-network-packet/"
 )
 
 SAVE_TEXT_PATH = "output.txt"
+
+
+max_document_length = 375
+max_heading_length = 10
+max_ngram = 10
+exclude_stopwords = ["dos"]
+stop_words = [word for word in stopwords.words() if word not in exclude_stopwords]
+doc_regex = "[\([][0-9]+[\])]|[”“‘’‛‟]|\d+\s"
+punctuations = """!"#$%&'()*+,-./:—;<=>−?–@[\]^_`{|}~"""
+punctuations_continuity_exclude = """—-–,−"""
+
 
 # ======================
 # Arguemnt Parser Config
@@ -46,6 +67,279 @@ args = update_parser(parser)
 # ======================
 # Secondaty Functions
 # ======================
+
+
+@dataclass
+class _SimilarWords:
+    bert_words = []
+    bert_vectors = []
+    bert_documents = []
+    continous_words = []
+    temporary_ngram_words = []
+    count_vectorizer_words = []
+    cv_counts = []
+    cv_words = []
+
+    bert_words_ngram = [[] for _ in range(max_ngram)]
+    bert_vectors_ngram = [[] for _ in range(max_ngram)]
+    bert_documents_ngram = [[] for _ in range(max_ngram)]
+    bert_words_all = []
+    bert_vectors_all = []
+
+
+similar = _SimilarWords()
+
+
+def process_txt_dataset(path):
+    document_list = []
+    with open(path) as file:
+        for line in tqdm(
+            file.readlines(),
+            unit=" paragraphs",
+            desc="Extracting",
+            postfix="Data from Dataset",
+        ):
+            line_text = line.strip()
+            line_text = re.sub(doc_regex, "", line_text)
+            line_length = len(line_text.split())
+            if 0 < line_length <= max_heading_length:
+                if (
+                    len(document_list) != 0
+                    and len(document_list[-1].split()) <= max_heading_length
+                ):
+                    document_list[-1] = line_text + "."
+                else:
+                    document_list.append(line_text + ".")
+            elif line_length > max_heading_length:
+                if (
+                    len(document_list) != 0
+                    and len(document_list[-1].split()) + line_length
+                    <= max_document_length
+                ):
+                    document_list[-1] += " " + line_text
+                else:
+                    document_list = process_dataset_long_paragraph(
+                        document_list,
+                        line_text,
+                        len(document_list[-1].split()) + line_length,
+                    )
+
+    return document_list
+
+
+def process_dataset_long_paragraph(document_list, sentence, sentence_length):
+    if sentence_length > max_document_length:
+        for i in range(2, sentence_length):
+            div = sentence_length / i
+            if div < max_document_length:
+                break
+        temp_sent = ""
+        sm_sent = sent_tokenize(sentence)
+
+        for sent in sm_sent:
+            if len(temp_sent.split() + sent.split()) > div:
+                if len(document_list[-1].split()) <= max_heading_length:
+                    document_list[-1] += " " + temp_sent
+                else:
+                    document_list.append(temp_sent)
+                temp_sent = ""
+            temp_sent = temp_sent + sent
+
+        if len(document_list[-1].split() + temp_sent.split()) < max_document_length:
+            document_list[-1] += " " + temp_sent
+        else:
+            document_list.append(temp_sent)
+    else:
+        document_list.append(sentence)
+
+    return document_list
+
+
+def tokenize_and_embeddings(models, document_list):
+    tokenizer = models["tokenizer"]
+    net = models["similar"]
+
+    continous_index = 0
+    document_index = 0
+    for document in tqdm(
+        document_list, unit=" documents", desc="Processing", postfix="Word Embeddings"
+    ):
+        # for document in document_list:
+        tokens = tokenizer(document, truncation=True, return_tensors="np")
+        words = tokenizer.convert_ids_to_tokens(tokens["input_ids"][0])
+        word_ids = tokens.word_ids()
+
+        input_ids = tokens["input_ids"]
+        attention_mask = tokens["attention_mask"]
+
+        # feedforward
+        if not args.onnx:
+            output = net.predict([input_ids, attention_mask])
+        else:
+            output = net.run(
+                None, {"input_ids": input_ids, "attention_mask": attention_mask}
+            )
+        last_hidden_state = output[0]
+        vectors = last_hidden_state[0]
+
+        word_list = []
+        vector_list = []
+        continous_words = []
+        word_index = -1
+        for i in range(len(words)):
+            if word_ids[i] is None or words[i] in punctuations:
+                if words[i] in punctuations_continuity_exclude:
+                    pass
+                else:
+                    continous_index = continous_index + 1
+                continue
+            if word_ids[i] > word_index:
+                if len(word_list) != 0 and word_list[-1].lower() in stop_words:
+                    word_list.pop()
+                    vector_list.pop()
+                    continous_words.pop()
+                    continous_index = continous_index + 1
+                word_list.append(words[i])
+                vector_list.append(vectors[i])
+                continous_words.append(continous_index)
+                word_index = word_ids[i]
+            elif word_ids[i] == word_index:
+                sub_word = words[i].replace("##", "")
+                word_list[-1] = word_list[-1] + sub_word
+                vector_list[-1] = vector_list[-1] + vectors[i]
+                if word_ids[i + 1] != word_ids[i]:
+                    vector_list[-1] = vector_list[-1] / word_ids.count(word_index)
+
+        yield word_list, vector_list, [document_index] * len(word_list), continous_words
+        document_index += 1
+
+
+def generate_n_grams(i, words, vectors, document, continous, n=1):
+    if i > n - 1 and n < max_ngram and continous[i] == continous[i - n]:
+        temp_word = ""
+        temp_vector = np.zeros([len(vectors[i])])
+        for j in range(n, -1, -1):
+            temp_word = temp_word + " " + words[i - j]
+            temp_vector = temp_vector + vectors[i - j]
+        similar.temporary_ngram_words.append(temp_word.strip())
+        similar.bert_words_ngram[n].append(temp_word.strip())
+        similar.bert_vectors_ngram[n].append(temp_vector / (n + 1))
+        similar.bert_documents_ngram[n].append(document[i])
+        generate_n_grams(i, words, vectors, document, continous, n=n + 1)
+
+    return
+
+
+def custom_analyzer(words):
+    final_list = []
+    for word in words:
+        final_list.append(word)
+        lemmatized_word = " ".join(
+            [
+                custom_analyzer.lemmatizer.lemmatize(token.lower())
+                for token in word.split()
+            ]
+        )
+        if word != lemmatized_word:
+            final_list.append(lemmatized_word)
+    return final_list
+
+
+custom_analyzer.lemmatizer = WordNetLemmatizer()
+count_vectorizer = CountVectorizer(analyzer=custom_analyzer)
+
+
+def load_dataset(models):
+    document_list = process_txt_dataset("dataset.txt")
+    for words, vectors, document, continous in tokenize_and_embeddings(
+        models, document_list
+    ):
+        temporary_ngram_words = []
+        for i in range(len(words)):
+            generate_n_grams(i, words, vectors, document, continous)
+        similar.bert_words.extend(words)
+        similar.bert_vectors.extend(vectors)
+        similar.bert_documents.extend(document)
+        similar.continous_words.extend(continous)
+        similar.count_vectorizer_words.append(words + temporary_ngram_words)
+
+    similar.bert_words_ngram[0] = similar.bert_words
+    similar.bert_vectors_ngram[0] = similar.bert_vectors
+    similar.bert_documents_ngram[0] = similar.bert_documents
+    similar.cv_counts = count_vectorizer.fit_transform(similar.count_vectorizer_words)
+    similar.cv_words = count_vectorizer.get_feature_names_out()
+    similar.bert_words_all = np.array(
+        list(itertools.chain.from_iterable(similar.bert_words_ngram))
+    )
+    similar.bert_vectors_all = np.array(
+        list(itertools.chain.from_iterable(similar.bert_vectors_ngram))
+    )
+
+    scaler = models["scaler"]
+    similar.bert_vectors_all = scaler.transform(similar.bert_vectors_all)
+    for i in tqdm(
+        range(max_ngram), desc="Generating", postfix="N-gram Words and Embeddings"
+    ):
+        similar.bert_vectors_ngram[i] = scaler.transform(similar.bert_vectors_ngram[i])
+
+
+def find_similar_words(
+    input_embedding,
+    pos_to_exclude=[],
+    max_output_words=10,
+    context_similarity_factor=0.25,
+    output_filter_factor=0.5,
+    single_word_split=True,
+    uncased_lemmatization=True,
+):
+    input_context_words = []
+
+    a = []
+    for i in range(0, len(similar.bert_vectors_all), 100000):
+        a.append(
+            cosine_similarity(
+                similar.bert_vectors_all[i : i + 100000, :], [input_embedding]
+            ).flatten()
+        )
+    cosine_sim = np.concatenate(a)
+    cosine_words = similar.bert_words_all
+
+    output_dict = {}
+    sorted_list = np.flip(np.argsort(cosine_sim))
+    lemmatized_words = {
+        self.lemmatizer.lemmatize(token.lower())
+        for word in input_context_words
+        for token in word.split()
+    }
+
+    for i in range(len(cosine_words)):
+        stop = 0
+        pop_list = []
+        original_word = cosine_words[sorted_list[i]]
+        pos_tags = [pos[1] for pos in nltk.pos_tag(original_word.split())]
+        lemmatized_word = {
+            self.lemmatizer.lemmatize(token.lower()) for token in original_word.split()
+        }
+        if len(
+            lemmatized_words.intersection(lemmatized_word)
+        ) > output_filter_factor * len(original_word.split()):
+            continue
+        if any(pos in pos_tags for pos in pos_to_exclude):
+            continue
+        if original_word not in output_dict.keys():
+            for word in output_dict.keys():
+                if original_word in word:
+                    stop = 1
+                    break
+                elif word in original_word:
+                    pop_list.append(word)
+                    stop = 0
+            if stop == 0:
+                pop = [output_dict.pop(key) for key in pop_list]
+                output_dict[original_word] = cosine_sim[sorted_list[i]]
+                if len(output_dict.keys()) == max_output_words:
+                    break
+    return output_dict
 
 
 # ======================
@@ -126,16 +420,16 @@ def predict(models, packet_hex):
     final_format = " ".join(final_format)
 
     tokenizer = models["tokenizer"]
-    similar = models["similar"]
+    net = models["similar"]
     tokens = tokenizer(final_format, truncation=True, return_tensors="np")
     input_ids = tokens["input_ids"]
     attention_mask = tokens["attention_mask"]
 
     # feedforward
     if not args.onnx:
-        output = similar.predict([input_ids, attention_mask])
+        output = net.predict([input_ids, attention_mask])
     else:
-        output = similar.run(
+        output = net.run(
             None, {"input_ids": input_ids, "attention_mask": attention_mask}
         )
     last_hidden_state = output[0]
@@ -147,6 +441,7 @@ def predict(models, packet_hex):
     embedding = scaler.transform(df)[0]
 
     cluster_centers = models["cluster_centers"]
+    tag_names = models["tag_names"]
 
     euclidean_distance = euclidean_distances(cluster_centers, [embedding])
     data = {i: euclidean_distance[i][0] for i in range(len(euclidean_distance))}
@@ -157,6 +452,20 @@ def predict(models, packet_hex):
         int(df.iloc[0]["Cluster"]),
         df.iloc[0]["Euclidean Distance"],
         embedding,
+    )
+
+    total_tags = 10
+    context_similarity_factor = 10
+    uncased_lemmatization = True
+    single_word_split = False
+    output_filter_factor = 1
+    tags, emb = find_similar_words(
+        input_embedding=(tag_names[cluster] + (embedding - cluster_centers[cluster])),
+        max_output_words=total_tags,
+        context_similarity_factor=context_similarity_factor,
+        uncased_lemmatization=uncased_lemmatization,
+        single_word_split=single_word_split,
+        output_filter_factor=output_filter_factor,
     )
 
     return 0
@@ -220,14 +529,17 @@ def main():
 
     scaler = joblib.load("scaler.pkl")
     cluster_centers = np.load("KMEANS-CLUSTER-CENTERS.npy")
+    tag_names = np.load("TAGS-NAMES-EMBEDDINGS.npy")
 
     models = {
         "tokenizer": tokenizer,
         "similar": similar,
         "scaler": scaler,
         "cluster_centers": cluster_centers,
-        # "joint": joint,
+        "tag_names": tag_names,
     }
+
+    load_dataset(models)
 
     recognize_from_packet(models)
 
