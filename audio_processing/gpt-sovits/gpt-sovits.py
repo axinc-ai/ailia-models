e36393e1ac525994d623be931b531666d5d28f43
Kazuki Kyakuno <kyakuno@axell.co.jp>
2024-03-21 16:35:53 +0900

    Remove torch dependency

--- audio_processing/gpt-sovits/gpt-sovits.py
+++ audio_processing/gpt-sovits/gpt-sovits.py
@@ -17,12 +17,8 @@ from scipy.io.wavfile import write
 from logging import getLogger   # noqa: E402
 logger = getLogger(__name__)
 
-import torch
-import torchaudio
-from torch import nn
-
+#import torchaudio
 import onnxruntime
-import LangSegment
 
 import os
 from text import cleaned_text_to_sequence
@@ -30,7 +26,6 @@ from text.japanese import g2p
 import soundfile
 
 import ffmpeg
-import numpy as np
 import librosa
 
 # ======================
@@ -101,13 +96,12 @@ def load_audio(file, sr):
     return np.frombuffer(out, np.float32).flatten()
 
 
-class T2SModel(nn.Module):
+class T2SModel():
     def __init__(self, sess_encoder, sess_fsdec, sess_sdec):
-        super().__init__()
         self.hz = 50
         self.max_sec = 54
         self.top_k = 5
-        self.early_stop_num = torch.LongTensor([self.hz * self.max_sec])
+        self.early_stop_num = np.array([self.hz * self.max_sec])
         self.sess_encoder = sess_encoder
         self.sess_fsdec = sess_fsdec
         self.sess_sdec = sess_sdec
@@ -115,79 +109,64 @@ class T2SModel(nn.Module):
     def forward(self, ref_seq, text_seq, ref_bert, text_bert, ssl_content):
         early_stop_num = self.early_stop_num
 
-        top_k = torch.LongTensor([5])
-        top_p = torch.Tensor([1.0])
-        temperature = torch.Tensor([1.0])
-        repetition_penalty = torch.Tensor([1.35])
+        top_k = np.array([5], dtype=np.int64)
+        top_p = np.array([1.0], dtype=np.float32)
+        temperature = np.array([1.0], dtype=np.float32)
+        repetition_penalty = np.array([1.35], dtype=np.float32)
 
         EOS = 1024
 
         #[1,N] [1,N] [N, 1024] [N, 1024] [1, 768, N]
-        x, prompts = self.sess_encoder.run(None, {"ref_seq":ref_seq.detach().numpy(), "text_seq":text_seq.detach().numpy(), "ref_bert":ref_bert.detach().numpy(), "text_bert":text_bert.detach().numpy(), "ssl_content":ssl_content.detach().numpy()})
-        x = torch.from_numpy(x)
-        prompts = torch.from_numpy(prompts)
+        x, prompts = self.sess_encoder.run(None, {"ref_seq":ref_seq, "text_seq":text_seq, "ref_bert":ref_bert, "text_bert":text_bert, "ssl_content":ssl_content})
 
         prefix_len = prompts.shape[1]
 
         #[1,N,512] [1,N]
-        y, k, v, y_emb, x_example = self.sess_fsdec.run(None, {"x":x.detach().numpy(), "prompts":prompts.detach().numpy(), "top_k":top_k.detach().numpy(), "top_p":top_p.detach().numpy(), "temperature":temperature.detach().numpy(), "repetition_penalty":repetition_penalty.detach().numpy()})
-        y = torch.from_numpy(y)
-        k = torch.from_numpy(k)
-        v = torch.from_numpy(v)
-        y_emb = torch.from_numpy(y_emb)
-        x_example = torch.from_numpy(x_example)
+        y, k, v, y_emb, x_example = self.sess_fsdec.run(None, {"x":x, "prompts":prompts, "top_k":top_k, "top_p":top_p, "temperature":temperature, "repetition_penalty":repetition_penalty})
 
         stop = False
         for idx in range(1, 1500):
             #[1, N] [N_layer, N, 1, 512] [N_layer, N, 1, 512] [1, N, 512] [1] [1, N, 512] [1, N]
-            y, k, v, y_emb, logits, samples = self.sess_sdec.run(None, {"iy":y.detach().numpy(), "ik":k.detach().numpy(), "iv":v.detach().numpy(), "iy_emb":y_emb.detach().numpy(), "ix_example":x_example.detach().numpy(), "top_k":top_k.detach().numpy(), "top_p":top_p.detach().numpy(), "temperature":temperature.detach().numpy(), "repetition_penalty":repetition_penalty.detach().numpy()})
-            y = torch.from_numpy(y)
-            k = torch.from_numpy(k)
-            v = torch.from_numpy(v)
-            y_emb = torch.from_numpy(y_emb)
-            logits = torch.from_numpy(logits)
-            samples = torch.from_numpy(samples)
+            y, k, v, y_emb, logits, samples = self.sess_sdec.run(None, {"iy":y, "ik":k, "iv":v, "iy_emb":y_emb, "ix_example":x_example, "top_k":top_k, "top_p":top_p, "temperature":temperature, "repetition_penalty":repetition_penalty})
             if early_stop_num != -1 and (y.shape[1] - prefix_len) > early_stop_num:
                 stop = True
-            if torch.argmax(logits, dim=-1)[0] == EOS or samples[0, 0] == EOS:
+            if np.argmax(logits, axis=-1)[0] == EOS or samples[0, 0] == EOS:
                 stop = True
             if stop:
                 break
         y[0, -1] = 0
 
-        return y[:, -idx:-1].unsqueeze(0)
+        return y[np.newaxis, :, -idx:-1]
 
 
 
 
 
-class GptSoVits(nn.Module):
+class GptSoVits():
     def __init__(self, t2s, sess):
-        super().__init__()
         self.t2s = t2s
         self.sess = sess
     
     def forward(self, ref_seq, text_seq, ref_bert, text_bert, ref_audio, ssl_content):
-        pred_semantic = self.t2s(ref_seq, text_seq, ref_bert, text_bert, ssl_content)
+        pred_semantic = self.t2s.forward(ref_seq, text_seq, ref_bert, text_bert, ssl_content)
         audio1 = self.sess.run(None, {
-            "text_seq" : text_seq.detach().cpu().numpy(),
-            "pred_semantic" : pred_semantic.detach().cpu().numpy(), 
-            "ref_audio" : ref_audio.detach().cpu().numpy()
+            "text_seq" : text_seq,
+            "pred_semantic" : pred_semantic, 
+            "ref_audio" : ref_audio
         })
-        return torch.from_numpy(audio1[0])
+        return audio1[0]
 
 
 
-class SSLModel(nn.Module):
+class SSLModel():
     def __init__(self, sess):
-        super().__init__()
         self.sess = sess
 
     def forward(self, ref_audio_16k):
         last_hidden_state = self.sess.run(None, {
-            "ref_audio_16k" : ref_audio_16k.detach().cpu().numpy()
+            "ref_audio_16k" : ref_audio_16k
         })
-        return torch.from_numpy(last_hidden_state[0])
+        return last_hidden_state[0]
 
 
 def generate_voice(ssl, t2s_encoder, t2s_first_decoder, t2s_stage_decoder, vits):
@@ -195,44 +174,44 @@ def generate_voice(ssl, t2s_encoder, t2s_first_decoder, t2s_stage_decoder, vits)
     gpt_sovits = GptSoVits(gpt, vits)
     ssl = SSLModel(ssl)
 
-    ref_audio = torch.randn((1, 48000 * 5)).float()
-
     input_audio = args.audio
     ref_phones = g2p(args.transcript)
-    ref_audio = torch.tensor([load_audio(input_audio, 48000)]).float()
+    ref_audio = load_audio(input_audio, 48000)
+    ref_audio = ref_audio[np.newaxis, :]
 
-    ref_seq = torch.LongTensor([cleaned_text_to_sequence(ref_phones)])
-
+    ref_seq = np.array([cleaned_text_to_sequence(ref_phones)], dtype=np.int64)
     text_phones = g2p(args.input)
-    print(text_phones)
-    text_seq = torch.LongTensor([cleaned_text_to_sequence(text_phones)])
+    text_seq = np.array([cleaned_text_to_sequence(text_phones)], dtype=np.int64)
 
     # empty for ja or en
-    ref_bert = torch.zeros((ref_seq.shape[1], 1024)).float()
-    text_bert = torch.zeros((text_seq.shape[1], 1024)).float()
-
-    ref_audio_16k = torchaudio.functional.resample(ref_audio,48000,16000).float()
+    ref_bert = np.zeros((ref_seq.shape[1], 1024), dtype=np.float32)
+    text_bert = np.zeros((text_seq.shape[1], 1024), dtype=np.float32)
+    
+    #import torch
+    #ref_audio_16k = torchaudio.functional.resample(torch.from_numpy(ref_audio),48000,16000).float().detach().numpy()
     vits_hps_data_sampling_rate = 32000
-    ref_audio_sr = torchaudio.functional.resample(ref_audio,48000,vits_hps_data_sampling_rate).float()
+    #ref_audio_sr = torchaudio.functional.resample(torch.from_numpy(ref_audio),48000,vits_hps_data_sampling_rate).float().detach().numpy()
 
     zero_wav = np.zeros(
         int(vits_hps_data_sampling_rate * 0.3),
         dtype=np.float32,
     )
     wav16k, sr = librosa.load(input_audio, sr=16000)
-    wav16k = torch.from_numpy(wav16k)
-    zero_wav_torch = torch.from_numpy(zero_wav)
-    wav16k = torch.cat([wav16k, zero_wav_torch]).unsqueeze(0)
+    wav16k = np.concatenate([wav16k, zero_wav], axis=0)
+    wav16k = wav16k[np.newaxis, :]
     ref_audio_16k = wav16k # hubertの入力のみpaddingする
-    
-    ssl_content = ssl(ref_audio_16k).float()
 
-    a = gpt_sovits(ref_seq, text_seq, ref_bert, text_bert, ref_audio_sr, ssl_content)
+    wav32k, sr = librosa.load(input_audio, sr=vits_hps_data_sampling_rate)
+    wav32k = wav32k[np.newaxis, :]
+
+    ssl_content = ssl.forward(ref_audio_16k)
+
+    a = gpt_sovits.forward(ref_seq, text_seq, ref_bert, text_bert, wav32k, ssl_content)
 
     savepath = args.savepath
     logger.info(f'saved at : {savepath}')
 
-    soundfile.write(savepath, a.cpu().detach().numpy(), vits_hps_data_sampling_rate)
+    soundfile.write(savepath, a, vits_hps_data_sampling_rate)
 
     logger.info('Script finished successfully.')
 
