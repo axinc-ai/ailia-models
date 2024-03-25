f952013051dcac7e639a73885b63e4fa23bee3c7
Daisuke AKAGAWA <daisuke1024akagawa@gmail.com>
2024-03-25 20:09:34 +0900

    add recognition_utils

---
+++ diffusion/instant_id/recognition_utils.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+from skimage import transform as trans
+
+ARCFACE_DST = np.array(
+    [
+        [38.2946, 51.6963],
+        [73.5318, 51.5014],
+        [56.0252, 71.7366],
+        [41.5493, 92.3655],
+        [70.7299, 92.2041],
+    ],
+    dtype=np.float32,
+)
+
+
+def estimate_norm(landmark, image_size):
+    if image_size % 112 == 0:
+        ratio = float(image_size) / 112.0
+        diff_x = 0
+    else:
+        ratio = float(image_size) / 128.0
+        diff_x = 8.0 * ratio
+    dst = ARCFACE_DST * ratio
+    dst[:, 0] += diff_x
+    tform = trans.SimilarityTransform()
+    tform.estimate(landmark, dst)
+    M = tform.params[0:2, :]
+    return M
+
+
+def norm_crop(img, landmark, image_size=112):
+    M = estimate_norm(landmark, image_size)
+    warped = cv2.warpAffine(img, M, (image_size, image_size), borderValue=0.0)
+    return warped
+
+
+def get_recognition():
+    pass
