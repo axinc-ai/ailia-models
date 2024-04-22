af45898fb2c47c657d46bcdf2516727902090840
ooe1123 <ooe1123@gmail.com>
2024-04-22 22:29:49 +0900

    fix

--- llm/llava/llava.py
+++ llm/llava/llava.py
@@ -39,10 +39,6 @@ IMG_SIZE = 336
 IGNORE_INDEX = -100
 IMAGE_TOKEN_INDEX = -200
 DEFAULT_IMAGE_TOKEN = "<image>"
-DEFAULT_IMAGE_PATCH_TOKEN = "<im_patch>"
-DEFAULT_IM_START_TOKEN = "<im_start>"
-DEFAULT_IM_END_TOKEN = "<im_end>"
-IMAGE_PLACEHOLDER = "<image-placeholder>"
 
 
 # ======================
