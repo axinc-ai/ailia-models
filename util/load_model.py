bba3bcfda3eed2c93b46980bca8f806fb8eec274
ooe1123 <ooe1123@gmail.com>
2024-07-31 23:24:47 +0900

    Implement facemesh option

---
+++ util/load_model.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from importlib import import_module
+
+import arg_utils
+
+
+top_path = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__),
+    )
+)
+
+
+def load_model(mod_name):
+    mod = None
+
+    def dummy_parser(parser, *args):
+        args = parser.parse_args()
+        return args
+
+    argv = sys.argv
+    update_parser = arg_utils.update_parser
+    try:
+        sys.argv = sys.argv[:1]
+        arg_utils.update_parser = dummy_parser
+
+        mod = import_module(mod_name)
+    finally:
+        sys.argv = argv
+        arg_utils.update_parser = update_parser
+
+    return mod
+
+
+def load_facemesh_v2(args=None):
+    sys.path.insert(0, os.path.join(top_path, "face_recognition/facemesh_v2"))
+    mod = load_model("facemesh_v2")
+    sys.path.pop(0)
+
+    if args:
+        for name in vars(args):
+            setattr(mod.args, name, getattr(args, name))
+    return mod
