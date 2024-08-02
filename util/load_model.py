fdb39c8f1848754deefc08b02ecd713381e3bc64
ooe1123 <ooe1123@gmail.com>
2024-08-02 22:23:23 +0900

    Add some model

--- util/load_model.py
+++ util/load_model.py
@@ -33,9 +33,42 @@ def load_model(mod_name):
     return mod
 
 
+def load_groudingdino(args=None):
+    sys.path.insert(0, os.path.join(top_path, "object_detection/groundingdino"))
+    mod = load_model("groundingdino")
+    sys.path.pop(0)
+
+    if args:
+        for name in vars(args):
+            setattr(mod.args, name, getattr(args, name))
+    return mod
+
+
+def load_face_detection_adas(args=None):
+    sys.path.insert(0, os.path.join(top_path, "face_detection/face-detection-adas"))
+    mod = load_model("face-detection-adas")
+    sys.path.pop(0)
+
+    if args:
+        for name in vars(args):
+            setattr(mod.args, name, getattr(args, name))
+    return mod
+
+
 def load_facemesh_v2(args=None):
     sys.path.insert(0, os.path.join(top_path, "face_recognition/facemesh_v2"))
     mod = load_model("facemesh_v2")
+    sys.path.pop(0)
+
+    if args:
+        for name in vars(args):
+            setattr(mod.args, name, getattr(args, name))
+    return mod
+
+
+def load_bytetrack(args=None):
+    sys.path.insert(0, os.path.join(top_path, "object_tracking/bytetrack"))
+    mod = load_model("bytetrack")
     sys.path.pop(0)
 
     if args:
