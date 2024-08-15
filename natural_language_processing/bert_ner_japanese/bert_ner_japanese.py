ecb400ad53e4f1d8d17a2fb7a9a488172da36fba
ooe1123 <ooe1123@gmail.com>
2024-08-16 02:00:01 +0900

    Implement bert-ner-japanase

--- natural_language_processing/bert_ner_japanese/bert_ner_japanese.py
+++ natural_language_processing/bert_ner_japanese/bert_ner_japanese.py
@@ -1,5 +1,7 @@
 import sys
 import time
+import json
+
 from logging import getLogger
 
 import numpy as np
@@ -8,8 +10,10 @@ import ailia
 
 # import original modules
 sys.path.append("../../util")
-from arg_utils import get_base_parser, update_parser, get_savepath  # noqa
+from arg_utils import get_base_parser, update_parser  # noqa
 from model_utils import check_and_download_models  # noqa
+
+from token_classification import TokenClassification
 
 logger = getLogger(__name__)
 
@@ -21,23 +25,27 @@ WEIGHT_PATH = "model.onnx"
 MODEL_PATH = "model.onnx.prototxt"
 REMOTE_PATH = "https://storage.googleapis.com/ailia-models/bert_ner_japanese/"
 
-TEXT_PATH = "sentences.txt"
-
 # ======================
 # Arguemnt Parser Config
 # ======================
 
-parser = get_base_parser("bert-ner-japanese ", TEXT_PATH, None)
-parser.add_argument("--onnx", action="store_true", help="execute onnxruntime version.")
+parser = get_base_parser("bert-ner-japanese ", None, None)
+parser.add_argument(
+    "-i",
+    "--input",
+    metavar="TEXT",
+    type=str,
+    default="株式会社Jurabiは、東京都台東区に本社を置くIT企業である。",
+    help="Input sentense.",
+)
 parser.add_argument(
     "--disable_ailia_tokenizer", action="store_true", help="disable ailia tokenizer."
 )
-args = update_parser(parser)
-
-
-# ======================
-# Secondaty Functions
-# ======================
+parser.add_argument(
+    "--disable_aggregation", action="store_true", help="disable aggregation."
+)
+parser.add_argument("--onnx", action="store_true", help="execute onnxruntime version.")
+args = update_parser(parser, check_input_type=False)
 
 
 # ======================
@@ -50,6 +58,7 @@ def preprocess(tokenizer, sentence):
         sentence,
         return_tensors="np",
         truncation=True,
+        # max_length=512,
         return_special_tokens_mask=True,
         return_offsets_mapping=False,
     )
@@ -63,8 +72,42 @@ def preprocess(tokenizer, sentence):
         yield model_inputs
 
 
-def post_processing(all_outputs):
-    pass
+def post_processing(tokenizer, all_outputs):
+    aggregation = not args.disable_aggregation
+
+    clf = TokenClassification(tokenizer)
+    ignore_labels = ["O"]
+    all_entities = []
+    for model_outputs in all_outputs:
+        logits = model_outputs["logits"][0]
+        input_ids = model_outputs["input_ids"][0]
+        special_tokens_mask = model_outputs["special_tokens_mask"][0]
+
+        maxes = np.max(logits, axis=-1, keepdims=True)
+        shifted_exp = np.exp(logits - maxes)
+        scores = shifted_exp / shifted_exp.sum(axis=-1, keepdims=True)
+
+        pre_entities = clf.gather_pre_entities(
+            input_ids,
+            scores,
+            special_tokens_mask,
+        )
+        grouped_entities = clf.aggregate(pre_entities, aggregation=aggregation)
+
+        # Filter anything that is in self.ignore_labels
+        entities = [
+            entity
+            for entity in grouped_entities
+            if entity.get("entity", None) not in ignore_labels
+            and entity.get("entity_group", None) not in ignore_labels
+        ]
+        all_entities.extend(entities)
+
+    num_chunks = len(all_outputs)
+    if 1 < num_chunks:
+        all_entities = clf.aggregate_overlapping_entities(all_entities)
+
+    return all_entities
 
 
 def predict(models, sentence):
@@ -93,16 +136,18 @@ def predict(models, sentence):
             )
         logits = output[0]
 
-        processed["logits"] = logits
+        item["logits"] = logits
         accumulator.append(item)
 
-    post_processing(accumulator)
+    entities = post_processing(tokenizer, accumulator)
 
-    return
+    return entities
 
 
-def recognize_from_sentence(models):
-    sentence = "株式会社Jurabiは、東京都台東区に本社を置くIT企業である。"
+def recognize_from_text(models):
+    sentence = args.input
+
+    logger.info("input_text: %s" % sentence)
 
     # inference
     logger.info("Start inference...")
@@ -111,7 +156,7 @@ def recognize_from_sentence(models):
         total_time_estimation = 0
         for i in range(args.benchmark_count):
             start = int(round(time.time() * 1000))
-            processed = predict(models, sentence)
+            entities = predict(models, sentence)
             end = int(round(time.time() * 1000))
             estimation_time = end - start
 
@@ -124,7 +169,9 @@ def recognize_from_sentence(models):
             f"\taverage time estimation {total_time_estimation / (args.benchmark_count - 1)} ms"
         )
     else:
-        processed = predict(models, sentence)
+        entities = predict(models, sentence)
+
+    print("tokenized:\n%s" % json.dumps(entities, indent=2, ensure_ascii=False))
 
     logger.info("Script finished successfully.")
 
@@ -150,16 +197,14 @@ def main():
 
         tokenizer = AutoTokenizer.from_pretrained("tokenizer")
     else:
-        from ailia_tokenizer import Tokenizer
-
-        # tokenizer = Tokenizer.from_pretrained("./tokenizer/")
+        raise NotImplementedError("ailia tokenizer is not supported yet.")
 
     models = {
         "net": net,
         "tokenizer": tokenizer,
     }
 
-    recognize_from_sentence(models)
+    recognize_from_text(models)
 
 
 if __name__ == "__main__":
