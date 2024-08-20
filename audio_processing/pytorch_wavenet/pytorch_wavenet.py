fdf7999500e631661f2505da26a7e7d473305548
ooe1123 <ooe1123@gmail.com>
2024-08-20 22:46:19 +0900

    formatten

--- audio_processing/pytorch_wavenet/pytorch_wavenet.py
+++ audio_processing/pytorch_wavenet/pytorch_wavenet.py
@@ -8,12 +8,13 @@ from torch.autograd import Variable, Function
 import ailia
 
 # import original moduls
-sys.path.append('../../util')
-from arg_utils import get_base_parser, update_parser, get_savepath  # noqa: E402
+sys.path.append("../../util")
+from utils import get_base_parser, update_parser, get_savepath  # noqa: E402
 from model_utils import check_and_download_models  # noqa: E402
 
 # logger
-from logging import getLogger   # noqa: E402
+from logging import getLogger  # noqa: E402
+
 logger = getLogger(__name__)
 
 
@@ -28,12 +29,11 @@ REMOTE_PATH = "https://storage.googleapis.com/ailia-models/pytorch-wavenet/"
 # ======================
 # Arguemnt Parser Config
 # ======================
-parser = get_base_parser('pytorch_wavenet', './dammy.wav', './output.txt', input_ftype='audio')
+parser = get_base_parser(
+    "pytorch_wavenet", "./dammy.wav", "./output.txt", input_ftype="audio"
+)
 parser.add_argument(
-    '-n', '--onnx',
-    action='store_true',
-    default=False,
-    help='Use onnxruntime'
+    "-n", "--onnx", action="store_true", default=False, help="Use onnxruntime"
 )
 args = update_parser(parser)
 
@@ -42,28 +42,30 @@ args = update_parser(parser)
 # Main function
 # ======================
 def get_model():
-    if not args.onnx :
-        logger.info('Use ailia')
+    if not args.onnx:
+        logger.info("Use ailia")
         net = ailia.Net(MODEL_PATH, WEIGHT_PATH, env_id=args.env_id)
-    else :
-        logger.info('Use onnxruntime')
+    else:
+        logger.info("Use onnxruntime")
         import onnxruntime
+
         net = onnxruntime.InferenceSession(WEIGHT_PATH)
     return net
 
 
 def generate_first_sample():
     first_samples = torch.randint(0, 256, (1028,))
-    #first_samples = torch.full((1028,),fill_value=126).long() # silence wave sample
+    # first_samples = torch.full((1028,),fill_value=126).long() # silence wave sample
     return first_samples
 
 
 def output_wav(filename, rate, data):
     from scipy.io.wavfile import write
+
     write(filename, rate, data)
 
 
-def generate_wave(net, num_samples, first_samples=None, temperature=1.):
+def generate_wave(net, num_samples, first_samples=None, temperature=1.0):
     tic = time.time()
 
     receptive_field = 1021
@@ -72,13 +74,15 @@ def generate_wave(net, num_samples, first_samples=None, temperature=1.):
     generated = Variable(first_samples, volatile=True)
     num_pad = receptive_field - generated.size(0)
 
-    #if num_pad > 0:
+    # if num_pad > 0:
     #    generated = _constant_pad_1d(generated, self.scope, pad_start=True)
     #    print("pad zero")
 
     for i in range(num_samples):
         input = Variable(torch.FloatTensor(1, classes, receptive_field).zero_())
-        input = input.scatter_(1, generated[-receptive_field:].view(1, -1, receptive_field), 1.)
+        input = input.scatter_(
+            1, generated[-receptive_field:].view(1, -1, receptive_field), 1.0
+        )
 
         output = _inference(net, input)
 
@@ -90,7 +94,7 @@ def generate_wave(net, num_samples, first_samples=None, temperature=1.):
             prob = prob.cpu()
             np_prob = prob.data.numpy()
             x = np.random.choice(classes, p=np_prob)
-            x = Variable(torch.LongTensor([x]))#np.array([x])
+            x = Variable(torch.LongTensor([x]))  # np.array([x])
         else:
             x = torch.max(x, 0)[1].float()
 
@@ -100,19 +104,19 @@ def generate_wave(net, num_samples, first_samples=None, temperature=1.):
         if i % 1600 == 0:
             print(str(100 * i // num_samples) + "% generated")
 
-    generated = (generated.float() / classes) * 2. - 1
+    generated = (generated.float() / classes) * 2.0 - 1
 
     mu_gen = _mu_law_expansion(generated, classes)
-    mu_gen = mu_gen.to('cpu').detach().numpy().copy() #to numpy
+    mu_gen = mu_gen.to("cpu").detach().numpy().copy()  # to numpy
 
     toc = time.time()
-    print("ailia processing does take {} seconds".format(str(toc-tic)))
+    print("ailia processing does take {} seconds".format(str(toc - tic)))
 
     return mu_gen
 
 
 def _inference(net, input):
-    input = input.to('cpu').detach().numpy().copy()
+    input = input.to("cpu").detach().numpy().copy()
     if not args.onnx:
         output = net.run(input)
     else:
@@ -128,7 +132,7 @@ def _mu_law_expansion(data, mu):
 
 def _constant_pad_1d(input, target_size, dimension=0, value=0, pad_start=False):
     num_pad = target_size - input.size(dimension)
-    assert num_pad >= 0, 'target size has to be greater than input size'
+    assert num_pad >= 0, "target size has to be greater than input size"
 
     input_size = input.size()
 
@@ -139,7 +143,9 @@ def _constant_pad_1d(input, target_size, dimension=0, value=0, pad_start=False):
 
     # crop output
     if pad_start:
-        c_output = c_output.narrow(dimension, num_pad, c_output.size(dimension) - num_pad)
+        c_output = c_output.narrow(
+            dimension, num_pad, c_output.size(dimension) - num_pad
+        )
     else:
         c_output = c_output.narrow(dimension, 0, c_output.size(dimension) - num_pad)
 
@@ -161,11 +167,11 @@ def main():
     generated = generate_wave(net, 16000, first_samples=sample, temperature=1.0)
 
     # output wav
-    output_wav('output.wav', 1600, generated)
+    output_wav("output.wav", 1600, generated)
 
     print(generated)
 
-    logger.info('Script finished successfully.')
+    logger.info("Script finished successfully.")
 
 
 if __name__ == "__main__":
