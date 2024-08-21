f7d734cb527e4da4e1faeba2b1f16606c0711d75
ooe1123 <ooe1123@gmail.com>
2024-08-22 00:21:34 +0900

    Revise pytorch_wavenet

--- audio_processing/pytorch_wavenet/README.md
+++ audio_processing/pytorch_wavenet/README.md
@@ -2,15 +2,36 @@
 
 ### Input
 
-audio file (randomly generated)
+audio file
+
+- For the sample: first_sample.wav
 
 ### Output
 
 audio file
 
+## Requirements
+
+This model requires additional module.
+
+```
+pip3 install soundfile
+```
+
 ### Usage
 
 Automatically downloads the onnx and prototxt files on the first run. It is necessary to be connected to the Internet while downloading.
+
+For the sample wav,
+```bash
+$ python3 pytorch_wavenet.py
+```
+
+If you want to specify the input audio file, put the wav path after the `--input` option.  
+You can use `--savepath` option to change the name of the output file to save.
+```bash
+$ python3 pytorch_wavenet.py --input WAV_PATH --savepath SAVE_WAV_PATH
+```
 
 In this version, you have to use `--onnx` option.
 
@@ -30,8 +51,8 @@ PyTorch 1.5.0
 
 ### Model Format
 
-ONNX opset = 11
+ONNX opset = 17
 
 ### Netron
 
-[pytorch_wavenet.onnx.prototxt](https://netron.app/?url=https://storage.googleapis.com/ailia-models/)
+[wavenet_pytorch_op_17.onnx.prototxt](https://netron.app/?url=https://storage.googleapis.com/ailia-models/pytorch_wavenet/wavenet_pytorch_op_17.onnx.prototxt)
--- audio_processing/pytorch_wavenet/pytorch_wavenet.py
+++ audio_processing/pytorch_wavenet/pytorch_wavenet.py
@@ -1,9 +1,6 @@
 import time
 import sys
 import numpy as np
-import torch
-import torch.nn.functional as F
-from torch.autograd import Variable
 
 import soundfile as sf
 
@@ -11,7 +8,8 @@ import ailia
 
 # import original moduls
 sys.path.append("../../util")
-from utils import get_base_parser, update_parser, get_savepath  # noqa: E402
+from arg_utils import get_base_parser, update_parser, get_savepath  # noqa: E402
+from math_utils import softmax  # noqa: E402
 from model_utils import check_and_download_models  # noqa: E402
 
 # logger
@@ -27,15 +25,22 @@ WEIGHT_PATH = "wavenet_pytorch_op_17.onnx"
 MODEL_PATH = "wavenet_pytorch_op_17.onnx.prototxt"
 REMOTE_PATH = "https://storage.googleapis.com/ailia-models/pytorch-wavenet/"
 
+SAMPLE_WAVE_PATH = "first_sample.wav"
+SAVE_WAVE_PATH = "output.wav"
 
 # ======================
 # Arguemnt Parser Config
 # ======================
 parser = get_base_parser(
-    "pytorch_wavenet", "./dammy.wav", "./output.txt", input_ftype="audio"
+    "pytorch_wavenet", SAMPLE_WAVE_PATH, SAVE_WAVE_PATH, input_ftype="audio"
+)
+parser.add_argument("--num_samples", type=float, default=160000, help="num_samples")
+parser.add_argument("--temperature", type=float, default=1.0, help="temperature")
+parser.add_argument(
+    "--no_input", action="store_true", default=False, help="not sample input"
 )
 parser.add_argument(
-    "-n", "--onnx", action="store_true", default=False, help="Use onnxruntime"
+    "--onnx", action="store_true", default=False, help="Use onnxruntime"
 )
 args = update_parser(parser)
 
@@ -55,25 +60,18 @@ def get_model():
     return net
 
 
-def generate_first_sample():
-    first_samples = torch.randint(0, 256, (3085,))
-    return first_samples
-
-
 def output_wav(filename, rate, data):
-    sf.write(filename, rate, data)
+    sf.write(filename, data, rate)
 
 
 def generate_wave(net, num_samples, first_samples=None, temperature=1.0):
     tic = time.time()
-
     progress_interval = 1000
 
-    # receptive_field = 1021
     classes = 256
 
     if first_samples is None:
-        first_samples = generate_first_sample()
+        first_samples = np.zeros(1, dtype=int) + (classes // 2)
 
     num_given_samples = first_samples.shape[0]
     total_samples = num_given_samples + num_samples
@@ -82,16 +80,15 @@ def generate_wave(net, num_samples, first_samples=None, temperature=1.0):
     input[:, first_samples[0], :] = 1
 
     # prepare queues
-    shapes = [
-        # fmt: off
-        (32, 2), (32, 3), (32, 5), (32, 9), (32, 17),
-        (32, 33), (32, 65), (32, 129), (32, 257), (32, 513),
-        (32, 2), (32, 3), (32, 5), (32, 9), (32, 17),
-        (32, 33), (32, 65), (32, 129), (32, 257), (32, 513),
-        (32, 2), (32, 3), (32, 5), (32, 9), (32, 17),
-        (32, 33), (32, 65), (32, 129), (32, 257), (32, 513),
-        # fmt: on
-    ]
+    kernel_size = 2
+    blocks = 3
+    layers = 10
+    shapes = []
+    for _ in range(blocks):
+        new_dilation = 1
+        for _ in range(layers):
+            shapes.append((32, (kernel_size - 1) * new_dilation + 1))
+            new_dilation *= 2
     dilated_queues = [np.zeros(shape, dtype=np.float32) for shape in shapes]
 
     # fill queues with given samples
@@ -104,34 +101,32 @@ def generate_wave(net, num_samples, first_samples=None, temperature=1.0):
         if i % progress_interval == 0:
             print(str(100 * i // total_samples) + "% generated")
 
-    # generated = Variable(first_samples, volatile=True)
-    # num_pad = receptive_field - generated.size(0)
-
     # generate new samples
+    generated = np.array([])
     for i in range(num_samples):
         x, dilated_queues = _inference(net, input, dilated_queues)
         x = x.squeeze()
 
         if temperature > 0:
             x /= temperature
-            prob = F.softmax(x, dim=0)
-            prob = prob.cpu()
-            np_prob = prob.data.numpy()
-            x = np.random.choice(classes, p=np_prob)
-            x = Variable(torch.LongTensor([x]))  # np.array([x])
+            prob = softmax(x, axis=0)
+            x = np.random.choice(classes, p=prob)
         else:
-            x = torch.max(x, 0)[1].float()
+            x = np.argmax(x)
 
-        generated = torch.cat((generated, x), 0)
+        # set new input
+        input.fill(0)
+        input[:, x, :] = 1
+
+        x = np.array([x])
+        generated = np.append(generated, x)
 
         # progress feedback
         if i % progress_interval == 0:
             print(str(100 * (i + num_given_samples) // total_samples) + "% generated")
 
-    generated = (generated.float() / classes) * 2.0 - 1
-
+    generated = (generated / classes) * 2.0 - 1
     mu_gen = _mu_law_expansion(generated, classes)
-    mu_gen = mu_gen.to("cpu").detach().numpy().copy()  # to numpy
 
     toc = time.time()
     print("ailia processing does take {} seconds".format(str(toc - tic)))
@@ -172,13 +167,27 @@ def main():
     net = get_model()
 
     # generate sample
-    sample = generate_first_sample()
+    if args.no_input:
+        sample = None
+    else:
+        input_file = args.input[0]
+        wav, _ = sf.read(input_file)
+        sample = (wav * 255).astype(int)
+
+    num_samples = args.num_samples
+    temperature = args.temperature
 
     # generate wave
-    generated = generate_wave(net, 160000, first_samples=sample, temperature=1.0)
+    generated = generate_wave(
+        net, num_samples, first_samples=sample, temperature=temperature
+    )
 
     # output wav
-    output_wav("output.wav", 16000, generated)
+    savepath = get_savepath(
+        args.savepath, args.input[0] if not args.no_input else "dummy.wav", ext=".wav"
+    )
+    logger.info(f"saved at : {savepath}")
+    output_wav(savepath, 16000, generated)
 
     logger.info("Script finished successfully.")
 
