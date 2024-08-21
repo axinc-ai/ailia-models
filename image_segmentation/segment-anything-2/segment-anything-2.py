ff2c48bb683f69ec7efd934c7ed7e3758625adc5
Kazuki Kyakuno <kyakuno@axell.co.jp>
2024-08-21 17:09:19 +0900

    Implement segment anything

--- image_segmentation/segment-anything-2/segment-anything-2.py
+++ image_segmentation/segment-anything-2/segment-anything-2.py
@@ -2,9 +2,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import ailia
+from typing import Optional
+from typing import Tuple
 
 # %%
 np.random.seed(3)
+
+import torch
+
+class PositionEmbeddingRandom():
+    """
+    Positional encoding using random spatial frequencies.
+    """
+
+    def __init__(self, num_pos_feats: int = 64, scale: Optional[float] = None) -> None:
+        if scale is None or scale <= 0.0:
+            scale = 1.0
+        self.positional_encoding_gaussian_matrix = scale * torch.randn((2, num_pos_feats)),
+
+    def _pe_encoding(self, coords: torch.Tensor) -> torch.Tensor:
+        """Positionally encode points that are normalized to [0,1]."""
+        # assuming coords are in [0, 1]^2 square and have d_1 x ... x d_n x 2 shape
+        coords = 2 * coords - 1
+        coords = coords @ self.positional_encoding_gaussian_matrix
+        coords = 2 * np.pi * coords
+        # outputs d_1 x ... x d_n x C shape
+        return torch.cat([torch.sin(coords), torch.cos(coords)], dim=-1)
+
+    def __call__(self, size: Tuple[int, int]) -> torch.Tensor:
+        """Generate positional encoding for a grid of the specified size."""
+        h, w = size
+        grid = torch.ones((h, w), dtype=torch.float32)
+        y_embed = grid.cumsum(dim=0) - 0.5
+        x_embed = grid.cumsum(dim=1) - 0.5
+        y_embed = y_embed / h
+        x_embed = x_embed / w
+
+        pe = self._pe_encoding(torch.stack([x_embed, y_embed], dim=-1))
+        return pe.permute(2, 0, 1)  # C x H x W
 
 def show_mask(mask, ax, random_color=False, borders = True):
     if random_color:
@@ -51,6 +86,7 @@ def show_masks(image, masks, scores, point_coords=None, box_coords=None, input_l
 
 def predict(
     features,
+    orig_hw,
     point_coords: Optional[np.ndarray] = None,
     point_labels: Optional[np.ndarray] = None,
     box: Optional[np.ndarray] = None,
@@ -59,17 +95,14 @@ def predict(
     return_logits: bool = False,
     normalize_coords=True,
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    if not self._is_image_set:
-        raise RuntimeError(
-            "An image must be set with .set_image(...) before mask prediction."
-        )
-
     # Transform input prompts
     mask_input, unnorm_coords, labels, unnorm_box = _prep_prompts(
-       features, point_coords, point_labels, box, mask_input, normalize_coords
+       point_coords, point_labels, box, mask_input, normalize_coords, orig_hw
     )
 
     masks, iou_predictions, low_res_masks = _predict(
+        features,
+        orig_hw,
         unnorm_coords,
         labels,
         unnorm_box,
@@ -84,7 +117,7 @@ def predict(
     return masks_np, iou_predictions_np, low_res_masks_np
 
 def _prep_prompts(
-    point_coords, point_labels, box, mask_logits, normalize_coords, img_idx=-1
+    point_coords, point_labels, box, mask_logits, normalize_coords, orig_hw
 ):
 
     unnorm_coords, labels, unnorm_box, mask_input = None, None, None, None
@@ -93,22 +126,22 @@ def _prep_prompts(
             point_labels is not None
         ), "point_labels must be supplied if point_coords is supplied."
         point_coords = torch.as_tensor(
-            point_coords, dtype=torch.float, device=self.device
+            point_coords, dtype=torch.float
         )
-        unnorm_coords = self._transforms.transform_coords(
-            point_coords, normalize=normalize_coords, orig_hw=self._orig_hw[img_idx]
+        unnorm_coords = transform_coords(
+            point_coords, normalize=normalize_coords, orig_hw=orig_hw
         )
-        labels = torch.as_tensor(point_labels, dtype=torch.int, device=self.device)
+        labels = torch.as_tensor(point_labels, dtype=torch.int)
         if len(unnorm_coords.shape) == 2:
             unnorm_coords, labels = unnorm_coords[None, ...], labels[None, ...]
     if box is not None:
-        box = torch.as_tensor(box, dtype=torch.float, device=self.device)
-        unnorm_box = self._transforms.transform_boxes(
-            box, normalize=normalize_coords, orig_hw=self._orig_hw[img_idx]
+        box = torch.as_tensor(box, dtype=torch.float,)
+        unnorm_box = transform_boxes(
+            box, normalize=normalize_coords, orig_hw=orig_hw
         )  # Bx2x2
     if mask_logits is not None:
         mask_input = torch.as_tensor(
-            mask_logits, dtype=torch.float, device=self.device
+            mask_logits, dtype=torch.float
         )
         if len(mask_input.shape) == 3:
             mask_input = mask_input[None, :, :, :]
@@ -116,21 +149,14 @@ def _prep_prompts(
 
 def _predict(
     features,
+    orig_hw,
     point_coords: Optional[torch.Tensor],
     point_labels: Optional[torch.Tensor],
     boxes: Optional[torch.Tensor] = None,
     mask_input: Optional[torch.Tensor] = None,
     multimask_output: bool = True,
     return_logits: bool = False,
-    img_idx: int = -1,
-    export_to_onnx = False,
-    export_to_tflite = False
 ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-    if not self._is_image_set:
-        raise RuntimeError(
-            "An image must be set with .set_image(...) before mask prediction."
-        )
-
     if point_coords is not None:
         concat_points = (point_coords, point_labels)
     else:
@@ -152,6 +178,9 @@ def _predict(
 
 
     model = ailia.Net(weight="prompt_encoder.onnx", stream=None, memory_mode=11, env_id=1)
+    print(concat_points)
+    if mask_input is None:
+        mask_input = np.zeros((1, 1))
     sparse_embeddings, dense_embeddings = model.run(concat_points, mask_input)
 
     # Predict masks
@@ -159,19 +188,24 @@ def _predict(
         concat_points is not None and concat_points[0].shape[0] > 1
     )  # multi object prediction
     high_res_features = [
-        feat_level[img_idx].unsqueeze(0)
-        for feat_level in _features["high_res_feats"]
+        feat_level.unsqueeze(0)
+        for feat_level in features["high_res_feats"]
     ]
     model = ailia.Net(weight="mask_decoder.onnx", stream=None, memory_mode=11, env_id=1)
-    low_res_masks, iou_predictions, _, _ = model.run([_features["image_embed"][img_idx].unsqueeze(0), self.model.sam_prompt_encoder.get_dense_pe(), sparse_embeddings, dense_embeddings, multimask_output, batched_mode, high_res_features])
+    pe = PositionEmbeddingRandom()
+    image_embedding_size = [64, 64]
+    image_feature = features["image_embed"].unsqueeze(0).numpy()
+    image_pe = pe(image_embedding_size).unsqueeze(0).numpy()
+    low_res_masks, iou_predictions, _, _ = model.run([image_feature, image_pe,  sparse_embeddings, dense_embeddings, multimask_output, batched_mode, high_res_features])
 
     # Upscale the masks to the original image resolution
     masks = postprocess_masks(
-        low_res_masks, _orig_hw[img_idx]
+        low_res_masks, orig_hw
     )
     low_res_masks = torch.clamp(low_res_masks, -32.0, 32.0)
+    mask_threshold = 0.0
     if not return_logits:
-        masks = masks > self.mask_threshold
+        masks = masks > mask_threshold
 
     return masks, iou_predictions, low_res_masks
 
@@ -186,7 +220,8 @@ def transform_coords(
         coords[..., 0] = coords[..., 0] / w
         coords[..., 1] = coords[..., 1] / h
 
-    coords = coords * self.resolution  # unnormalize coords
+    resolution = 1024
+    coords = coords * resolution  # unnormalize coords
     return coords
 
 def transform_boxes(
@@ -198,11 +233,12 @@ def transform_boxes(
 def postprocess_masks(self, masks: torch.Tensor, orig_hw) -> torch.Tensor:
     # max_hole_areaは0.0を仮定している
     # そうでない場合はオリジナルは穴埋め処理が入る
+    import torch.nn.functional as F
     masks = F.interpolate(masks, orig_hw, mode="bilinear", align_corners=False)
     return masks
 
 
-show = False
+show = True
 
 
 model = ailia.Net(weight="image_encoder.onnx", stream=None, memory_mode=11, env_id=1)
@@ -219,14 +255,16 @@ img = cv2.resize(img, (1024, 1024))
 img = numpy.expand_dims(img, 0)
 img = numpy.transpose(img, (0, 3, 1, 2))
 print(img.shape)
+orig_hw = [img.shape[2], img.shape[3]]
 feats = model.run(img)
 
-_features = {"image_embed": feats[-1], "high_res_feats": feats[:-1]}
+features = {"image_embed": feats[-1], "high_res_feats": feats[:-1]}
 
 input_point = np.array([[500, 375]])
 input_label = np.array([1])
 
 masks, scores, logits = predict(
+    orig_hw=orig_hw,
     features=features,
     point_coords=input_point,
     point_labels=input_label,
