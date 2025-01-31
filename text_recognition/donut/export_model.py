0fa1159ea734f1de0cdc27fc4f9c8e837bf0e566
ooe1123 <ooe1123@gmail.com>
2025-01-31 23:50:08 +0900

    Implement export code

--- text_recognition/donut/export_model.py
+++ text_recognition/donut/export_model.py
@@ -1,7 +1,8 @@
 import json
+import os
 import sys
 import argparse
-from types import MethodType
+from types import MethodType, SimpleNamespace
 from typing import Optional, Tuple, Union
 
 import cv2
@@ -21,6 +22,89 @@ parser.add_argument(
     "--pretrained_path", type=str, default="naver-clova-ix/donut-base-finetuned-cord-v2"
 )
 args, left_argv = parser.parse_known_args()
+
+
+class Exp(torch.nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(
+        self,
+        input_ids,
+        attention_mask,
+        encoder_hidden_states,
+        key_cache0,
+        value_cache0,
+        key_cache1,
+        value_cache1,
+        key_cache2,
+        value_cache2,
+        key_cache3,
+        value_cache3,
+        key_cache4,
+        value_cache4,
+        key_cache5,
+        value_cache5,
+        key_cache6,
+        value_cache6,
+        key_cache7,
+        value_cache7,
+    ):
+        outputs = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            encoder_hidden_states=encoder_hidden_states,
+            past_key_values=[
+                [
+                    key_cache0,
+                    value_cache0,
+                    key_cache1,
+                    value_cache1,
+                ],
+                [
+                    key_cache2,
+                    value_cache2,
+                    key_cache3,
+                    value_cache3,
+                ],
+                [
+                    key_cache4,
+                    value_cache4,
+                    key_cache5,
+                    value_cache5,
+                ],
+                [
+                    key_cache6,
+                    value_cache6,
+                    key_cache7,
+                    value_cache7,
+                ],
+            ],
+            return_dict=True,
+            output_attentions=False,
+            output_hidden_states=False,
+        )
+        kv_cache = outputs.past_key_values
+        return (
+            outputs.logits,
+            kv_cache[0][0],
+            kv_cache[0][1],
+            kv_cache[0][2],
+            kv_cache[0][3],
+            kv_cache[1][0],
+            kv_cache[1][1],
+            kv_cache[1][2],
+            kv_cache[1][3],
+            kv_cache[2][0],
+            kv_cache[2][1],
+            kv_cache[2][2],
+            kv_cache[2][3],
+            kv_cache[3][0],
+            kv_cache[3][1],
+            kv_cache[3][2],
+            kv_cache[3][3],
+        )
 
 
 def forward(
@@ -150,14 +234,140 @@ def main():
             forward, decoder_layer.encoder_attn
         )
 
-    task_name = args.task
-    task_prompt = f"<s_{task_name}>"
+    model_name = os.path.basename(args.pretrained_path)
 
-    img = cv2.imread("cord_sample_receipt1.png")[..., ::-1]
-    img = Image.fromarray(img)
-    output = pretrained_model.inference(image=img, prompt=task_prompt)["predictions"][0]
+    with torch.no_grad():
+        print("------>")
+        image_tensors = torch.randn(1, 3, 1280, 960)
+        file_path = f"{model_name}_encoder.onnx"
+        torch.onnx.export(
+            pretrained_model.encoder,
+            image_tensors,
+            file_path,
+            input_names=["x"],
+            output_names=["output"],
+            verbose=False,
+            opset_version=17,
+        )
+        print("<------")
+        print(f"{file_path} created.")
 
-    print(json.dumps(output, indent=4, ensure_ascii=False))
+        print("------>")
+        model = Exp(pretrained_model.decoder.model)
+        input_ids = torch.tensor([[57579]])
+        model_kwargs = {
+            "encoder_outputs": SimpleNamespace(
+                last_hidden_state=torch.randn(1, 1200, 1024),
+            ),
+            "use_cache": True,
+            "cache_position": torch.tensor([0]),
+        }
+        model_inputs = prepare_inputs_for_inference(
+            pretrained_model.decoder.model, input_ids, **model_kwargs
+        )
+        kv_cache = model_inputs["past_key_values"]
+        x = (
+            model_inputs["input_ids"],
+            model_inputs["attention_mask"],
+            model_inputs["encoder_hidden_states"],
+            kv_cache[0][0],
+            kv_cache[0][1],
+            kv_cache[0][2],
+            kv_cache[0][3],
+            kv_cache[1][0],
+            kv_cache[1][1],
+            kv_cache[1][2],
+            kv_cache[1][3],
+            kv_cache[2][0],
+            kv_cache[2][1],
+            kv_cache[2][2],
+            kv_cache[2][3],
+            kv_cache[3][0],
+            kv_cache[3][1],
+            kv_cache[3][2],
+            kv_cache[3][3],
+        )
+        file_path = f"{model_name}.onnx"
+        torch.onnx.export(
+            model,
+            x,
+            file_path,
+            input_names=[
+                "input_ids",
+                "attention_mask",
+                "encoder_hidden_states",
+                "key_cache0",
+                "value_cache0",
+                "key_cache1",
+                "value_cache1",
+                "key_cache2",
+                "value_cache2",
+                "key_cache3",
+                "value_cache3",
+                "key_cache4",
+                "value_cache4",
+                "key_cache5",
+                "value_cache5",
+                "key_cache6",
+                "value_cache6",
+                "key_cache7",
+                "value_cache7",
+            ],
+            output_names=[
+                "logits",
+                "key_cache_out0",
+                "value_cache_out0",
+                "key_cache_out1",
+                "value_cache_out1",
+                "key_cache_out2",
+                "value_cache_out2",
+                "key_cache_out3",
+                "value_cache_out3",
+                "key_cache_out4",
+                "value_cache_out4",
+                "key_cache_out5",
+                "value_cache_out5",
+                "key_cache_out6",
+                "value_cache_out6",
+                "key_cache_out7",
+                "value_cache_out7",
+            ],
+            dynamic_axes={
+                "input_ids": {1: "n"},
+                "attention_mask": {1: "n"},
+                "key_cache0": [2],
+                "value_cache0": [2],
+                "key_cache1": [2],
+                "value_cache1": [2],
+                "key_cache2": [2],
+                "value_cache2": [2],
+                "key_cache3": [2],
+                "value_cache3": [2],
+                "key_cache4": [2],
+                "value_cache4": [2],
+                "key_cache5": [2],
+                "value_cache5": [2],
+                "key_cache6": [2],
+                "value_cache6": [2],
+                "key_cache7": [2],
+                "value_cache7": [2],
+            },
+            verbose=False,
+            opset_version=17,
+        )
+        print("<------")
+        print(f"{file_path} created.")
+
+    inference = False
+    if inference:
+        task_name = args.task
+        task_prompt = f"<s_{task_name}>"
+        img = cv2.imread("cord_sample_receipt1.png")[..., ::-1]
+        img = Image.fromarray(img)
+        output = pretrained_model.inference(image=img, prompt=task_prompt)[
+            "predictions"
+        ][0]
+        print(json.dumps(output, indent=4, ensure_ascii=False))
 
 
 if __name__ == "__main__":
