83b4f2fc8b147f918bfc592decc3a9f0a9d69ca4
ooe1123 <ooe1123@gmail.com>
2025-05-15 22:40:34 +0900

    Implement glass

--- anomaly_detection/glass/glass.py
+++ anomaly_detection/glass/glass.py
@@ -5,8 +5,6 @@ from logging import getLogger
 
 import ailia
 import cv2
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
 import scipy.ndimage as ndimage
 from PIL import Image
@@ -17,9 +15,6 @@ from arg_utils import get_base_parser, get_savepath, update_parser
 from detector_utils import load_image
 from image_utils import normalize_image
 from model_utils import check_and_download_models
-
-# from glass_utils import *
-
 
 logger = getLogger(__name__)
 
@@ -35,31 +30,35 @@ SAVE_IMAGE_PATH = "./output.png"
 BATCH_SIZE = 8
 IMAGE_SIZE = 288
 
+DATASET_NAME = [
+    "mvtec_carpet",
+    "mvtec_grid",
+    "mvtec_leather",
+    "mvtec_tile",
+    "mvtec_wood",
+    "mvtec_bottle",
+    "mvtec_cable",
+    "mvtec_capsule",
+    "mvtec_hazelnut",
+    "mvtec_metal_nut",
+    "mvtec_pill",
+    "mvtec_screw",
+    "mvtec_toothbrush",
+    "mvtec_transistor",
+    "mvtec_zipper",
+]
+
 # ======================
 # Arguemnt Parser Config
 # ======================
 
 parser = get_base_parser("GLASS", IMAGE_PATH, SAVE_IMAGE_PATH)
 parser.add_argument(
-    "-a",
-    "--arch",
-    default="resnet18",
-    choices=("resnet18", "wide_resnet50_2"),
-    help="arch model.",
-)
-parser.add_argument(
-    "-f",
-    "--feat",
-    metavar="PICKLE_FILE",
-    default=None,
-    help="train set feature pkl files.",
-)
-parser.add_argument(
-    "-tr",
-    "--train_dir",
-    metavar="DIR",
-    default="./train",
-    help="directory of the train files.",
+    "-d",
+    "--dataset_name",
+    default="mvtec_bottle",
+    choices=DATASET_NAME,
+    help="dataset name",
 )
 parser.add_argument(
     "-gt",
@@ -69,14 +68,8 @@ parser.add_argument(
     help="directory of the ground truth mask files.",
 )
 parser.add_argument("--seed", type=int, default=1024, help="random seed")
-parser.add_argument("-th", "--threshold", type=float, default=None, help="threshold")
 parser.add_argument("--onnx", action="store_true", help="execute onnxruntime version.")
 args = update_parser(parser)
-
-
-# ======================
-# Secondary Functions
-# ======================
 
 
 # ======================
@@ -84,10 +77,8 @@ args = update_parser(parser)
 # ======================
 
 
-def preprocess(img):
+def crop_img(img):
     im_h, im_w, _ = img.shape
-
-    img = img[:, :, ::-1]  # BGR -> RBG
 
     # resize
     short, long = (im_w, im_h) if im_w <= im_h else (im_h, im_w)
@@ -104,6 +95,30 @@ def preprocess(img):
     if oh > IMAGE_SIZE:
         y = int(round((oh - IMAGE_SIZE) / 2.0))
         img = img[y : y + IMAGE_SIZE, :, :]
+
+    return img
+
+
+def load_gt_imgs(img_paths, gt_type_dir):
+    gt_imgs = []
+    for i_img in range(0, len(img_paths)):
+        image_path = img_paths[i_img]
+        gt_img = None
+        if gt_type_dir:
+            fname = os.path.splitext(os.path.basename(image_path))[0]
+            gt_fpath = os.path.join(gt_type_dir, fname + "_mask.png")
+            if os.path.exists(gt_fpath):
+                gt_img = load_image(gt_fpath)
+                gt_img = cv2.cvtColor(gt_img, cv2.COLOR_BGRA2BGR)
+                gt_img = crop_img(gt_img)
+
+        gt_imgs.append(gt_img)
+
+    return gt_imgs
+
+
+def preprocess(img):
+    img = img[:, :, ::-1]  # BGR -> RBG
 
     img = normalize_image(img, normalize_type="ImageNet")
 
@@ -123,8 +138,6 @@ def predict(net, imgs):
         pad = np.zeros((BATCH_SIZE - b, 3, h, w), dtype=np.float32)
         imgs = np.concatenate([imgs, pad], axis=0)
 
-    # imgs = np.load("image.npy")
-
     # feedforward
     if not args.onnx:
         output = net.predict([imgs])
@@ -142,14 +155,19 @@ def predict(net, imgs):
 
 
 def recognize_from_image(net):
+    img_paths = args.input
+    gt_type_dir = args.gt_dir
+    gt_imgs = load_gt_imgs(img_paths, gt_type_dir)
+
     # input image loop
-    for i in range(0, len(args.input), BATCH_SIZE):
+    for img_no in range(0, len(img_paths), BATCH_SIZE):
         # prepare input data
         imgs = []
-        for image_path in args.input[i : i + BATCH_SIZE]:
+        for image_path in img_paths[img_no : img_no + BATCH_SIZE]:
             logger.info(image_path)
             img = load_image(image_path)
-            img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+            img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+            img = crop_img(img)
             imgs.append(img)
 
         # inference
@@ -170,10 +188,14 @@ def recognize_from_image(net):
 
         for i in range(len(imgs)):
             defect = imgs[i]
-            target = imgs[i]
             segmentations = masks[i]
+            target = gt_imgs[img_no + i]
             defect = cv2.resize(defect, (IMAGE_SIZE, IMAGE_SIZE))
-            target = cv2.resize(target, (IMAGE_SIZE, IMAGE_SIZE))
+            target = (
+                cv2.resize(target, (IMAGE_SIZE, IMAGE_SIZE))
+                if target is not None
+                else None
+            )
 
             mask = cv2.cvtColor(
                 cv2.resize(segmentations, (defect.shape[1], defect.shape[0])),
@@ -182,11 +204,12 @@ def recognize_from_image(net):
             mask = (mask * 255).astype("uint8")
             mask = cv2.applyColorMap(mask, cv2.COLORMAP_JET)
 
-            res_img = np.hstack([defect, target, mask])
-            res_img = cv2.resize(res_img, (256 * 3, 256))
+            col = [defect, target, mask] if target is not None else [defect, mask]
+            res_img = np.hstack(col)
+            res_img = cv2.resize(res_img, (256 * len(col), 256))
 
             # plot result
-            savepath = get_savepath(args.savepath, image_path, ext=".png")
+            savepath = get_savepath(args.savepath, img_paths[img_no + i], ext=".png")
             cv2.imwrite(savepath, res_img)
             logger.info(f"saved at : {savepath}")
 
@@ -194,25 +217,11 @@ def recognize_from_image(net):
 
 
 def main():
-    # info = {
-    #     "resnet18": (
-    #         WEIGHT_RESNET18_PATH,
-    #         MODEL_RESNET18_PATH,
-    #         ("140", "156", "172"),
-    #         448,
-    #         100,
-    #     ),
-    #     "wide_resnet50_2": (
-    #         WEIGHT_WIDE_RESNET50_2_PATH,
-    #         MODEL_WIDE_RESNET50_2_PATH,
-    #         ("356", "398", "460"),
-    #         1792,
-    #         550,
-    #     ),
-    # }
-    # # model files check and download
-    # weight_path, model_path, feat_names, t_d, d = info[args.arch]
-    # check_and_download_models(weight_path, model_path, REMOTE_PATH)
+    WEIGHT_PATH = f"{args.dataset_name}.onnx"
+    MODEL_PATH = f"{args.dataset_name}.onnx.prototxt"
+
+    # model files check and download
+    check_and_download_models(WEIGHT_PATH, MODEL_PATH, REMOTE_PATH)
 
     env_id = args.env_id
 
@@ -222,8 +231,7 @@ def main():
     else:
         import onnxruntime
 
-        # providers = ["CUDAExecutionProvider", "CPUExecutionProvider"]
-        providers = ["CPUExecutionProvider"]
+        providers = ["CUDAExecutionProvider", "CPUExecutionProvider"]
         net = onnxruntime.InferenceSession(WEIGHT_PATH, providers=providers)
 
     recognize_from_image(net)
