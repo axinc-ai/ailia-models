fix calculation of circle coordinates of paddleocr_v3

text_recognition/paddleocr_v3/paddleocr_v3.py
@@ -475,9 +475,9 @@ class DBPostProcess(object):
         # calculate circle coordinates
         pitch = 10
         x_upper = np.cos(np.arange(1, 0, (-1 / pitch)) * np.pi) * distance
-        y_upper = -np.sqrt(distance ** 2 - x_upper ** 2)
+        y_upper = -np.sqrt(np.abs(distance ** 2 - x_upper ** 2))
         x_lower = np.cos(np.arange(0, 1, (1 / pitch)) * np.pi) * distance
-        y_lower = np.sqrt(distance ** 2 - x_lower ** 2)
+        y_lower = np.sqrt(np.abs(distance ** 2 - x_lower ** 2))
         x = np.concatenate([x_upper, x_lower])
         y = np.concatenate([y_upper, y_lower])
         circle = np.concatenate([x[:, np.newaxis], y[:, np.newaxis]], axis=1)