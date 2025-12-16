81602b6638399b0e8df5b54153b47c3e65eacad3
Kazuki Kyakuno <kyakuno@axell.co.jp>
2025-12-17 08:36:54 +0900

    Add benchmark

--- audio_processing/sensevoice/sensevoice.py
+++ audio_processing/sensevoice/sensevoice.py
@@ -7,14 +7,24 @@ from pathlib import Path
 from funasr_onnx import SenseVoiceSmall
 from funasr_onnx.utils.postprocess_utils import rich_transcription_postprocess
 
+from logging import getLogger
+logger = getLogger(__name__)
 
 model_dir = "iic/SenseVoiceSmall"
 
 model = SenseVoiceSmall(model_dir, batch_size=10, quantize=False, cache_dir="./")
 
 # inference
-wav_or_scp = ["./example/en.mp3"]
+wav_or_scp = ["./example/ax.wav"]
 #wav_or_scp = ["./example/en.mp3", "./example/ja.mp3", "./example/ax.wav"]
 
+import time
+
+start = int(round(time.time() * 1000))
 res = model(wav_or_scp, language="auto", use_itn=True)
+end = int(round(time.time() * 1000))
+estimation_time = end - start
+#logger.info(f"\tencoder processing time {estimation_time} ms")
+print(f"\tprocessing time {estimation_time} ms")
+
 print([rich_transcription_postprocess(i) for i in res])
