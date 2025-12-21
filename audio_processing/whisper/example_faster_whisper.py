1e377b3760f55efd0449b97f167a8f49ec282735
Kazuki Kyakuno <kyakuno@axell.co.jp>
2025-12-22 08:31:53 +0900

    Added benchmark script

---
+++ audio_processing/whisper/example_faster_whisper.py
@@ -0,0 +1,27 @@
+﻿# Inference sample for benchmarking
+# pip3 install faster_whisper
+
+from faster_whisper import WhisperModel
+import time
+
+model_size = "large-v3-turbo"
+input_file = "ax.wav"
+
+# Run on CPU with FP32
+model = WhisperModel(model_size, device="cpu", compute_type="float32")
+# or run on GPU with INT8
+# model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
+# or run on CPU with INT8
+# model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
+start = int(round(time.time() * 1000))
+segments, info = model.transcribe(input_file, beam_size=5)
+end = int(round(time.time() * 1000))
+estimation_time = (end - start)
+
+print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+
+for segment in segments:
+    print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+
+print(f'\ttotal processing time {estimation_time} ms')
